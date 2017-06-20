34308c4b78b1b186f17acb6a7dd2767229c6a162
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-06-20 16:03:57 +0100

    Added github PR widget and set debugging mode.

---
+++ dashboard/jobs/github_prs.rb
@@ -0,0 +1,22 @@
+require 'octokit'
+
+SCHEDULER.every '1m', :first_in => 0 do |job|
+  client = Octokit::Client.new(:access_token => "6b2ea5805304d73feff275fce90d59f0c5e2a8b7")
+  my_organization = "TransformCore"
+  repos = client.organization_repositories(my_organization).map { |repo| repo.name }
+
+  open_pull_requests = repos.inject([]) { |pulls, repo|
+    client.pull_requests("#{my_organization}/#{repo}", :state => 'open').each do |pull|
+      pulls.push({
+        title: pull.title,
+        repo: repo,
+        updated_at: pull.updated_at.strftime("%b %-d %Y, %l:%m %p"),
+        creator: "@" + pull.user.login,
+        })
+    end
+    pulls
+  }
+  puts 'asdf'
+
+  send_event('PR_WIDGET_DATA_ID', { header: "Open Pull Requests", pulls: open_pull_requests })
+end
