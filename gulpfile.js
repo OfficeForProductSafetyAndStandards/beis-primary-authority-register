2632f07e4b8e6dce4db9c3ca8a4691ef44013e48
Stephen Mount <stemount@gmail.com>
2017-06-22 14:08:41 +0100

    minor improvements.

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,14 @@ var gulp = require('gulp'),
 // Drupal theme directory.
 var themeDir = 'web/themes/custom/par_theme/assets/stylesheets/';
 
+// Gulp tasks.
 gulp.task('sass', function () {
  return gulp.src(themeDir + '*.scss')
   .pipe(sourcemaps.init())
   .pipe(sass().on('error', sass.logError))
   .pipe(sourcemaps.write(themeDir))
   .pipe(gulp.dest(themeDir));
-})
+});
 
 gulp.task('watch', function() {
   gulp.watch(themeDir, ['sass']);
