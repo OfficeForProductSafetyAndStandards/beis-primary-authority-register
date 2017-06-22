a0388571a495780ce9ac4f08366d21349e82898e
Chris Moreton <chris@netsensia.com>
2017-06-22 14:44:38 +0100

    Use docker-compose on Travis

--- docker/reset-hard.sh
+++
@@ -1,2 +0,0 @@
-# This command will fail unless you have running images and running containers
-docker rm --force `docker ps -qa` && docker rmi $(docker images -q) --force && docker-compose up -d --force-recreate --build && sh setup.sh
