2ad24f4d2d06436347217f05cfdc462e90e44fe1
Chris Moreton <chris@netsensia.com>
2017-06-22 22:54:23 +0100

    Fix drupal-update.sh

--- drupal-update.sh
+++ drupal-update.sh
@@ -21,7 +21,7 @@ cd ${ROOT}/web; ../vendor/drush/drush/drush cr;
 # Run db updates.
 cd ${ROOT}/web; ../vendor/drush/drush/drush updb -y;
 # Import configuration twice to fix a problem with config import when new modules are added to 'core.extensions.yml'.
-cd ${ROOT}/web; ../vendor/drush/drush/drush cim -y; ./vendor/drush/drush/drush cim -y
+cd ${ROOT}/web; ../vendor/drush/drush/drush cim -y; ../vendor/drush/drush/drush cim -y
 # Take the site out of maintenance mode.
 cd ${ROOT}/web; ../vendor/drush/drush/drush sset system.maintenance_mode 0;
 # Clear cache.
