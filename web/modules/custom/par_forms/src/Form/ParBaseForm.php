917671bd0a97b2f8b575f4a4f493ae9c966774c4
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-06-22 17:45:05 +0100

    PAR-102: Initial commit for PAR base forms.

---
+++ web/modules/custom/par_forms/src/Form/ParBaseForm.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\par_forms\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Session\SessionManagerInterface;
+use Drupal\user\PrivateTempStoreFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * The base form controller for all PAR forms.
+ */
+abstract class ParBaseForm extends FormBase {
+
+  /**
+   * @var \Drupal\Core\Session\SessionManagerInterface
+   */
+  private $sessionManager;
+
+  /**
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  private $currentUser;
+
+  /**
+   * @var \Drupal\user\PrivateTempStore
+   */
+  protected $store;
+
+  /**
+   * @var string
+   *   A machine safe value representing the current form journey
+   */
+  protected $flow;
+
+  /**
+   * Constructs a \Drupal\par_forms\Form\ParBaseForm.
+   *
+   * @param \Drupal\user\PrivateTempStoreFactory $temp_store_factory
+   * @param \Drupal\Core\Session\SessionManagerInterface $session_manager
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   */
+  public function __construct(PrivateTempStoreFactory $temp_store_factory, SessionManagerInterface $session_manager, AccountInterface $current_user) {
+    $this->sessionManager = $session_manager;
+    $this->currentUser = $current_user;
+    $this->store = $temp_store_factory->get('par_form_flows');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('user.private_tempstore'),
+      $container->get('session_manager'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['next'] = [
+      '#type' => 'submit',
+      '#value' => t('Next'),
+    ];
+    $form['save'] = [
+      '#type' => 'submit',
+      '#value' => t('Save'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+}
