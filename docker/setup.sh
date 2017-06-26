eb5f7c14e9ba95bc7274ee95bf23542b4cc81a73
Chris Moreton <chris@netsensia.com>
2017-06-26 13:03:03 +0100

    Modifications to local development environment

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,12 +1,10 @@
 # Pull the docker images
 
-docker-compose up -d --force-recreate --build
+docker-compose up -d --force-recreate
 
 # Install dependencies
 
-if [ ! -f ../vendor/autoload.php ]; then
-    $1 docker exec -ti par_beta_web bash -c "sh composer.sh"
-fi
+docker exec -ti par_beta_web bash -c 'su - composer -c "cd /var/www/html && php composer.phar install"'
 
 # Setup the development settings file:
 
@@ -17,20 +15,21 @@ fi
     
 # Install test dependencies
  
-    $1 docker exec -it par_beta_web bash -c "cd /var/www/html/tests && rm -rf node_modules && /usr/local/n/versions/node/7.2.1/bin/npm install"
+    docker exec -it par_beta_web bash -c "cd /var/www/html/tests && rm -rf node_modules && /usr/local/n/versions/node/7.2.1/bin/npm install"
 
 # Load the test data:
  
     sleep 5 # Time for the server to boot
-    $1 docker exec -i par_beta_web /var/www/html/vendor/bin/drush sql-cli @dev --root=/var/www/html/web < fresh_drupal_postgres.sql
+    docker exec -i par_beta_web /var/www/html/vendor/bin/drush sql-cli @dev --root=/var/www/html/web < fresh_drupal_postgres.sql
 
 # Update Drupal
 
-    $1 docker exec -i par_beta_web sh /var/www/html/drupal-update.sh /var/www/html
+    docker exec -i par_beta_web sh /var/www/html/drupal-update.sh /var/www/html
 
 # Install front end dependencies
 
-    $1 docker exec -i par_beta_web gulp build
+    docker exec -it par_beta_web bash -c "rm -rf node_modules && /usr/local/n/versions/node/7.2.1/bin/npm install"
+    docker exec -i par_beta_web /usr/local/n/versions/node/7.2.1/bin/npm run gulp
 
 
 
