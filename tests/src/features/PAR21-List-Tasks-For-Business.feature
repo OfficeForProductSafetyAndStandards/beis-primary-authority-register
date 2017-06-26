5e0d8780dd1615e80e73fc18e6a7408b65a47050
jaffamonkey <paullittlebury@gmail.com>
2017-06-26 10:57:17 +0100

    Updated features and added Visual regression tool

---
+++ tests/src/features/PAR21-List-Tasks-For-Business.feature
@@ -0,0 +1,3 @@
+Feature: As the Business user,
+    I need to be able to see landing page for my business,
+    so that I can access the tasks required of me.
---
+++ tests/visualReview/protractor.js
@@ -0,0 +1,35 @@
+const VisualReview = require('visualreview-protractor');
+var vr = new VisualReview({
+    hostname: 'localhost',
+    port: 7000,
+});
+
+exports.config = {
+
+    specs: [
+        'spec.js'
+    ],
+
+    capabilities: {
+        browserName: 'chrome'
+    },
+
+    framework: 'jasmine2',
+
+    beforeLaunch: function () {
+        // Creates a new run under project name 'myProject', suite 'mySuite'.
+        return vr.initRun('myProject', 'mySuite');
+    },
+
+    afterLaunch: function (exitCode) {
+        // finalizes the run, cleans up temporary files
+        return vr.cleanup(exitCode);
+    },
+
+    // expose VisualReview protractor api in tests
+    params: {
+        visualreview: vr
+    }
+
+};
+
