97d50f40b6b62e9e233450aa8f2df540d6550cea
Chris Moreton <chris@netsensia.com>
2017-06-27 11:16:33 +0100

    Detect OS in setup script

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,10 +1,35 @@
+#!/usr/bin/env bash
+
+PRECOMMAND=""
+
+case "$(uname -s)" in
+
+   Darwin)
+     echo 'Mac OS X'
+     ;;
+
+   Linux)
+     echo 'Linux'
+     ;;
+
+   CYGWIN*|MINGW32*|MSYS*)
+     echo 'MS Windows'
+     dos2unix ../drupal-update.sh
+     PRECOMMAND="winpty"
+     ;;
+
+   *)
+     echo 'other OS' 
+     ;;
+esac
+
 # Pull the docker images
 
     docker-compose up -d --force-recreate
 
 # Install dependencies
 
-    $1 docker exec -ti par_beta_web bash -c 'su - composer -c "cd /var/www/html && php composer.phar install"'
+    $PRECOMMAND docker exec -ti par_beta_web bash -c 'su - composer -c "cd /var/www/html && php composer.phar install"'
 
 # Setup the development settings file:
 
@@ -15,21 +40,21 @@ fi
     
 # Install test dependencies
  
-    $1 docker exec -it par_beta_web bash -c "cd /var/www/html/tests && rm -rf node_modules && /usr/local/n/versions/node/7.2.1/bin/npm install"
+    $PRECOMMAND docker exec -it par_beta_web bash -c "cd /var/www/html/tests && rm -rf node_modules && /usr/local/n/versions/node/7.2.1/bin/npm install"
 
 # Load the test data:
  
     sleep 5 # Time for the server to boot
-    $1 docker exec -i par_beta_web /var/www/html/vendor/bin/drush sql-cli @dev --root=/var/www/html/web < fresh_drupal_postgres.sql
+    $PRECOMMAND docker exec -i par_beta_web /var/www/html/vendor/bin/drush sql-cli @dev --root=/var/www/html/web < fresh_drupal_postgres.sql
 
 # Update Drupal
 
-    $1 docker exec -i par_beta_web sh /var/www/html/drupal-update.sh /var/www/html
+    $PRECOMMAND docker exec -i par_beta_web sh /var/www/html/drupal-update.sh /var/www/html
 
 # Install front end dependencies
 
-    $1 docker exec -it par_beta_web bash -c "rm -rf node_modules && /usr/local/n/versions/node/7.2.1/bin/npm install"
-    $1 docker exec -i par_beta_web /usr/local/n/versions/node/7.2.1/bin/npm run gulp
+    $PRECOMMAND docker exec -it par_beta_web bash -c "rm -rf node_modules && /usr/local/n/versions/node/7.2.1/bin/npm install"
+    $PRECOMMAND docker exec -i par_beta_web /usr/local/n/versions/node/7.2.1/bin/npm run gulp
 
 
 
