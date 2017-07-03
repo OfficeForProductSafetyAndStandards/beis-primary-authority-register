76097cbc6f2ddb1c364f729de9251cda035fa05e
Chris Moreton <chris@netsensia.com>
2017-07-03 17:14:07 +0100

    Update refresh dependencies script

--- docker/refresh-dependencies.sh
+++ docker/refresh-dependencies.sh
@@ -3,3 +3,7 @@ docker exec -i par_beta_web bash -c 'su - composer -c "cd /var/www/html && php c
 docker exec -i par_beta_web bash -c "cd /var/www/html/tests && rm -rf node_modules/* && /usr/local/n/versions/node/7.2.1/bin/npm install"
 docker exec -i par_beta_web bash -c "rm -rf node_modules/* && /usr/local/n/versions/node/7.2.1/bin/npm install"
 docker exec -i par_beta_web bash -c "/usr/local/n/versions/node/7.2.1/bin/npm run gulp" 
+if [ ! -f ../web/sites/settings.local.php ]; then
+    cp ../web/sites/example.settings.local.php ../web/sites/default/settings.local.php
+    cat ../web/sites/settings.local.php.docker.append >> ../web/sites/default/settings.local.php
+fi
