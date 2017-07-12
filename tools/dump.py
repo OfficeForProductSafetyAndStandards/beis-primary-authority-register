b27d4cacd916c8f3e2e7fe00a1ab840a3082498d
Chris Moreton <chris@netsensia.com>
2017-07-12 15:21:48 +0100

    Remove S3 upload from dump script

--- tools/dump.py
+++ tools/dump.py
@@ -1,11 +1,6 @@
-## To be called on the PaaS environment to dump the current database
-
 import json, os
-import boto
-import boto.s3
 import sys
-from boto.s3.key import Key
-import datetime
+import time
 
 from subprocess import call
 
@@ -24,24 +19,6 @@ f.close()
 
 os.system("chmod 600 /home/vcap/.pgpass")
 
-outputfilename = '/home/vcap/par.sql'
+outputfilename = '/home/vcap/par-' + str(time.time()) + '.sql'
 
 os.system("../bin/pgsql/bin/pg_dump -h " + host + " -U " + username + " " + name + " > " + outputfilename)
-
-AWS_ACCESS_KEY_ID = os.environ.get('S3_ACCESS_KEY')
-AWS_SECRET_ACCESS_KEY = os.environ.get('S3_SECRET_KEY')
-S3_BUCKET = os.environ.get('S3_BUCKET')
-
-conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
-
-bucket = conn.create_bucket(S3_BUCKET, location=boto.s3.connection.Location.DEFAULT)
-
-print 'Uploading %s to Amazon S3 bucket %s' % \
-   (outputfilename, S3_BUCKET)
-
-def percent_cb(complete, total):
-    sys.stdout.write('.')
-    sys.stdout.flush()
-
-k = Key(bucket, "par-" + datetime.datetime.utcnow() + ".sql")
-k.set_contents_from_filename(testfile, cb=percent_cb, num_cb=10)
