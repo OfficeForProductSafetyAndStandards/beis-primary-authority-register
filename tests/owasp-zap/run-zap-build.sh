c00eb6d5ad4b1366e6381d46d919624a7307e4f1
jaffamonkey <paullittlebury@gmail.com>
2017-07-13 09:40:07 +0100

    Added artillery.io tests and owasp zap security test scripts

--- tests/owasp-zap/run-zap-build.sh
+++ tests/owasp-zap/run-zap-build.sh
@@ -1,2 +1,2 @@
 docker pull owasp/zap2docker-stable
-docker run -t owasp/zap2docker-weekly zap-baseline.py -t http://localhost:80
+docker run -t owasp/zap2docker-stable zap-baseline.py -t http://192.168.82.68:8111:80
--- tests/owasp-zap/run-zap-local.sh
+++ tests/owasp-zap/run-zap-local.sh
@@ -1,2 +1,2 @@
 docker pull owasp/zap2docker-stable
-docker run -t owasp/zap2docker-weekly zap-baseline.py -t http://localhost:8111 -r testreport.html
+docker run -t owasp/zap2docker-stable zap-baseline.py -t http://localhost:8111 -r testreport.html
