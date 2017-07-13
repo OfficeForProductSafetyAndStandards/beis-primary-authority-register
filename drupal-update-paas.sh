d3377513d493588445c76342be7171a45116b45d
Chris Moreton <chris@netsensia.com>
2017-07-13 16:40:10 +0100

    Use sh instead of source in Drupal update

--- drupal-update-paas.sh
+++ drupal-update-paas.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 PATH=$PATH:/home/vcap/app/bin/pgsql/bin
-HOME=$HOME/app source /home/vcap/app/.profile.d/bp_env_vars.sh
+HOME=$HOME/app sh /home/vcap/app/.profile.d/bp_env_vars.sh
 cd /home/vcap/app
 sh drupal-update.sh /home/vcap/app
 
