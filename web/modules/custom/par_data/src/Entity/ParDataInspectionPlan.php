417d5711206fc40f0be768c261698cc39fe51981
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-07-17 13:53:15 +0100

    PAR-205: Added PAR Inspection Plan entities.

---
+++ web/modules/custom/par_data/src/Entity/ParDataInspectionPlan.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\par_data\Entity;
+
+use Drupal\trance\Trance;
+
+/**
+ * Defines the par_data_inspection_plan entity.
+ *
+ * @ingroup par_data
+ *
+ * @ContentEntityType(
+ *   id = "par_data_inspection_plan",
+ *   label = @Translation("PAR Inspection Plan"),
+ *   label_collection = @Translation("PAR Inspection Plans"),
+ *   label_singular = @Translation("PAR Inspection Plan"),
+ *   label_plural = @Translation("PAR Inspection Plans"),
+ *   label_count = @PluralTranslation(
+ *     singular = "@count inspection plan",
+ *     plural = "@count inspection plans"
+ *   ),
+ *   bundle_label = @Translation("PAR Inspection Plan type"),
+ *   handlers = {
+ *     "storage" = "Drupal\trance\TranceStorage",
+ *     "storage_schema" = "Drupal\trance\TranceStorageSchema",
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\trance\TranceListBuilder",
+ *     "views_data" = "Drupal\trance\TranceViewsData",
+ *     "form" = {
+ *       "default" = "Drupal\trance\Form\ParEntityForm",
+ *       "add" = "Drupal\trance\Form\ParEntityForm",
+ *       "edit" = "Drupal\trance\Form\ParEntityForm",
+ *       "delete" = "Drupal\trance\Form\TranceDeleteForm",
+ *     },
+ *     "access" = "Drupal\trance\Access\TranceAccessControlHandler",
+ *   },
+ *   base_table = "par_data_inspection_plan",
+ *   data_table = "par_data_inspection_plan_field_data",
+ *   revision_table = "par_data_inspection_plan_revision",
+ *   revision_data_table = "par_data_inspection_plan_field_revision",
+ *   admin_permission = "administer par_data_inspection_plan entities",
+ *   translatable = TRUE,
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "revision" = "revision_id",
+ *     "bundle" = "type",
+ *     "label" = "name",
+ *     "uuid" = "uuid",
+ *     "uid" = "user_id",
+ *     "langcode" = "langcode",
+ *     "status" = "status"
+ *   },
+ *   links = {
+ *     "collection" = "/admin/content/par_data/par_data_inspection_plan",
+ *     "canonical" = "/admin/content/par_data/par_data_inspection_plan/{par_data_inspection_plan}",
+ *     "edit-form" = "/admin/content/par_data/par_data_inspection_plan/{par_data_inspection_plan}/edit",
+ *     "delete-form" = "/admin/content/par_data/par_data_inspection_plan/{par_data_inspection_plan}/delete"
+ *   },
+ *   bundle_entity_type = "par_data_inspection_plan_type",
+ *   permission_granularity = "bundle",
+ *   field_ui_base_route = "entity.par_data_inspection_plan_type.edit_form"
+ * )
+ */
+class ParDataInspectionPlan extends Trance {
+
+}
---
+++ web/modules/custom/par_data/src/Entity/ParDataInspectionPlanType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\par_data\Entity;
+
+use Drupal\trance\TranceType;
+
+/**
+ * Defines the par_data_inspection_plan_type entity.
+ *
+ * @ConfigEntityType(
+ *   id = "par_data_inspection_plan_type",
+ *   label = @Translation("PAR Inspection Plan Type"),
+ *   handlers = {
+ *     "list_builder" = "Drupal\trance\TranceTypeListBuilder",
+ *     "form" = {
+ *       "add" = "Drupal\trance\Form\TranceTypeForm",
+ *       "edit" = "Drupal\trance\Form\TranceTypeForm",
+ *       "delete" = "Drupal\trance\Form\TranceTypeDeleteForm"
+ *     }
+ *   },
+ *   config_prefix = "par_data_inspection_plan_type",
+ *   admin_permission = "administer site configuration",
+ *   bundle_of = "par_data_authority",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid"
+ *   },
+ *   links = {
+ *     "canonical" = "/admin/structure/par_data/par_data_inspection_plan_type/{par_data_inspection_plan_type}",
+ *     "edit-form" = "/admin/structure/par_data/par_data_inspection_plan_type/{par_data_inspection_plan_type}/edit",
+ *     "delete-form" = "/admin/structure/par_data/par_data_inspection_plan_type/{par_data_inspection_plan_type}/delete",
+ *     "collection" = "/admin/structure/par_data/par_data_inspection_plan_type"
+ *   },
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "description",
+ *     "help"
+ *   }
+ * )
+ */
+class ParDataInspectionPlanType extends TranceType {
+
+}
