bfe06b84c645bbaf158700a37c14b835a7766530
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-07-17 11:31:19 +0100

    PAR-205: Added PAR Partnership entities.

---
+++ web/modules/custom/par_data/src/Entity/ParDataPartnership.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\par_data\Entity;
+
+use Drupal\trance\Trance;
+
+/**
+ * Defines the par_entity entity.
+ *
+ * @ingroup par_entity
+ *
+ * @ContentEntityType(
+ *   id = "par_data_partnership",
+ *   label = @Translation("PAR Partnership"),
+ *   label_collection = @Translation("PAR Partnerships"),
+ *   label_singular = @Translation("PAR Partnership"),
+ *   label_plural = @Translation("PAR Partnerships"),
+ *   label_count = @PluralTranslation(
+ *     singular = "@count partnership item",
+ *     plural = "@count partnerships items"
+ *   ),
+ *   bundle_label = @Translation("PAR Partnership type"),
+ *   handlers = {
+ *     "storage" = "Drupal\trance\TranceStorage",
+ *     "storage_schema" = "Drupal\trance\TranceStorageSchema",
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\trance\TranceListBuilder",
+ *     "views_data" = "Drupal\trance\TranceViewsData",
+ *     "form" = {
+ *       "default" = "Drupal\trance\Form\ParEntityForm",
+ *       "add" = "Drupal\trance\Form\ParEntityForm",
+ *       "edit" = "Drupal\trance\Form\ParEntityForm",
+ *       "delete" = "Drupal\trance\Form\TranceDeleteForm",
+ *     },
+ *     "access" = "Drupal\trance\Access\TranceAccessControlHandler",
+ *   },
+ *   base_table = "par_data_partnership",
+ *   data_table = "par_data_partnership_field_data",
+ *   revision_table = "par_data_partnership_revision",
+ *   revision_data_table = "par_data_partnership_field_revision",
+ *   admin_permission = "administer par_data_partnership entities",
+ *   translatable = TRUE,
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "revision" = "revision_id",
+ *     "bundle" = "type",
+ *     "label" = "name",
+ *     "uuid" = "uuid",
+ *     "uid" = "user_id",
+ *     "langcode" = "langcode",
+ *     "status" = "status"
+ *   },
+ *   links = {
+ *     "collection" = "/admin/content/par_data/par_data_partnership",
+ *     "canonical" = "/admin/content/par_data/par_data_partnership/{par_entity}",
+ *     "edit-form" = "/admin/content/par_data/par_data_partnership/{par_entity}/edit",
+ *     "delete-form" = "/admin/content/par_data/par_data_partnership/{par_entity}/delete"
+ *   },
+ *   bundle_entity_type = "par_data_partnership_type",
+ *   permission_granularity = "bundle",
+ *   field_ui_base_route = "entity.par_data_partnership_type.edit_form"
+ * )
+ */
+class ParDataPartnership extends Trance {
+
+}
---
+++ web/modules/custom/par_data/src/Entity/ParDataPartnershipType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\par_data\Entity;
+
+use Drupal\trance\TranceType;
+
+/**
+ * Defines the par_entity type entity.
+ *
+ * @ConfigEntityType(
+ *   id = "par_data_partnership_type",
+ *   label = @Translation("PAR Partnership Type"),
+ *   handlers = {
+ *     "list_builder" = "Drupal\trance\TranceTypeListBuilder",
+ *     "form" = {
+ *       "add" = "Drupal\trance\Form\TranceTypeForm",
+ *       "edit" = "Drupal\trance\Form\TranceTypeForm",
+ *       "delete" = "Drupal\trance\Form\TranceTypeDeleteForm"
+ *     }
+ *   },
+ *   config_prefix = "par_data_partnership_type",
+ *   admin_permission = "administer site configuration",
+ *   bundle_of = "par_data_partnership",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid"
+ *   },
+ *   links = {
+ *     "canonical" = "/admin/structure/par_data/par_data_partnership/{par_data_partnership}",
+ *     "edit-form" = "/admin/structure/par_data/par_data_partnership/{par_data_partnership}/edit",
+ *     "delete-form" = "/admin/structure/par_data/par_data_partnership/{par_data_partnership}/delete",
+ *     "collection" = "/admin/structure/par_data/par_data_partnership"
+ *   },
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "description",
+ *     "help"
+ *   }
+ * )
+ */
+class ParDataPartnershipType extends TranceType {
+
+}
