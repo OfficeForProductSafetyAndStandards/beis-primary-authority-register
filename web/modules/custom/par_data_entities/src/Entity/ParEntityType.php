7341ec36a4a134ac8a2cc17757c1852f39446aa3
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-07-17 10:07:44 +0100

    PAR-205: Bringing back old module to ensure it is correctly removed.

---
+++ web/modules/custom/par_data_entities/src/Entity/ParEntityType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\par_data_entities\Entity;
+
+use Drupal\trance\TranceType;
+
+/**
+ * Defines the par_entity type entity.
+ *
+ * @ConfigEntityType(
+ *   id = "par_entity_type",
+ *   label = @Translation("PAR Entity Type"),
+ *   handlers = {
+ *     "list_builder" = "Drupal\trance\TranceTypeListBuilder",
+ *     "form" = {
+ *       "add" = "Drupal\trance\Form\TranceTypeForm",
+ *       "edit" = "Drupal\trance\Form\TranceTypeForm",
+ *       "delete" = "Drupal\trance\Form\TranceTypeDeleteForm"
+ *     }
+ *   },
+ *   config_prefix = "par_entity_type",
+ *   admin_permission = "administer site configuration",
+ *   bundle_of = "par_entity",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid"
+ *   },
+ *   links = {
+ *     "canonical" = "/admin/structure/par_entity_type/{par_entity_type}",
+ *     "edit-form" = "/admin/structure/par_entity_type/{par_entity_type}/edit",
+ *     "delete-form" = "/admin/structure/par_entity_type/{par_entity_type}/delete",
+ *     "collection" = "/admin/structure/par_entity_type"
+ *   },
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "description",
+ *     "help"
+ *   }
+ * )
+ */
+class ParEntityType extends TranceType {
+
+}
