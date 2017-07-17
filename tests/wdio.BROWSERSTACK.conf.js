d44adcbbaa7dbc5a073b303272d2f12225a02038
jaffamonkey <paullittlebury@gmail.com>
2017-07-17 10:14:46 +0100

    Added conf file for browserstack testing

--- tests/wdio.BROWSERSTACK.conf.js
+++ tests/wdio.BROWSERSTACK.conf.js
@@ -1,13 +1,10 @@
 const config = require('./wdio.conf.js').config;
 
-config.capabilities = [{
-    browserName: 'chrome',
-}];
 config.services = ['browserstack'];
 config.user = process.env.BROWSERSTACK_USERNAME;
 config.key = process.env.BROWSERSTACK_ACCESS_KEY;
 config.browserstackLocal = true;
-// config.host = 'hub-cloud.browserstack.com';
+config.host = 'hub-cloud.browserstack.com';
 config.baseUrl = 'http://localhost:8111';
 config.tags = '@ci, ~@Pending';
 exports.config = config;
