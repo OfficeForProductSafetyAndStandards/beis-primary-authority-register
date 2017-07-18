08a19b367475e331c5d6885b41e464d686b3ffcf
Chris Moreton <chris@netsensia.com>
2017-07-18 16:20:04 +0100

    Don't perform DB dumps until build fixed

--- drupal-update-paas.sh
+++ drupal-update-paas.sh
@@ -3,8 +3,8 @@ PRE_UNSANITIZED_FILENAME=drush-dump-pre-drush-updates-unsanitized-$TIMESTAMP.sql
 POST_UNSANITIZED_FILENAME=drush-dump-post-drush-updates-unsanitized-$TIMESTAMP.sql
 POST_SANITIZED_FILENAME=drush-dump-post-drush-updates-sanitized-$TIMESTAMP.sql
 cd /home/vcap/app
-sh drupal-dump.sh /home/vcap/app paas-unsanitized $PRE_UNSANITIZED_FILENAME
+#sh drupal-dump.sh /home/vcap/app paas-unsanitized $PRE_UNSANITIZED_FILENAME
 sh drupal-update.sh /home/vcap/app
-sh drupal-dump.sh /home/vcap/app paas-unsanitized $POST_UNSANITIZED_FILENAME
-sh drupal-dump.sh /home/vcap/app paas-sanitized $POST_SANITIZED_FILENAME
+#sh drupal-dump.sh /home/vcap/app paas-unsanitized $POST_UNSANITIZED_FILENAME
+#sh drupal-dump.sh /home/vcap/app paas-sanitized $POST_SANITIZED_FILENAME
 
