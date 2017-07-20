282087fd5e39fd8beb9d4a1c956f20f6d40bdd10
jaffamonkey <paullittlebury@gmail.com>
2017-07-20 16:54:11 +0100

    Added new basic check of errors

--- tests/wdio.DEV.conf.js
+++ tests/wdio.DEV.conf.js
@@ -7,5 +7,5 @@ config.capabilities = [{
 config.screenshotPath = './errorShots/';
 config.services = ['selenium-standalone'];
 config.baseUrl = 'http://127.0.0.1:8111';
-config.tags = '@ci, ~@Pending';
+config.tags = '@ci2, ~@Pending';
 exports.config = config;
