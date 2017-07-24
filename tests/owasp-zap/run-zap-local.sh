a09db5a8fc970b1834843f67c729f219277ce011
jaffamonkey <paullittlebury@gmail.com>
2017-07-24 14:17:20 +0100

    Updated (incorrectly) failing external link test

--- tests/owasp-zap/run-zap-local.sh
+++ tests/owasp-zap/run-zap-local.sh
@@ -1,12 +1,2 @@
 docker pull owasp/zap2docker-stable
-docker run -t owasp/zap2docker-stable zap-baseline.py -t [url] -r testreport.html
-
-
-
-git rm tests/pa11y/Dockerfile
-git rm tests/pa11y/docker/README.md
-git rm tests/pa11y/docker/compose/docker-compose.yml
-git rm tests/pa11y/docker/pa11y-dashboard/Dockerfile
-git rm tests/pa11y/docker/pa11y-dashboard/Makefile
-git rm tests/pa11y/docker/pa11y-dashboard/docker-entrypoint.sh
-git rm tests/reports/html/.gitignore
+docker run -t owasp/zap2docker-stable zap-baseline.py -t http://localhost:8111 -r testreport.html
