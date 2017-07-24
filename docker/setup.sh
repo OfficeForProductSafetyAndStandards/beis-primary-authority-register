77ebd9eb9cb410b32b1c7178ea5e7d0d08e5b319
Stephen Mount <stemount@gmail.com>
2017-07-24 14:10:02 +0100

    Putting in a note about /home/vcap so nobody else raises it as a Q

--- docker/setup.sh
+++ docker/setup.sh
@@ -34,6 +34,8 @@ fi
 
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush fsg s3backups $DATAFILE.tar.gz /dump.sql.tar.gz"
     docker exec -i par_beta_web bash -c "cd / && tar --no-same-owner -zxvf dump.sql.tar.gz"
+
+    # The tar.gz maintains the tar folder structure, so it exports in /home/vcap to maintain consistency.
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush sql-cli @dev --root=/var/www/html/web < /home/vcap/$DATAFILE && rm /home/vcap/$DATAFILE"
 
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush cc drush"
