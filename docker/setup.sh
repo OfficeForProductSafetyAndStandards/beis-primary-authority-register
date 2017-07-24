d4310f1fd828f89df0e853a6eef5298f6f9d3736
Stephen Mount <stemount@gmail.com>
2017-07-24 14:08:31 +0100

    just maintaining tabs/spaces indentation, not the end of the world.

--- docker/setup.sh
+++ docker/setup.sh
@@ -4,14 +4,14 @@
 
     cd /vagrant/docker
     sudo sh destroy-dependencies.sh
-    
+
 # Install dependencies
 
     docker exec -i par_beta_web bash -c 'su - composer -c "cd ../../var/www/html && php composer.phar install"'
     docker exec -i par_beta_web bash -c "cd /var/www/html/tests && rm -rf node_modules/* && ../../../../usr/local/n/versions/node/7.2.1/bin/npm install"
     docker exec -i par_beta_web bash -c "rm -rf node_modules/* && ../../../usr/local/n/versions/node/7.2.1/bin/npm install"
     docker exec -i par_beta_web bash -c "../../../usr/local/n/versions/node/7.2.1/bin/npm run gulp"
-    
+
 # Setup the development settings file:
 
 if [ ! -f ../web/sites/settings.local.php ]; then
@@ -21,22 +21,22 @@ fi
 
 # Load the test data:
 
-DATAFILE=drush-dump-post-drush-updates-sanitized-201707211640.sql
+    DATAFILE=drush-dump-post-drush-updates-sanitized-201707211640.sql
 
-	# Time for the server to boot
-    sleep 5 
+    # Time for the server to boot
+    sleep 5
 
-	# Got to load a clean database before "cr" and "fsg" commands can be bootstrapped
+    # Got to load a clean database before "cr" and "fsg" commands can be bootstrapped
     docker exec -i par_beta_web bash -c "vendor/bin/drush sql-cli @dev --root=/var/www/html/web < docker/fresh_drupal_postgres.sql"
-    
-	docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush cc drush"
+
+    docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush cc drush"
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush cr"
-    
+
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush fsg s3backups $DATAFILE.tar.gz /dump.sql.tar.gz"
     docker exec -i par_beta_web bash -c "cd / && tar --no-same-owner -zxvf dump.sql.tar.gz"
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush sql-cli @dev --root=/var/www/html/web < /home/vcap/$DATAFILE && rm /home/vcap/$DATAFILE"
-	
-	docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush cc drush"
+
+    docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush cc drush"
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush cr"
 
 # Update Drupal
