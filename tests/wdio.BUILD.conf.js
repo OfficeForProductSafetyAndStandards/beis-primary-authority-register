f8bfa8faa546d7c63cb4720e731915239cea2fed
jaffamonkey <paullittlebury@gmail.com>
2017-07-25 10:31:18 +0100

    Changed phantomjs to headless chrome for build testing

--- tests/wdio.BUILD.conf.js
+++ tests/wdio.BUILD.conf.js
@@ -1,10 +1,17 @@
 const config = require('./wdio.conf.js').config;
 
 config.capabilities = [{
-    browserName: 'phantomjs',
+    browserName: 'chrome',
+    chromeOptions: {
+        args: [
+            'headless',
+            'disable-gpu',
+            'window-size=1366,768',
+        ],
+    },
 }];
 
-config.services = ['phantomjs'];
+config.services = ['chromedriver'];
 config.baseUrl = 'http://127.0.0.1:80';
 config.tags = '@ci, ~@Pending';
 exports.config = config;
