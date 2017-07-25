6e1680951a7545057e9a392b02c9dab7b9af0fdc
Chris Moreton <chris@netsensia.com>
2017-07-25 11:50:58 +0100

    Next attempt to load S3 data

--- docker/setup.sh
+++ docker/setup.sh
@@ -21,8 +21,6 @@ fi
 
 # Load the test data:
 
-    DATAFILE=drush-dump-post-drush-updates-sanitized-201707211640.sql
-
     # Time for the server to boot
     sleep 5
 
@@ -34,11 +32,11 @@ fi
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush cc drush"
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush cr"
 
-    docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush fsg s3backups $DATAFILE.tar.gz /dump.sql.tar.gz"
+    docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush fsg s3backups latest.sql.tar.gz /dump.sql.tar.gz"
     docker exec -i par_beta_web bash -c "cd / && tar --no-same-owner -zxvf dump.sql.tar.gz"
 
     # The tar.gz maintains the tar folder structure, so it exports in /home/vcap to maintain consistency.
-    docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush sql-cli @dev --root=/var/www/html/web < /home/vcap/$DATAFILE && rm /home/vcap/$DATAFILE"
+    docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush sql-cli @dev --root=/var/www/html/web < /home/vcap/latest.sql && rm /home/vcap/latest.sql"
 
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush cc drush"
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush cr"
