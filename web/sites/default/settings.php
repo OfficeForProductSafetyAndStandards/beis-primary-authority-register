65b5c4581c9741fb6e398517d29cefcbc48e2495
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-07-26 23:04:27 +0100

    DEV: Adding config for Notify.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -842,6 +842,17 @@ $schemes = [
 $settings['flysystem'] = $schemes;
 
 /**
+ * Set GovUK Notify settings.
+ *
+ * These are confidential and should be set with ENV variables.
+ *
+ * SPECIAL ATTENTION MUST BE MADE TO ENSURE THE PRODUCTION KEY
+ * CAN NEVER BE SET ON ANYTHING BUT PRODUCTION ENVIRONMENT.
+ */
+$config['govuk_notify.settings']['api_key'] = getenv('PAR_GOVUK_NOTIFY_KEY');
+$config['govuk_notify.settings']['default_template_id'] = getenv('PAR_GOVUK_NOTIFY_TEMPLATE');
+
+/**
  * Load local development override configuration, if available.
  *
  * Use settings.local.php to override variables on secondary (staging,
