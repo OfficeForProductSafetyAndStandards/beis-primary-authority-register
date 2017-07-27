4f3ac9c54a1d4eef1a5591bbc628417aefc66b9c
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-07-27 16:08:12 +0100

    Added new data file.

--- docker/setup.sh
+++ docker/setup.sh
@@ -21,7 +21,7 @@ fi
 
 # Load the test data:
 
-    DATAFILE=drush-dump-post-drush-updates-sanitized-201707271133.sql
+    DATAFILE=drush-dump-post-drush-updates-sanitized-201707271349.sql
     
     # Time for the server to boot
     sleep 5
