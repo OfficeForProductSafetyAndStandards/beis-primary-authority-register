e4967e7f88b57ef1ca11cb809502265480fe01b1
Chris Moreton <chris@netsensia.com>
2017-07-27 14:38:43 +0100

    Remove /home/vcap when tarring backup

--- docker/setup.sh
+++ docker/setup.sh
@@ -39,8 +39,6 @@ fi
 
     docker exec -i par_beta_web bash -c "vendor/bin/drush @dev --root=/var/www/html/web sql-drop -y"
 
-    # The tar.gz maintains the tar folder structure, so it exports in /home/vcap to maintain consistency.
-    
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush sql-cli @dev --root=/var/www/html/web < $DATAFILE && rm $DATAFILE"
 
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush cc drush"
--- drupal-dump.sh
+++ drupal-dump.sh
@@ -15,27 +15,27 @@ else
 fi
 
 if [ -n "$3" ]; then
-  FILEPATH=$3
+  SQL_FILENAME=$3
 else
   echo $USAGE;
   exit 1;
 fi
 
-LOCALPATH=/home/vcap/$FILEPATH
-REMOTEPATH=$FILEPATH.tar.gz
+TAR_PATH=/home/vcap
 
 echo "Current working directory is ${PROJECT_ROOT}/web"
-COMMAND="../vendor/drush/drush/drush sql-dump @$DRUPAL_ENV --result-file=$LOCALPATH"
+COMMAND="../vendor/drush/drush/drush sql-dump @$DRUPAL_ENV --result-file=$TAR_PATH/$SQL_FILENAME"
 echo $COMMAND
 
 cd ${PROJECT_ROOT}/web; $COMMAND
 
-COMMAND="tar -zcvf $LOCALPATH.tar.gz $LOCALPATH"
+cd $TAR_PATH
+COMMAND="tar -zcvf $SQL_FILENAME.tar.gz $SQL_FILENAME"
 echo $COMMAND
 
 cd ${PROJECT_ROOT}/web; $COMMAND
 
-COMMAND="../vendor/drush/drush/drush fsp s3backups $LOCALPATH.tar.gz $REMOTEPATH"
+COMMAND="../vendor/drush/drush/drush fsp s3backups $SQL_FILENAME.tar.gz $SQL_FILENAME.tar.gz"
 echo $COMMAND
 
 cd ${PROJECT_ROOT}/web; $COMMAND
