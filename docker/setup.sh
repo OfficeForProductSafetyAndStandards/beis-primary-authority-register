d7d30bb2e03b3fd35095586c10ac211e7e17a454
Chris Moreton <chris@netsensia.com>
2017-07-27 11:05:50 +0100

    Redump database following S3 download

--- docker/setup.sh
+++ docker/setup.sh
@@ -37,6 +37,8 @@ fi
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush fsg s3backups $DATAFILE.tar.gz /dump.sql.tar.gz"
     docker exec -i par_beta_web bash -c "cd / && tar --no-same-owner -zxvf dump.sql.tar.gz"
 
+    docker exec -i par_beta_web bash -c "vendor/bin/drush @dev --root=/var/www/html/web sql-drop -y"
+
     # The tar.gz maintains the tar folder structure, so it exports in /home/vcap to maintain consistency.
     docker exec -i par_beta_web bash -c "cd web && ../vendor/bin/drush sql-cli @dev --root=/var/www/html/web < /home/vcap/$DATAFILE && rm $DATAFILE"
 
