535459db6bedc5e71cd618119333fe5a326529ff
jaffamonkey <paullittlebury@gmail.com>
2017-07-29 07:32:36 +0100

    updated owasp zap run scripts

--- tests/owasp-zap/run-zap-build.sh
+++ tests/owasp-zap/run-zap-build.sh
@@ -1,3 +1,2 @@
 docker pull owasp/zap2docker-stable
-docker run -u zap -p 8080:8080 -i owasp/zap2docker-stable zap.sh -daemon -host 0.0.0.0 -port 8080 &&
 docker run -t owasp/zap2docker-stable zap-baseline.py -t http://localhost:8111
