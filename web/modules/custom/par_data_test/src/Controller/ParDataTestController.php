ee5b0d740fafb31ddaed8c1af41ef2a250af9051
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-08-03 10:45:42 +0100

    DEV: Added test data reset url.

---
+++ web/modules/custom/par_data_test/src/Controller/ParDataTestController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\par_data_test\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+* A controller for all styleguide page output.
+*/
+class ParDataTestController extends ControllerBase {
+
+  /**
+  * The main index page for the styleguide.
+  */
+  public function reset() {
+    // Load the install file to reset the data.
+    module_load_include('install', 'par_data_test');
+
+    par_data_test_uninstall();
+    par_data_test_install();
+
+    $build = [
+      '#type' => 'markup',
+      '#markup' => 'The test data has been reset.'
+    ];
+
+    return $build;
+  }
+
+}
