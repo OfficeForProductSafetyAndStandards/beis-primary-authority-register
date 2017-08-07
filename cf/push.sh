f8dc0b63c2ee63397c4731dd88b2cfce5bf911c2
Chris Moreton <chris@netsensia.com>
2017-08-07 17:09:04 +0100

    Start building push tool

---
+++ cf/push.sh
@@ -0,0 +1,10 @@
+cd ..
+cf push -f manifest.$1.yml
+source cf/.env.$1
+cf set-env par-beta-$1 S3_ACCESS_KEY $S3_ACCESS_KEY
+cf set-env par-beta-$1 S3_SECRET_KEY $S3_SECRET_KEY
+cf set-env par-beta-$1 PAR_HASH_SALT $PAR_HASH_SALT
+cf set-env par-beta-$1 S3_BUCKET_PUBLIC $S3_BUCKET_PUBLIC
+cf set-env par-beta-$1 S3_BUCKET_PRIVATE $S3_BUCKET_PRIVATE
+cf restage par-beta-$1
+cf update-service beis-par-cdn-route -c '{"domain": "primary-authority.beis.gov.uk"}'
