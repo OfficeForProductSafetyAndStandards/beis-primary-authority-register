b19231ec015e62cf99e62c0292a3be672f5b5826
Chris Moreton <chris@netsensia.com>
2017-08-08 14:22:50 +0100

    Fix cf/cd typo

--- cf/push.sh
+++ cf/push.sh
@@ -6,10 +6,13 @@ cf set-env par-beta-$1 S3_SECRET_KEY $S3_SECRET_KEY
 cf set-env par-beta-$1 PAR_HASH_SALT $PAR_HASH_SALT
 cf set-env par-beta-$1 S3_BUCKET_PUBLIC $S3_BUCKET_PUBLIC
 cf set-env par-beta-$1 S3_BUCKET_PRIVATE $S3_BUCKET_PRIVATE
-cd set-env par-beta-$1 APP_ENV $1
-cd set-env par-beta-$1 PAR_GOVUK_NOTIFY_KEY $PAR_GOVUK_NOTIFY_KEY
-cd set-env par-beta-$1 PAR_GOVUK_NOTIFY_TEMPLATE $PAR_GOVUK_NOTIFY_TEMPLATE
+cf set-env par-beta-$1 APP_ENV $1
+cf set-env par-beta-$1 PAR_GOVUK_NOTIFY_KEY $PAR_GOVUK_NOTIFY_KEY
+cf set-env par-beta-$1 PAR_GOVUK_NOTIFY_TEMPLATE $PAR_GOVUK_NOTIFY_TEMPLATE
 cf restage par-beta-$1
+
+cf ssh $1 -c "cd app/tools && python extract_postgres_env_vars.py"
+cf ssh $1 -c "cd app && source drupal-update-paas-envs.sh && sh drupal-update.sh /home/vcap/app"
 
 if [$1 == "production"]; then
     cf update-service beis-par-cdn-route -c '{"domain": "primary-authority.beis.gov.uk"}'
