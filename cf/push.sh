b16a960d77deef4926afa90e36d71affe0e6fdd5
Chris Moreton <chris@netsensia.com>
2017-08-08 13:16:45 +0100

    Reconfigure domain mapping if pushing to production

--- cf/push.sh
+++ cf/push.sh
@@ -6,5 +6,9 @@ cf set-env par-beta-$1 S3_SECRET_KEY $S3_SECRET_KEY
 cf set-env par-beta-$1 PAR_HASH_SALT $PAR_HASH_SALT
 cf set-env par-beta-$1 S3_BUCKET_PUBLIC $S3_BUCKET_PUBLIC
 cf set-env par-beta-$1 S3_BUCKET_PRIVATE $S3_BUCKET_PRIVATE
+cd set-env par-beta-$1 APP_ENV $1
 cf restage par-beta-$1
-cf update-service beis-par-cdn-route -c '{"domain": "primary-authority.beis.gov.uk"}'
+
+if [$1 == "production"]; then
+    cf update-service beis-par-cdn-route -c '{"domain": "primary-authority.beis.gov.uk"}'
+fi
