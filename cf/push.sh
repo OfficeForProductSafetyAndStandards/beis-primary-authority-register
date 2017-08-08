f8c836bf20c55afed5389819da0cd1f82ed3f8f6
Chris Moreton <chris@netsensia.com>
2017-08-08 15:33:31 +0100

    Don't back up unsanitized versions any longer

--- cf/push.sh
+++ cf/push.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd ..
 cf push -f manifest.$1.yml
 source cf/.env.$1
@@ -14,6 +16,4 @@ cf restage par-beta-$1
 cf ssh $1 -c "cd app/tools && python extract_postgres_env_vars.py"
 cf ssh $1 -c "cd app && source drupal-update-paas-envs.sh && sh drupal-update.sh /home/vcap/app"
 
-if [$1 == "production"]; then
-    cf update-service beis-par-cdn-route -c '{"domain": "primary-authority.beis.gov.uk"}'
-fi
+sh update-domain-$1.sh
