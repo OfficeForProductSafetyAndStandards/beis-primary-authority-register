fa5fb32e4a46d8f1d255a13f5388a451574417e4
Chris Moreton <chris@netsensia.com>
2017-08-08 22:53:16 +0100

    Run Drupal update following selective deploy

--- cf/push.sh
+++ cf/push.sh
@@ -8,12 +8,13 @@ cf set-env par-beta-$1 S3_SECRET_KEY $S3_SECRET_KEY
 cf set-env par-beta-$1 PAR_HASH_SALT $PAR_HASH_SALT
 cf set-env par-beta-$1 S3_BUCKET_PUBLIC $S3_BUCKET_PUBLIC
 cf set-env par-beta-$1 S3_BUCKET_PRIVATE $S3_BUCKET_PRIVATE
+cf set-env par-beta-$1 S3_BUCKET_ARTIFACTS $S3_BUCKET_ARTIFACTS
 cf set-env par-beta-$1 APP_ENV $1
 cf set-env par-beta-$1 PAR_GOVUK_NOTIFY_KEY $PAR_GOVUK_NOTIFY_KEY
 cf set-env par-beta-$1 PAR_GOVUK_NOTIFY_TEMPLATE $PAR_GOVUK_NOTIFY_TEMPLATE
 cf restage par-beta-$1
 
-cf ssh $1 -c "cd app/tools && python extract_postgres_env_vars.py"
-cf ssh $1 -c "cd app && source drupal-update-paas-envs.sh && sh drupal-update.sh /home/vcap/app"
+cf ssh par-beta-$1 -c "cd app/tools && python extract_postgres_env_vars.py"
+cf ssh par-beta-$1 -c "cd app && source drupal-update-paas-envs.sh && sh drupal-update.sh /home/vcap/app"
 
 sh update-domain-$1.sh
