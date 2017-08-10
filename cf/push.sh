ed44dd2091e8ceaedeaeee4391d8696f1ac8fa1f
Chris Moreton <chris@netsensia.com>
2017-08-10 14:07:10 +0100

    Re-instate deploy section in .travis.yml

--- cf/push.sh
+++ cf/push.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-cd ..
-cf push -f manifest.$1.yml
-source cf/.env.$1
-cf set-env par-beta-$1 S3_ACCESS_KEY $S3_ACCESS_KEY
-cf set-env par-beta-$1 S3_SECRET_KEY $S3_SECRET_KEY
-cf set-env par-beta-$1 PAR_HASH_SALT $PAR_HASH_SALT
-cf set-env par-beta-$1 S3_BUCKET_PUBLIC $S3_BUCKET_PUBLIC
-cf set-env par-beta-$1 S3_BUCKET_PRIVATE $S3_BUCKET_PRIVATE
-cf set-env par-beta-$1 S3_BUCKET_ARTIFACTS $S3_BUCKET_ARTIFACTS
-cf set-env par-beta-$1 APP_ENV $1
-cf set-env par-beta-$1 PAR_GOVUK_NOTIFY_KEY $PAR_GOVUK_NOTIFY_KEY
-cf set-env par-beta-$1 PAR_GOVUK_NOTIFY_TEMPLATE $PAR_GOVUK_NOTIFY_TEMPLATE
-cf restage par-beta-$1
+ENV=$1
+VER=$2
 
-cf ssh par-beta-$1 -c "cd app/tools && python post_deploy.py"
+rm -rf build
+
+cd ..
+cf push -f manifest.$ENV.yml
+source cf/.env.$ENV
+cf set-env par-beta-$ENV S3_ACCESS_KEY $S3_ACCESS_KEY
+cf set-env par-beta-$ENV S3_SECRET_KEY $S3_SECRET_KEY
+cf set-env par-beta-$ENV PAR_HASH_SALT $PAR_HASH_SALT
+cf set-env par-beta-$ENV S3_BUCKET_PUBLIC $S3_BUCKET_PUBLIC
+cf set-env par-beta-$ENV S3_BUCKET_PRIVATE $S3_BUCKET_PRIVATE
+cf set-env par-beta-$ENV S3_BUCKET_ARTIFACTS $S3_BUCKET_ARTIFACTS
+cf set-env par-beta-$ENV APP_ENV $ENV
+cf set-env par-beta-$ENV PAR_GOVUK_NOTIFY_KEY $PAR_GOVUK_NOTIFY_KEY
+cf set-env par-beta-$ENV PAR_GOVUK_NOTIFY_TEMPLATE $PAR_GOVUK_NOTIFY_TEMPLATE
+cf restage par-beta-$ENV
+
+cf ssh par-beta-$ENV -c "cd app/tools && python post_deploy.py"
 
 cd cf
-sh update-domain-$1.sh
+sh update-domain-$ENV.sh
