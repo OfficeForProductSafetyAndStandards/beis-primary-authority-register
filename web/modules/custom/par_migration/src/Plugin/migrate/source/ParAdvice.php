6d88b648cb3ff5e8c728fe637b29864cb01ffe16
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-08-17 00:19:00 +0100

    real-migration: you know it.

--- web/modules/custom/par_migration/src/Plugin/migrate/source/ParAdvice.php
+++ web/modules/custom/par_migration/src/Plugin/migrate/source/ParAdvice.php
@@ -2,9 +2,12 @@
 
 namespace Drupal\par_migration\Plugin\migrate\source;
 
+use Drupal\Core\State\StateInterface;
 use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\Row;
 use Drupal\migrate\MigrateException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Migration of PAR2 Advice.
@@ -19,6 +22,34 @@ class ParAdvice extends SqlBase {
    * @var string $table The name of the database table.
    */
   protected $table = 'par_advice';
+
+  /**
+   * @var array
+   *   A cached array of regulatory functions keyed by authority ID.
+   */
+  protected $regulatoryFunctions = [];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, StateInterface $state) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration, $state);
+
+    $this->collectRegulatoryFunctions();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration = NULL) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $migration,
+      $container->get('state')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -37,6 +68,24 @@ class ParAdvice extends SqlBase {
       ]);
 
     return $query;
+  }
+
+  protected function collectRegulatoryFunctions() {
+    $result = $this->select('par_advice_regulatory_functions', 'r')
+      ->fields('r', [
+        'advice_regulatory_function_id',
+        'advice_id',
+        'regulatory_function_id',
+      ])
+      ->isNotNull('r.advice_id')
+      ->orderBy('r.advice_id')
+      ->execute();
+
+    while ($row = $result->fetchAssoc()) {
+      $this->regulatoryFunctions[$row['advice_id']][] = [
+        'target_id' => (int) $row['regulatory_function_id'],
+      ];
+    }
   }
 
   /**
@@ -71,7 +120,7 @@ class ParAdvice extends SqlBase {
   }
 
   /**
-   * Attaches "nid" property to a row if row "bid" points to a
+   * Adds the lookuped regulatory functions.
    *
    * @param \Drupal\migrate\Row $row
    *
@@ -79,6 +128,11 @@ class ParAdvice extends SqlBase {
    * @throws \Exception
    */
   function prepareRow(Row $row) {
+    $advice = $row->getSourceProperty('advice_id');
+
+    $regulatory_functions = array_key_exists($advice, $this->regulatoryFunctions) ? $this->regulatoryFunctions[$advice] : [];
+    $row->setSourceProperty('regulatory_functions', $regulatory_functions);
+
     return parent::prepareRow($row);
   }
 
