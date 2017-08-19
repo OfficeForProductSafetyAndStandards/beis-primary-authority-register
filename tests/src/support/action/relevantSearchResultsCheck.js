11f839d66e02891859a99db10ac9adf8a5964550
jaffamonkey <paullittlebury@gmail.com>
2017-08-19 12:15:00 +0100

    Added search results check step

--- tests/src/support/action/relevantSearchResultsCheck.js
+++ tests/src/support/action/relevantSearchResultsCheck.js
@@ -8,12 +8,15 @@ module.exports = (done) => {
      * The command to perform on the browser object (addValue or setValue)
      * @type {String}
      */
-    browser.setViewportSize({
-        width: 1024,
-        height: 768,
+    // browser.setViewportSize({
+    //     width: 1024,
+    //     height: 768,
+    // });
+    let doneCallback = done;
+    browser.url('/dv/partnership-dashboard?partnership_status=1');
+    browser.getText('.table-scroll-wrapper', function(err, text) {
+        doneCallback = 'Awaiting Reviuew';
+        expect(text).to.contain('Awaiting Reviuew');
+        doneCallback();
     });
-    browser.selectByValue('#edit-partnership-status', '1');
-    browser.click('#edit-submit-par-data-transition-journey-1-step-1');
-    browser.click('td.views-field.views-field-nothing a');
-    done();
 };
