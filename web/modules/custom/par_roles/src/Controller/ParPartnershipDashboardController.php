37066493eaecb43db2e5b2272750411af5a2e3f1
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-08-22 08:57:52 +0100

    Updated membership checks.

--- web/modules/custom/par_roles/src/Controller/ParPartnershipDashboardController.php
+++ web/modules/custom/par_roles/src/Controller/ParPartnershipDashboardController.php
@@ -25,13 +25,23 @@ class ParPartnershipDashboardController extends ControllerBase {
   protected $flowStorage;
 
   /**
+   * The PAR data manager for acting upon PAR Data.
+   *
+   * @var \Drupal\par_data\ParDataManagerInterface
+   */
+  protected $parDataManager;
+
+  /**
    * Constructs a \Drupal\par_flows\Form\ParBaseForm.
    *
    * @param \Drupal\Core\Config\Entity\ConfigEntityStorageInterface $flow_storage
    *   The flow entity storage handler.
+   * @param \Drupal\par_data\ParDataManagerInterface $par_data_manager
+   *   The par data manager.
    */
-  public function __construct(ConfigEntityStorageInterface $flow_storage) {
+  public function __construct(ConfigEntityStorageInterface $flow_storage, ParDataManagerInterface $par_data_manager) {
     $this->flowStorage = $flow_storage;
+    $this->parDataManager = $par_data_manager;
   }
 
   /**
@@ -40,50 +50,49 @@ class ParPartnershipDashboardController extends ControllerBase {
   public static function create(ContainerInterface $container) {
     $entity_manager = $container->get('entity.manager');
     return new static(
-      $entity_manager->getStorage('par_flow')
+      $entity_manager->getStorage('par_flow'),
+      $container->get('par_data.manager')
     );
+  }
+
+  /**
+   * Returns the PAR data manager.
+   *
+   * @return \Drupal\par_data\ParDataManagerInterface
+   *   Get the logger channel to use.
+   */
+  public function getParDataManager() {
+    return $this->parDataManager;
   }
 
   /**
    * {@inheritdoc}
    */
   public function content(ParDataPartnership $par_data_partnership = NULL) {
-
-    $current_user_people = NULL;
-
     // We need to find out what kind of membership
     // the current user has to this partnership.
     if ($this->currentUser()->isAuthenticated()) {
-      $current_user_people = \Drupal::entityTypeManager()
-        ->getStorage('par_data_person')
-        ->loadByProperties(['email' => $this->currentUser()->getEmail()]);
-    }
+      $account = User::Load($this->currentUser()->id());
 
-    // We can use this information to figure out what journey to send the user down.
-    if ($current_user_people) {
-      foreach ($current_user_people as $person) {
-        if ($par_data_partnership->isAuthorityMember($person) && !isset($route)) {
-          // Get the start of an authority journey.
-          $route = $this->flowStorage->load('transition_partnership_details')->getRouteByStep(2);
-        }
-        elseif ($par_data_partnership->isOrganisationMember($person) && !isset($route)) {
-          $par_data_organisation = current($par_data_partnership->getOrganisation());
-          switch ($par_data_organisation->getType()) {
-            case 'coordinator':
-              $route = $this->flowStorage->load('transition_coordinator')->getRouteByStep(2);
-              break;
-            case 'business':
-              $route = $this->flowStorage->load('transition_business')->getRouteByStep(2);
-              break;
-          }
-
-        }
+      $par_data_manager = $this->getParDataManager();
+      // Add the correct roles to this user account also.
+      if ($par_data_manager->isMemberOfAuthority($account) && $account->hasRole('par_authority')) {
+        // Get the start of an authority journey.
+        $route = $this->flowStorage->load('transition_partnership_details')->getRouteByStep(2);
+      }
+      else if ($par_data_manager->isMemberOfCoordinator($account) && $account->hasRole('par_coordinator')) {
+        // Get the start of an authority journey.
+        $route = $this->flowStorage->load('transition_coordinator')->getRouteByStep(2);
+      }
+      else if ($par_data_manager->isMemberOfBusiness($account) && $account->hasRole('par_business')) {
+        // Get the start of an authority journey.
+        $route = $this->flowStorage->load('transition_business')->getRouteByStep(2);
       }
     }
 
     if (!isset($route)) {
       $route = 'view.par_data_transition_journey_1_step_1.dv_journey_1_step_1';
-      drupal_set_message(t("We couldn't figure out where to take you because you don't seem to be a member of this partnership."), 'error');
+      drupal_set_message(t("You don't seem to be a part of this partnership, please contact the Help Desk if this is in error."), 'error');
     }
 
     return $this->redirect($route, $this->getRouteParams());
