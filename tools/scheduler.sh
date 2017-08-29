71bce851d71984ae007db11c5e30c9305fef96e3
Chris Moreton <chris@netsensia.com>
2017-08-29 11:55:11 +0100

    Add scheduler daemon

---
+++ tools/scheduler.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#############################################################
+# This script will run the Drupal scheduler every 60 seconds
+# It should be initiated as a task using
+#
+# cf run-task par-beta-$ENV "tools/scheduler.sh"
+#############################################################
+
+while true
+do
+    if [ $APP_ENV == "production" ]; then
+        URL="https://primary-authority.beis.gov.uk"
+    else
+        URL="https://par-beta-$APP_ENV.cloudapps.digital"
+    fi
+
+    URL+="/admin/config/system/cron"
+    wget $URL
+    sleep 60
+done
