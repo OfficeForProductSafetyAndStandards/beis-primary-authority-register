71fcd1782c1f2b6f6e52d6251207fe6f506ab255
Chris Moreton <chris@netsensia.com>
2017-08-29 13:02:58 +0100

    Call Drupal directly rather than URL for cron

--- tools/scheduler.sh
+++ tools/scheduler.sh
@@ -9,13 +9,7 @@
 
 while true
 do
-    if [ $APP_ENV == "production" ]; then
-        URL="https://primary-authority.beis.gov.uk"
-    else
-        URL="https://par-beta-$APP_ENV.cloudapps.digital"
-    fi
-
-    URL+="/admin/config/system/cron"
-    wget $URL
+    cd /home/vcap/app/web
+    ../vendor/drush/drush/drush
     sleep 60
 done
