2fd48cb0bf0204ee9644b4d2b14c76bcfdf46cc8
jaffamonkey <paullittlebury@gmail.com>
2017-08-30 13:17:48 +0100

    Completed search result checker

--- tests/src/support/action/relevantSearchResultsCheck.js
+++ tests/src/support/action/relevantSearchResultsCheck.js
@@ -1,25 +1,24 @@
 /**
  * Check if the given elements text is the same as the given text
  * @param  {Function} done          Function to execute when finished
- * @element  {Function} done          Function to execute when finished
+ * @param  {String}   keyword       The search keyword
  */
 
-module.exports = (done) => {
+module.exports = (keyword, done) => {
     /**
      * The command to perform on the browser object (addValue or setValue)
      * @type {String}
      */
-    // browser.setViewportSize({
-    //     width: 1024,
-    //     height: 768,
-    // });
-    const q = require('q');
-    const checks = [];
-    browser.url('/dv/rd-dashboard?keywords=sale&partnership_status=All');
-    browser.elements('tbody tr').getText().then(function (res){
-        res.value.forEach(function (text) {
-            expect(text).to.contain('sale');
-        });
-        return q.all(checks);
+        // browser.setViewportSize({
+        //     width: 1024,
+        //     height: 768,
+        // });
+    browser.setValue('#edit-keywords', keyword);
+    browser.click('#edit-submit-par-data-transition-journey-1-step-1');
+    const links = $$('td.views-field-nothing');
+    links.forEach(function (link) {
+        let elem = link.getText();
+        expect(elem).to.contain(keyword);
     });
+    done();
 };
