fa3d59e6f3d69de9b4b4af0459774933b5058f97
stephenb01 <stephen.blinkhorne@talquin.com>
2017-09-01 09:18:32 +0100

    PAR-538: WIP - Added new contact form for the authority journey.

--- web/modules/features/par_partnership_flows/src/Controller/ParPartnershipFlowsInspectionPlanListController.php
+++ web/modules/features/par_partnership_flows/src/Controller/ParPartnershipFlowsInspectionPlanListController.php
@@ -18,6 +18,40 @@ class ParPartnershipFlowsInspectionPlanListController extends ParBaseController 
    */
   public function content(ParDataPartnership $par_data_partnership = NULL) {
 
+    $build['documentation_list'] = [
+      '#theme' => 'table',
+      '#attributes' => ['class' => ['form-group']],
+      '#title' => 'Advice documentation',
+      '#header' => [
+        'Document',
+        'Status',
+        'Consulted National Regulator',
+        'Approved RD Executive',
+      ],
+      '#empty' => $this->t("There is no documentation for this partnership."),
+    ];
+
+    // Get each Advice document and add as a table row.
+    foreach ($par_data_partnership->getInspectionPlan() as $inspection_plan) {
+      dump($inspection_plan);
+      $inspection_plan_view_builder = $this->getParDataManager()->getViewBuilder('par_data_inspection_plan');
+
+      // The first column contains a rendered summary of the document.
+      $inspection_plan_summary = $inspection_plan_view_builder->view($inspection_plan, 'summary');
+
+      if ($inspection_plan_summary) {
+        $build['documentation_list']['#rows'][] = [
+          'data' => [
+            'document' => $this->getRenderer()->render($inspection_plan_summary),
+            'status' => $inspection_plan->retrieveStringValue('inspection_status'),
+            'consulted' => $inspection_plan->retrieveStringValue('consulted_national_regulator'),
+            'approved' => $inspection_plan->retrieveStringValue('approved_rd_executive'),
+          ],
+        ];
+      }
+
+    }
+
     $build['save'] = [
       '#type' => 'markup',
       '#markup' => t('@link', [
