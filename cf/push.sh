35bb60bc53e1a34a1b18a225291ac489376864bb
Chris Moreton <chris@netsensia.com>
2017-09-05 14:07:39 +0100

    README for bringing up new environment

--- cf/push.sh
+++ cf/push.sh
@@ -68,17 +68,20 @@ command -v cf >/dev/null 2>&1 || {
 
 echo -n "Enter the environment name (e.g. staging): "
 read ENV
-echo -n "Enter the build version (e.g. v1.0.0): "
-read VER
-echo -n "Number of instances: "
-read INSTANCES
 
-if [ $ENV == "production" ]; then
-   echo -n "You have chosen to deploy to production. Are you sure? [sure|no] : "
-   read SURE
-   if [ $SURE != "sure" ]; then
-       exit 0
-   fi
+if [[ $1 != "environment-only" ]]; then
+    echo -n "Enter the build version (e.g. v1.0.0): "
+    read VER
+    echo -n "Number of instances: "
+    read INSTANCES
+    
+    if [ $ENV == "production" ]; then
+       echo -n "You have chosen to deploy to production. Are you sure? [sure|no] : "
+       read SURE
+       if [ $SURE != "sure" ]; then
+           exit 0
+       fi
+    fi
 fi
 
 ####################################################################################
@@ -127,84 +130,100 @@ SENTRY_DSN_PUBLIC=`vault read -field=SENTRY_DSN_PUBLIC secret/par/env/$ENV`
 echo "Resealing the vault.."
 vault seal
 
-####################################################################################
-# Pull the packaged version from S3
-####################################################################################
+if [[ $1 != "environment-only" ]]; then
 
-echo "Pulling version $VER"
-
-####################################################################################
-# We are in the /cf directory
-# Pull the package to the build directory
-####################################################################################
-
-BUILD_DIR=build-$ENV
-rm -rf $BUILD_DIR
-mkdir $BUILD_DIR
-
-cd $BUILD_DIR
-
-aws s3 cp s3://transform-par-beta-artifacts/builds/$VER.tar.gz .
-
-####################################################################################
-# Check that we got the package
-####################################################################################
-
-if [ ! -f $VER.tar.gz ]; then
-   exit
+    ####################################################################################
+    # Pull the packaged version from S3
+    ####################################################################################
+    
+    echo "Pulling version $VER"
+    
+    ####################################################################################
+    # We are in the /cf directory
+    # Pull the package to the build directory
+    ####################################################################################
+    
+    BUILD_DIR=build-$ENV
+    rm -rf $BUILD_DIR
+    mkdir $BUILD_DIR
+    
+    cd $BUILD_DIR
+    
+    aws s3 cp s3://transform-par-beta-artifacts/builds/$VER.tar.gz .
+    
+    ####################################################################################
+    # Check that we got the package
+    ####################################################################################
+    
+    if [ ! -f $VER.tar.gz ]; then
+       exit
+    fi
+    
+    ####################################################################################
+    # Unpack and remove package file
+    ####################################################################################
+    
+    tar -zxvf $VER.tar.gz
+    rm $VER.tar.gz
+    
+    ####################################################################################
+    # Stay in the build directory to push the unpacked code
+    ####################################################################################
+    
+    if [ ! -f cf/manifests/manifest.$ENV.yml ]; then
+        echo "Manifest file cf/manifests/manifest.$ENV.yml not found"
+        exit 1
+    fi
+    
+    cf push -f cf/manifests/manifest.$ENV.yml --hostname par-beta-$ENV-green par-beta-$ENV-green 
 fi
-
-####################################################################################
-# Unpack and remove package file
-####################################################################################
-
-tar -zxvf $VER.tar.gz
-rm $VER.tar.gz
-
-####################################################################################
-# Stay in the build directory to push the unpacked code
-####################################################################################
-
-if [ ! -f cf/manifests/manifest.$ENV.yml ]; then
-    echo "Manifest file cf/manifests/manifest.$ENV.yml not found"
-    exit 1
-fi
-
-cf push -f cf/manifests/manifest.$ENV.yml --hostname par-beta-$ENV-green par-beta-$ENV-green 
 
 ####################################################################################
 # Set environment variables
 ####################################################################################
 
-cf set-env par-beta-$ENV-green S3_ACCESS_KEY $S3_ACCESS_KEY
-cf set-env par-beta-$ENV-green S3_SECRET_KEY $S3_SECRET_KEY
-cf set-env par-beta-$ENV-green PAR_HASH_SALT $PAR_HASH_SALT
-cf set-env par-beta-$ENV-green S3_BUCKET_PUBLIC $S3_BUCKET_PUBLIC
-cf set-env par-beta-$ENV-green S3_BUCKET_PRIVATE $S3_BUCKET_PRIVATE
-cf set-env par-beta-$ENV-green S3_BUCKET_ARTIFACTS $S3_BUCKET_ARTIFACTS
-cf set-env par-beta-$ENV-green APP_ENV $ENV
-cf set-env par-beta-$ENV-green PAR_GOVUK_NOTIFY_KEY $PAR_GOVUK_NOTIFY_KEY
-cf set-env par-beta-$ENV-green PAR_GOVUK_NOTIFY_TEMPLATE $PAR_GOVUK_NOTIFY_TEMPLATE
-cf set-env par-beta-$ENV-green CLAMAV_HTTP_PASS $CLAMAV_HTTP_PASS
-cf set-env par-beta-$ENV-green CLAMAV_HTTP_USER $CLAMAV_HTTP_USER
-cf set-env par-beta-$ENV-green SENTRY_DSN $SENTRY_DSN
-cf set-env par-beta-$ENV-green SENTRY_DSN_PUBLIC $SENTRY_DSN_PUBLIC
+if [[ $1 == "environment-only" ]]; then
+    TARGET_ENV=par-beta-$ENV
+else
+    TARGET_ENV=par-beta-$ENV-green
+fi
 
-cf restage par-beta-$ENV-green
+cf set-env $TARGET_ENV S3_ACCESS_KEY $S3_ACCESS_KEY
+cf set-env $TARGET_ENV S3_SECRET_KEY $S3_SECRET_KEY
+cf set-env $TARGET_ENV PAR_HASH_SALT $PAR_HASH_SALT
+cf set-env $TARGET_ENV S3_BUCKET_PUBLIC $S3_BUCKET_PUBLIC
+cf set-env $TARGET_ENV S3_BUCKET_PRIVATE $S3_BUCKET_PRIVATE
+cf set-env $TARGET_ENV S3_BUCKET_ARTIFACTS $S3_BUCKET_ARTIFACTS
+cf set-env $TARGET_ENV APP_ENV $ENV
+cf set-env $TARGET_ENV PAR_GOVUK_NOTIFY_KEY $PAR_GOVUK_NOTIFY_KEY
+cf set-env $TARGET_ENV PAR_GOVUK_NOTIFY_TEMPLATE $PAR_GOVUK_NOTIFY_TEMPLATE
+cf set-env $TARGET_ENV CLAMAV_HTTP_PASS $CLAMAV_HTTP_PASS
+cf set-env $TARGET_ENV CLAMAV_HTTP_USER $CLAMAV_HTTP_USER
+cf set-env $TARGET_ENV SENTRY_DSN $SENTRY_DSN
+cf set-env $TARGET_ENV SENTRY_DSN_PUBLIC $SENTRY_DSN_PUBLIC
 
-if [[ $1 != "skip-post-deploy" ]]; then
+cf restage $TARGET_ENV
+
+if [[ $1 != "environment-only" ]]; then
+
     cf ssh par-beta-$ENV-green -c "cd app/tools && python post_deploy.py"
     
     ####################################################################################
     # Blue/Green magic - switch domain routes to newly-deployed app
     ####################################################################################
     
+    if [[ $ENV == "production" ]]; then
+        CDN_DOMAIN="primary-authority.beis.gov.uk"
+    else
+        CDN_DOMAIN=$ENV-cdn.par-beta.co.uk
+    fi
+    
     cf map-route par-beta-$ENV-green cloudapps.digital -n par-beta-$ENV
     cf unmap-route par-beta-$ENV cloudapps.digital -n par-beta-$ENV
-    cf map-route par-beta-$ENV-green $ENV-cdn.par-beta.co.uk
-    cf unmap-route par-beta-$ENV $ENV-cdn.par-beta.co.uk
+    cf map-route par-beta-$ENV-green $CDN_DOMAIN
+    cf unmap-route par-beta-$ENV $CDN_DOMAIN
     cf delete par-beta-$ENV -f
     cf rename par-beta-$ENV-green par-beta-$ENV
+    
+    cf scale par-beta-$ENV -i $INSTANCES
 fi
-
-cf scale par-beta-$ENV -i $INSTANCES
