e9c926494f9554e4b8fa9b5437af9f7c17cb7e0e
jaffamonkey <paullittlebury@gmail.com>
2017-09-06 17:02:23 +0100

    Correct build conf

--- tests/wdio.BUILD.conf.js
+++ tests/wdio.BUILD.conf.js
@@ -10,7 +10,7 @@ config.capabilities = [{
 
 config.screenshotPath = './errorShots/';
 config.services = ['selenium-standalone'];
-config.baseUrl = 'http://127.0.0.1:8111';
+config.baseUrl = 'http://127.0.0.1:80';
 config.tags = '@ci, ~@Pending, ~@Bug, ~@userjourney1';
 config.cucumberOpts.failFast = true;
 exports.config = config;
