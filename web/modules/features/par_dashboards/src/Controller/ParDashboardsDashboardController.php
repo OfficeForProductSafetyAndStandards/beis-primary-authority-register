44d4901c507f8f1e9e52e4172051e8e43d58bf2e
stephenb01 <stephen.blinkhorne@talquin.com>
2017-09-07 02:09:30 +0100

    PAR-335: Fixed config issue.

--- web/modules/features/par_dashboards/src/Controller/ParDashboardsDashboardController.php
+++ web/modules/features/par_dashboards/src/Controller/ParDashboardsDashboardController.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\par_dashboards\Controller;
 
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 use Drupal\par_flows\Controller\ParBaseController;
+use Drupal\user\Entity\User;
 
 /**
  * A controller for all PAR Flow Transition pages.
@@ -18,6 +21,30 @@ class ParDashboardsDashboardController extends ParBaseController {
       '#type' => 'markup',
       '#markup' => $this->t('Intro to new dashboard'),
     ];
+
+    // Need to see what permissions the user has so we can display the correct
+    // links.
+    // bypass partnership journey,
+    // authority partnership journey,
+    // business partnership journey,
+    // coordinator partnership journey
+    // Also need to see if there are any other links based on partnerships to
+    // be displayed.
+    $account = User::load(\Drupal::currentUser()->id());
+
+    if ($account->hasPermission('bypass partnership journey')) {
+      $build['partnerships'] = [
+        '#type' => 'fieldset',
+        '#attributes' => ['class' => 'form-group'],
+        '#collapsible' => FALSE,
+        '#collapsed' => FALSE,
+      ];
+
+      $build['partnerships']['link'] = [
+        '#type' => 'markup',
+        '#markup' => t('<a href="/partnerships">Partnership list</a>'),
+      ];
+    }
 
     return parent::build($build);
 
