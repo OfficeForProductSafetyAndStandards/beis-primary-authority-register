f6ce480badec93e7affd0e978e564e621c95b9f5
jaffamonkey <paullittlebury@gmail.com>
2017-09-07 15:00:11 +0100

    reverted browserstack creds

--- tests/wdio.BROWSERSTACK.conf.js
+++ tests/wdio.BROWSERSTACK.conf.js
@@ -64,8 +64,8 @@ config.capabilities = [{
 ];
 
 config.services = ['browserstack'];
-config.user = USER;
-config.key = KEY;
+-config.user = process.env.BSK_USER;
+-config.key = process.env.BSK_KEY;
 config.browserstackLocal = true;
 config.host = 'hub-cloud.browserstack.com';
 config.baseUrl = 'http://localhost:8111';
