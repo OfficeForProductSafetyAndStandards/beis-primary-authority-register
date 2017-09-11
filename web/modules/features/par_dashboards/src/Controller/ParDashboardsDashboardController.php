ee0b1e6a72b03a069dd2e454edf1547f4e0fbab1
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-09-11 13:23:05 +0100

    PAR-335: Updated dashbaord view.

--- web/modules/features/par_dashboards/src/Controller/ParDashboardsDashboardController.php
+++ web/modules/features/par_dashboards/src/Controller/ParDashboardsDashboardController.php
@@ -12,157 +12,111 @@ use Drupal\Core\Session\AccountProxyInterface;
 class ParDashboardsDashboardController extends ParBaseController {
 
   /**
+   * Current user account accessing the dashboard
+   */
+  protected $account;
+
+  /**
+   * Set the current user account.
+   */
+  public function setCurrentUser() {
+    $this->account = User::load(\Drupal::currentUser()->id());
+  }
+
+  /**
+   * Get the current user account.
+   */
+  public function getUserAccount() {
+    return $this->account;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function content() {
-    $account = User::load(\Drupal::currentUser()->id());
+    $this->setCurrentUser();
     $build = [];
 
-    if ($account->hasPermission('manage my authorities')) {
-      $this->buildAuthority($build, $account);
+    // Your partnerships.
+    $partnerships =  $this->getParDataManager()->hasMembershipsByType($this->getUserAccount(), 'par_data_partnership');
+    $can_manage_partnerships = $this->getUserAccount()->hasPermission('manage my organisations') || $this->getUserAccount()->hasPermission('manage my authorities');
+    $can_create_partnerships = $this->getUserAccount()->hasPermission('apply for partnership');
+    if (($partnerships && $can_manage_partnerships) || $can_create_partnerships) {
+      $build['partnerships'] = [
+        '#type' => 'fieldset',
+        '#title' => $this->t('Your partnerships'),
+        '#attributes' => ['class' => 'form-group'],
+        '#collapsible' => FALSE,
+        '#collapsed' => FALSE,
+      ];
+
+      // Manage my partnerships link.
+      if ($partnerships) {
+        $manage_my_partnerships = $this->getLinkByRoute('view.par_user_partnerships.partnerships_page');
+        $build['partnerships']['see'] = [
+          '#type' => 'markup',
+          '#markup' => $manage_my_partnerships->setText('See your partnerships')->toString(),
+        ];
+      }
+
+      // Create partnerships link.
+      if ($can_create_partnerships) {
+        $create_partnerships = $this->getLinkByRoute('view.par_user_partnerships.partnerships_page');
+        $build['partnerships']['add'] = [
+          '#type' => 'markup',
+          '#markup' => $create_partnerships->setText('Create a new partnership (TBC:need link)')->toString(),
+        ];
+      }
     }
 
-    if ($account->hasPermission('manage my organisations')) {
-      $this->buildOrganisation($build, $account);
-    }
-
-    return parent::build($build);
-  }
-
-  /**
-   * Build the page for an authority.
-   *
-   * @param array $build
-   *   Referenced array with the current build structure.
-   * @param \Drupal\user\Entity\User $account
-   *   Details of the current user.
-   */
-  private function buildAuthority(array &$build, User $account) {
-    // Need to get the authority the user belongs to.
-    $par_data_manager = \Drupal::service('par_data.manager');
-    $memberships = $par_data_manager->hasMemberships($account, TRUE);
-
-    $build['details_intro'] = [
-      '#type' => 'markup',
-      '#markup' => $this->t('Primary Authority Partner:'),
-    ];
-
-    if (!empty($memberships['par_data_authority'])) {
-      $par_data_authority = current($memberships['par_data_authority']);
-
-      $authority_builder = $this->getParDataManager()
-        ->getViewBuilder('par_data_authority');
-
-      $authority_name = $authority_builder->view($par_data_authority, 'title');
-      $authority_name['#prefix'] = '<h1>';
-      $authority_name['#suffix'] = '</h1>';
-
-      $build['authority_name'] = $this->renderMarkupField($authority_name);
-    }
-    else {
-      $build['authority_name'] = [
+    // Applications, partnerships that need completion.
+    // @TODO NEED A WAY TO GET INCOMPLETE APLICATIONS (WE DO NOT RECORD THIS YET)
+    if ($this->getUserAccount()->hasPermission('complete partnership organisation details')) {
+      $build['applications'] = [
+        '#type' => 'fieldset',
+        '#title' => $this->t('Applications'),
+        '#attributes' => ['class' => 'form-group'],
+        '#collapsible' => FALSE,
+        '#collapsed' => FALSE,
+      ];
+      $manage_my_partnerships = $this->getLinkByRoute('view.par_user_partnerships.partnerships_page');
+      $build['applications']['see'] = [
         '#type' => 'markup',
-        '#markup' => $this->t('(none)'),
+        '#markup' => $manage_my_partnerships->setText('See my pending partnerships (TBC:need link)')->toString(),
       ];
     }
 
-    $build['partnerships'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Your partnerships'),
-      '#attributes' => ['class' => 'form-group'],
-      '#collapsible' => FALSE,
-      '#collapsed' => FALSE,
-    ];
+    // Partnerships search link.
+    if ($this->getUserAccount()->hasPermission('search partnership')) {
+      $build['partnerships_find'] = [
+        '#type' => 'fieldset',
+        '#title' => $this->t('Find a partnership'),
+        '#attributes' => ['class' => 'form-group'],
+        '#collapsible' => FALSE,
+        '#collapsed' => FALSE,
+      ];
 
-    $build['partnerships']['see'] = [
-      '#type' => 'markup',
-      '#markup' => $this->t('<a href="/partnerships">See all partnerships</a><br>'),
-    ];
-
-    $build['partnerships']['add'] = [
-      '#type' => 'markup',
-      '#markup' => $this->t('<a href="/partnerships">Create a new partnership (TBC:need link)</a>'),
-    ];
-
-    $build['partnerships_find'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Find a partnership'),
-      '#attributes' => ['class' => 'form-group'],
-      '#collapsible' => FALSE,
-      '#collapsed' => FALSE,
-    ];
-
-    $build['partnerships_find']['link'] = [
-      '#type' => 'markup',
-      '#markup' => $this->t('<a href="/partnerships/search">Search for a partnership</a>'),
-    ];
-
-    $build['enforcement'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Enforcement notices'),
-      '#attributes' => ['class' => 'form-group'],
-      '#collapsible' => FALSE,
-      '#collapsed' => FALSE,
-    ];
-  }
-
-  /**
-   * Build the page for an organisation.
-   *
-   * @param array $build
-   *   Referenced array with the current build structure.
-   * @param \Drupal\user\Entity\User $account
-   *   Details of the current user.
-   */
-  private function buildOrganisation(array &$build, User $account) {
-    $par_data_manager = \Drupal::service('par_data.manager');
-    $memberships = $par_data_manager->hasMemberships($account, TRUE);
-
-    $build['details_intro'] = [
-      '#type' => 'markup',
-      '#markup' => $this->t('Primary Authority Organisation:'),
-    ];
-
-    if (!empty($memberships['par_data_organisation'])) {
-      $par_data_organisation = current($memberships['par_data_organisation']);
-
-      $organisation_builder = $this->getParDataManager()
-        ->getViewBuilder('par_data_organisation');
-
-      $organisation_name = $organisation_builder->view($par_data_organisation, 'name');
-      $organisation_name['#prefix'] = '<h1>';
-      $organisation_name['#suffix'] = '</h1>';
-
-      $build['business_name'] = $this->renderMarkupField($organisation_name);
+      $search_partnerships = $this->getLinkByRoute('view.partnership_search.par_partnership_search');
+      $build['partnerships_find']['link'] = [
+        '#type' => 'markup',
+        '#markup' => $search_partnerships->setText('Search for a partnership')->toString(),
+      ];
     }
 
-    $build['partnerships'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Your partnerships'),
-      '#attributes' => ['class' => 'form-group'],
-      '#collapsible' => FALSE,
-      '#collapsed' => FALSE,
-    ];
+    // Enforcement notices that need attention.
+    if ($this->getUserAccount()->hasPermission('enforce organisation')) {
+      $build['enforcement'] = [
+        '#type' => 'fieldset',
+        '#title' => $this->t('Enforcement notices'),
+        '#attributes' => ['class' => 'form-group'],
+        '#collapsible' => FALSE,
+        '#collapsed' => FALSE,
+      ];
 
-    $build['partnerships']['see'] = [
-      '#type' => 'markup',
-      '#markup' => $this->t('<a href="/partnerships">See all partnerships</a><br>'),
-    ];
+      // @TODO TO BE ADDED. MUST DISCUSS, DO WE NEED A SEPARATE VIEW PAGE FOR MANAGING ALL NOTICES.
+    }
 
-    $build['applications'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Applications'),
-      '#attributes' => ['class' => 'form-group'],
-      '#collapsible' => FALSE,
-      '#collapsed' => FALSE,
-    ];
-
-    // @todo update this to become a flow link.
-    $build['applications']['see'] = [
-      '#type' => 'markup',
-      '#markup' => $this->t('<a href="/partnerships">See my pending partnerships (TBC:need link)</a><br>'),
-    ];
-
+    return parent::build($build);
   }
 
 }
