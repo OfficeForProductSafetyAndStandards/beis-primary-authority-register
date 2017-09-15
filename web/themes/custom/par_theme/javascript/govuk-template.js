d75222c752ac6464fe40d1ec6e8f25eaf8fe98e4
Stephen Mount <stemount@gmail.com>
2017-09-15 12:37:55 +0100

    Menu links toggle on mobile.

--- web/themes/custom/par_theme/javascript/govuk-template.js
+++ web/themes/custom/par_theme/javascript/govuk-template.js
@@ -77,7 +77,8 @@
 }).call(this);
 
 (function() {
-  "use strict"
+
+  'use strict';
 
   // add cookie message
   if (window.GOVUK && GOVUK.addCookieMessage) {
@@ -86,24 +87,27 @@
 
   // header navigation toggle
   if (document.querySelectorAll && document.addEventListener){
-    var els = document.querySelectorAll('.js-header-toggle'),
-        i, _i;
-    for(i=0,_i=els.length; i<_i; i++){
-      els[i].addEventListener('click', function(e){
-        e.preventDefault();
-        var target = document.getElementById(this.getAttribute('href').substr(1)),
-            targetClass = target.getAttribute('class') || '',
-            sourceClass = this.getAttribute('class') || '';
+    var els = document.querySelectorAll('.js-header-toggle');
+    var i;
+    var _i;
 
-        if(targetClass.indexOf('js-visible') !== -1){
+    for (i = 0, _i = els.length; i < _i; i++) {
+
+      els[i].addEventListener('click', function (e) {
+        e.preventDefault();
+
+        var target = document.getElementById(this.getAttribute('href').substr(1));
+        var targetClass = target.getAttribute('class') || '';
+        var sourceClass = this.getAttribute('class') || '';
+
+        if (targetClass.indexOf('js-visible') !== -1) {
           target.setAttribute('class', targetClass.replace(/(^|\s)js-visible(\s|$)/, ''));
-        } else {
+        }
+        else {
           target.setAttribute('class', targetClass + " js-visible");
         }
-        if(sourceClass.indexOf('js-hidden') !== -1){
+        if (sourceClass.indexOf('js-hidden') !== -1) {
           this.setAttribute('class', sourceClass.replace(/(^|\s)js-hidden(\s|$)/, ''));
-        } else {
-          this.setAttribute('class', sourceClass + " js-hidden");
         }
       });
     }
