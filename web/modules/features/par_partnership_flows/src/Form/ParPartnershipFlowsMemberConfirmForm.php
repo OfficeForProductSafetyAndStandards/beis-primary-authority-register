0a8524fb3d5d3ee149003c219ef50be1a91d61a2
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-09-21 10:38:55 +0100

    Migration: Added default value to membership selection.

--- web/modules/features/par_partnership_flows/src/Form/ParPartnershipFlowsMemberConfirmForm.php
+++ web/modules/features/par_partnership_flows/src/Form/ParPartnershipFlowsMemberConfirmForm.php
@@ -199,6 +199,7 @@ class ParPartnershipFlowsMemberConfirmForm extends ParBaseForm {
             '#title' => t('A match was found for @name', ['@name' => $name]),
             '#description' => t('Did you mean any of these organisations?'),
             '#options' => $radio_options + ['new' => "Add as a new organisation."],
+            '#default_value' => current(array_keys($radio_options)),
             '#required' => TRUE,
           ];
         }
