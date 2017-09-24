41d6d79e2f6e183c5ae6ecc1f5ddc4fcd22015e2
Jon Edery <jon.edery@googlmail.com>
2017-09-24 13:38:27 +0100

    form struture updates an validate/create action entity functionality

--- web/modules/features/par_enforcement_flows/src/Form/ParEnforcementAddActionForm.php
+++ web/modules/features/par_enforcement_flows/src/Form/ParEnforcementAddActionForm.php
@@ -59,12 +59,35 @@ class ParEnforcementAddActionForm extends ParBaseForm {
       '#default_value' => $this->getDefaultValues('details'),
     ];
 
-    $add_file_upload_link = $this->getFlow()->getNextLink('file_upload')->setText('Attach file')->toString();
-    $form['add_upload_files'] = [
+    $enforcement_action_fields = \Drupal::getContainer()->get('entity_field.manager')->getFieldDefinitions('par_data_enforcement_action', 'document');
+    $field_definition = $enforcement_action_fields['document'];
+    $file_extensions = $field_definition->getSetting('file_extensions');
+
+    // Multiple file field.
+    $form['files'] = [
+      '#type' => 'managed_file',
+      '#title' => t('Attach file(s)'),
+      '#description' => t('(document or photo)'),
+      '#upload_location' => 's3private://documents/enforcement_action/',
+      '#multiple' => TRUE,
+      '#default_value' => $this->getDefaultValues("files"),
+      '#upload_validators' => [
+        'file_validate_extensions' => [
+          0 => $file_extensions
+        ]
+      ]
+    ];
+
+    $form['next'] = [
+      '#type' => 'submit',
+      '#name' => 'next',
+      '#value' => $this->t('Continue'),
+    ];
+
+    $cancel_link = $this->getFlow()->getPrevLink('cancel')->setText('Cancel')->toString();
+    $form['cancel'] = [
       '#type' => 'markup',
-      '#markup' => t('@link', ['@link' => $add_file_upload_link]) . ' (document or photo)',
-      '#prefix' => '<div>',
-      '#suffix' => '</div></br>',
+      '#markup' => t('@link', ['@link' => $cancel_link]),
     ];
 
     return parent::buildForm($form, $form_state);
@@ -73,8 +96,47 @@ class ParEnforcementAddActionForm extends ParBaseForm {
   /**
    * {@inheritdoc}
    */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // No validation yet.
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
+
+    $title = $this->getTempDataValue('title_of_action');
+
+    $enforcementAction_data = [
+      'type' => 'enforcement_action',
+      'title' => $title,
+      'details' => $this->getTempDataValue('details'),
+      'enforcement_action_status' => 'PENDING',
+      'enforcement_action_notes' => 'PENDING',
+      'primary_authority_status' => 'PENDING',
+      'primary_authority_notes' => '',
+      'referral_notes' => '',
+      'document' => $this->getDefaultValues("files"),
+      'field_blocked_advice' => [],
+      'field_regulatory_function' => $this->getTempDataValue('regulatory_functions'),
+    ];
+
+    $enforcementAction = \Drupal::entityManager()->getStorage('par_data_enforcement_action')->create($enforcementAction_data);
+
+    if ($enforcementAction->save()) {
+      //$this->deleteStore();
+    }
+    else {
+      $message = $this->t('This %premises could not be saved for %form_id');
+      $replacements = [
+        '%premises' => $title,
+        '%form_id' => $this->getFormId(),
+      ];
+      $this->getLogger($this->getLoggerChannel())
+        ->error($message, $replacements);
+    }
   }
 
 }
