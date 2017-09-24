8ff98983ff1734cc7c4f8a39479a90a3827dbd0b
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-09-24 23:59:15 +0100

    PAR-644: Tidy up of partnership steps.

--- web/modules/features/par_partnership_flows/src/Form/ParPartnershipFlowsApplicationConfirmationForm.php
+++ web/modules/features/par_partnership_flows/src/Form/ParPartnershipFlowsApplicationConfirmationForm.php
@@ -74,32 +74,6 @@ class ParPartnershipFlowsAuthorityDetailsForm extends ParBaseForm {
     // View and perform operations on the information about the business.
     $form['about_business'] = $this->renderSection('About the business', $par_data_organisation, ['comments' => 'about']);
 
-    // Only show SIC Codes and Employee number if the partnership is a direct
-    // partnership.
-    if ($par_data_partnership->isDirect()) {
-      // Add the SIC Codes with the relevant operational links.
-      $form['sic_codes'] = $this->renderSection('SIC Codes', $par_data_organisation, ['field_sic_code' => 'full']);
-
-      // Add the number of employees with a link to edit the field.
-      $form['employee_no'] = $this->renderSection('Number of Employees', $par_data_organisation, ['employees_band' => 'full']);
-    }
-
-    // Only show Members list, Sectors and Number of businesses if the
-    // partnership is a coordinated partnership.
-    if ($par_data_partnership->isCoordinated()) {
-      $form['associations'] = $this->renderSection('Number of Associations', $par_data_organisation, ['size' => 'full']);
-
-      // @TODO We need to add members list and Sectors. TBD Later.
-    }
-
-    // Display all the legal entities along with the links for the allowed
-    // operations on these.
-    $form['legal_entities'] = $this->renderSection('Legal Entities', $par_data_organisation, ['field_legal_entity' => 'summary']);
-
-    // Display all the trading names along with the links for the allowed
-    // operations on these.
-    $form['trading_names'] = $this->renderSection('Trading Names', $par_data_organisation, ['trading_name' => 'full']);
-
     // Everything below is for the authorioty to edit and add to.
     $par_data_authority = current($par_data_partnership->getAuthority());
     $form['authority'] = [
@@ -110,54 +84,10 @@ class ParPartnershipFlowsAuthorityDetailsForm extends ParBaseForm {
     ];
 
     // Display details about the partnership for information.
-    $form['partnership_since'] = $this->renderSection('In partnership since', $par_data_partnership, ['approved_date' => 'full']);
-
-    // Display details about the partnership for information.
-    $form['regulatory_functions'] = $this->renderSection('Partnered for', $par_data_partnership, ['field_regulatory_function' => 'full']);
-
-    // Display details about the partnership for information.
-    $form['about_partnership'] = $this->renderSection('About the partnership', $par_data_partnership, ['about_partnership' => 'about'], ['edit-field']);
-
-    if ($this->getFlowName() !== 'partnership_application') {
-      $form['inspection_plans'] = [
-        '#type' => 'fieldset',
-        '#title' => t('Inspection plans'),
-        '#attributes' => ['class' => 'form-group'],
-        '#collapsible' => FALSE,
-        '#collapsed' => FALSE,
-      ];
-
-      $form['inspection_plans']['link'] = [
-        '#type' => 'markup',
-        '#markup' => t('@link', [
-          '@link' => $this->getFlow()
-            ->getNextLink('inspection_plans')
-            ->setText('See all Inspection Plans')
-            ->toString(),
-        ]),
-      ];
-
-      $form['advice'] = [
-        '#type' => 'fieldset',
-        '#title' => t('Advice and Documents'),
-        '#attributes' => ['class' => 'form-group'],
-        '#collapsible' => FALSE,
-        '#collapsed' => FALSE,
-      ];
-
-      $form['advice']['link'] = [
-        '#type' => 'markup',
-        '#markup' => t('@link', [
-          '@link' => $this->getFlow()
-            ->getNextLink('advice')
-            ->setText('See all Advice')
-            ->toString(),
-        ]),
-      ];
-    }
+    $form['about_partnership'] = $this->renderSection('About the partnership', $par_data_partnership, ['about_partnership' => 'about']);
 
     // Display the authority contacts for information.
-    $form['authority_contacts'] = $this->renderSection('Contacts - Primary Authority', $par_data_partnership, ['field_authority_person' => 'detailed'], ['edit-entity', 'add']);
+    $form['authority_contacts'] = $this->renderSection('Contacts - Primary Authority', $par_data_partnership, ['field_authority_person' => 'detailed']);
 
     // Display all the legal entities along with the links for the allowed
     // operations on these.
