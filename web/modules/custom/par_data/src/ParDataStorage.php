41d5204ab230e6dc6a13383d3169add3c0289a78
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-09-25 21:15:22 +0100

    PAR-681: Added par entity storage class.

--- web/modules/custom/par_data/src/ParDataStorage.php
+++ web/modules/custom/par_data/src/ParDataStorage.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace Drupal\par_flows;
+namespace Drupal\par_data;
 
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Config\Entity\ConfigEntityStorage;
+use Drupal\par_data\Entity\ParDataEntity;
+use Drupal\trance\TranceStorage;
 
 /**
  * Defines the storage class for flows.
@@ -11,7 +13,36 @@ use Drupal\Core\Config\Entity\ConfigEntityStorage;
  * This extends the base storage class adding extra
  * entity loading mechanisms.
  */
-class ParDataStorage extends ConfigEntityStorage {
+class ParDataStorage extends TranceStorage {
 
-  
+  /**
+   * Soft delete all PAR Data entities.
+   *
+   * {@inheritdoc}
+   */
+  public function delete(array $entities) {
+    if (!$entities) {
+      // If no IDs or invalid IDs were passed, do nothing.
+      return;
+    }
+
+    // Ensure that the entities are keyed by ID.
+    $keyed_entities = [];
+    foreach ($entities as $entity) {
+      $keyed_entities[$entity->id()] = $entity;
+    }
+
+    // Perform the delete and reset the static cache for the deleted entities.
+    $this->doDelete($keyed_entities);
+    $this->resetCache(array_keys($keyed_entities));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function doDelete($entities) {
+    foreach ($entities as $id => $entity) {
+      $entity->set(ParDataEntity::DELETE_FIELD, TRUE)->save();
+    }
+  }
 }
