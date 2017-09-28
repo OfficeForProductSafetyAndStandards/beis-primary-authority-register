da0153347ef1eedbe2f507583e94b17d4b2e60bc
Chris Moreton <chris@netsensia.com>
2017-09-28 15:41:34 +0100

    Further work on partnership report

--- tools/partnership_report/partnership_report.php
+++ tools/partnership_report/partnership_report.php
@@ -1,18 +1,44 @@
 <?php
 
-$lines = file('./partnership_report.csv');
+$originalLines = file('./partnership_report.csv');
 
-$totalLines = count($lines);
+$totalLines = count($originalLines);
 
 $colours = ['#ddd', '#fff'];
 
+$lines = [];
+$lastKey = '';
+$legalEntityNames = [];
+$lastCsv = null;
+
+foreach ($originalLines as $line) {
+    $csv = str_getcsv($line);
+    $key = $csv[4] . '_' . $csv[5];
+    
+    if ($key == $lastKey) {
+        $legalEntityNames[] = $csv[6];
+    } else {
+        if ($lastCsv) {
+            $lastCsv[7] = count($legalEntityNames);
+            $lastCsv[6] = implode(PHP_EOL, $legalEntityNames);
+            $lines[] = $lastCsv;
+        }
+        
+        $lastKey = $key;
+        $legalEntityNames = [$csv[6]];
+        $lastCsv = $csv;
+    }
+}
+
 $s = '';
 $lineCount = 0;
+
 foreach ($lines as $line) {
+    
     $stripeColour = $colours[$lineCount++ % 2];
-    $csv = str_getcsv($line);
     $s .= '<tr>';
-    foreach ($csv as $c) {
+    foreach ($line as $c) {
+        $c = str_replace(PHP_EOL, '<br>', $c);
         $s .= '<td style="background-color:' . $stripeColour . '">' . $c . '</td>';
     }
     $s .= '</tr>' . PHP_EOL;
@@ -28,6 +54,12 @@ $o .= '<th>Partnership Status</th>';
 $o .= '<th>Partnership Type</th>';
 $o .= '<th>Primary Authority</th>';
 $o .= '<th>Organisation Name</th>';
+$o .= '<th>Legal Entities</th>';
+$o .= '<th>No. of legal entities (Co-ord)</th>';
+$o .= '<th>Inspection Plan published</th>';
+$o .= '<th>Advice published</th>';
+$o .= '<th>Business sector (Direct)</th>';
+$o .= '<th>Business Size (Direct)</th>';
 $o .= '</tr>';
 $o .= $s . '</table>';
 
--- tools/partnership_report/partnership_report.sql
+++ tools/partnership_report/partnership_report.sql
@@ -5,20 +5,66 @@ COPY(
     par_partnerships_field_data.partnership_status,
     par_partnerships_field_data.partnership_type,
     par_authorities_field_data.authority_name,
-    par_organisations_field_data.organisation_name
+    par_organisations_field_data.organisation_name,
+    par_legal_entities_field_data.registered_name,
+    1,
+    par_inspection_plans_field_data.inspection_status,
+    par_advice_field_data.status,
+    par_sic_codes_field_data.sic_code,
+    par_organisations_field_data.employees_band,
 FROM
     par_partnerships_field_data,
     par_data_partnership__field_authority,
     par_authorities_field_data,
     par_data_partnership__field_organisation,
-    par_organisations_field_data
+    par_organisations_field_data,
+    par_data_organisation__field_legal_entity,
+    par_legal_entities_field_data,
+    par_data_partnership__field_inspection_plan,
+    par_inspection_plans_field_data,
+    par_data_partnership__field_advice,
+    par_advice_field_data,
+    par_data_organisation__field_sic_code,
+    par_sic_codes_field_data
 WHERE
     par_partnerships_field_data.id = par_data_partnership__field_authority.entity_id
 AND
-   par_partnerships_field_data.id = par_data_partnership__field_organisation.entity_id
+    par_partnerships_field_data.id = par_data_partnership__field_organisation.entity_id
 AND
-   par_data_partnership__field_authority.field_authority_target_id = par_authorities_field_data.id
+    par_data_partnership__field_authority.field_authority_target_id = par_authorities_field_data.id
 AND
-   par_data_partnership__field_organisation.field_organisation_target_id = par_organisations_field_data.id
+    par_data_partnership__field_organisation.field_organisation_target_id = par_organisations_field_data.id
+AND
+    par_organisations_field_data.id = par_data_organisation__field_legal_entity.entity_id
+AND
+    par_data_organisation__field_legal_entity.field_legal_entity_target_id = par_legal_entities_field_data.id
+AND
+    par_partnerships_field_data.id = par_data_partnership__field_inspection_plan.entity_id
+AND
+    par_data_partnership__field_inspection_plan.field_inspection_plan_target_id = par_inspection_plans_field_data.id
+AND
+    par_partnerships_field_data.id = par_data_partnership__field_advice.entity_id
+AND
+    par_data_partnership__field_advice.field_advice_target_id = par_advice_field_data.id
+AND
+    par_organisations_field_data.id = par_data_organisation__field_sic_code.entity_id
+AND
+    par_data_organisation__field_sic_code.field_sic_code_target_id = par_sic_codes_field_data.id    
+AND 
+    par_partnerships_field_data.revoked is null AND par_partnerships_field_data.deleted is null
+AND 
+    par_authorities_field_data.revoked is null AND par_authorities_field_data.deleted is null
+AND 
+    par_legal_entities_field_data.revoked is null AND par_legal_entities_field_data.deleted is null    
+AND 
+    par_organisations_field_data.revoked is null AND par_organisations_field_data.deleted is null            
+AND 
+    par_inspection_plans_field_data.revoked is null AND par_inspection_plans_field_data.deleted is null    
+AND 
+    par_advice_field_data.revoked is null AND par_advice_field_data.deleted is null
+ORDER BY
+    par_authorities_field_data.authority_name,
+    par_organisations_field_data.organisation_name,
+    par_legal_entities_field_data.registered_name
 ) To stdout With CSV DELIMITER ',';
 
