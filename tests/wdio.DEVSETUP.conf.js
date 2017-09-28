57d0e7528800a6485f79354b35c7e7e0af0bee5a
jaffamonkey <paullittlebury@gmail.com>
2017-09-28 16:39:14 +0100

    updates

---
+++ tests/wdio.DEVSETUP.conf.js
@@ -0,0 +1,13 @@
+const config = require('./wdio.conf.js').config;
+
+config.capabilities = [{
+    browserName: 'chrome',
+}];
+
+config.screenshotPath = './errorShots/';
+config.services = ['selenium-standalone'];
+config.baseUrl = 'http://127.0.0.1:8111';
+config.tags = '@setup';
+config.cucumberOpts.failFast = true;
+exports.config = config;
+
