01a822a5add2fe53e74b0f06e94e21748570a80b
jaffamonkey <paullittlebury@gmail.com>
2017-09-29 17:15:23 +0100

    updates

--- tests/wdio.BUILDSETUP.conf.js
+++ tests/wdio.BUILDSETUP.conf.js
@@ -12,4 +12,5 @@ config.services = ['selenium-standalone'];
 config.baseUrl = 'http://127.0.0.1:80';
 config.tags = '@setup, ~@Pending, ~@ci, ~@deprecated, ~@Bug';
 config.cucumberOpts.failFast = true;
+config.specs = ['./src/features/to-come/xsetup-users.feature'];
 exports.config = config;
