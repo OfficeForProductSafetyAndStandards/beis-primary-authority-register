84b3395aca1c23395f9e264b27177342e00d3527
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-10-10 14:48:50 +0100

    PAR-742: updated filter.

--- web/modules/custom/par_data/src/Views/ParDataViewsData.php
+++ web/modules/custom/par_data/src/Views/ParDataViewsData.php
@@ -10,6 +10,10 @@ use Drupal\views\EntityViewsDataInterface;
  */
 class ParDataViewsData extends EntityViewsData implements EntityViewsDataInterface {
 
+  public function getParDataManager() {
+    return \Drupal::service('par_data.manager');
+  }
+
   /**
    * {@inheritdoc}
    */
@@ -36,19 +40,26 @@ class ParDataViewsData extends EntityViewsData implements EntityViewsDataInterfa
         'id' => 'par_partnership_revision_documents_completion_percentage',
       ],
     );
-    
-    // PAR Status Field and Filter.
+
+    // PAR Status Filter.
+    $entity_bundle = $this->getParDataManager()->getParBundleEntity($this->entityType->id());
+    $status_field = $entity_bundle->getConfigurationElementByType('entity', 'status_field');
+    $data[$this->entityType->getDataTable()][$status_field] = [
+      'title' => t('PAR Status'),
+      'filter' => [
+        'title' => t('PAR Status'),
+        'help' => t('Provides a status filter with configurable values to include.'),
+        'id' => 'par_data_status_filter',
+      ],
+    ];
+
+    // PAR Status Field.
     $data[$this->entityType->getDataTable()]['par_status'] = [
       'title' => t('PAR Status'),
       'field' => [
         'title' => t('PAR Status'),
         'help' => t('Provides the status field for PAR entities.'),
         'id' => 'par_data_status',
-      ],
-      'filter' => [
-        'title' => t('PAR Status'),
-        'help' => t('Provides a status filter with configurable values to include.'),
-        'id' => 'par_data_status_filter',
       ],
     ];
 
@@ -64,10 +75,10 @@ class ParDataViewsData extends EntityViewsData implements EntityViewsDataInterfa
 
     // Custom filter for Par Membership checks.
     $data[$this->entityType->getDataTable()]['id_filter'] = [
-      'title' => t('Partnership: Can user update?'),
+      'title' => t('Membership Filter'),
       'filter' => [
-        'title' => t('Partnership: Can user update?'),
-        'help' => t('Filter by partnerships this user can update.'),
+        'title' => t('Membership Filter'),
+        'help' => t('Filter by entities this user can update.'),
         'id' => 'par_member',
       ],
     ];
