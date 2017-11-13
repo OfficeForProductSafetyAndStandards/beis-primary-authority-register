7c8873f7cf552fc6ca7bd372e2da825209e10d77
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2017-11-13 15:05:52 +0000

    v9: Critical issues resolved with 9.0.0 release.

--- tools/cache_warmer.py
+++ tools/cache_warmer.py
@@ -1,5 +1,5 @@
 #############################################
-# Post deployment updates
+# Cache warming wrapper
 #############################################
 
 import json, os
@@ -7,27 +7,12 @@ import sys
 
 from subprocess import call
 
-j = json.loads(os.environ.get('VCAP_SERVICES'))
-
-credentials = j["postgres"][0]["credentials"]
-host = credentials["host"]
-name = credentials["name"]
-port = credentials["port"]
-username = credentials["username"]
-password = credentials["password"]
-
-f = open("/home/vcap/.pgpass", "w")
-f.write(host + ":5432:" + name + ":" + username + ":" + password)
-f.close()
-
-os.system("chmod 600 /home/vcap/.pgpass")
-
 os.environ["PHPRC"] = os.environ["HOME"] + "/app/php/etc"
 os.environ["PATH"]= os.environ["PATH"] + ":" + os.environ["HOME"] + "/app/php/bin:" + os.environ["HOME"] + "/app/php/sbin"
 os.environ["HTTPD_SERVER_ADMIN"] = "admin@localhost"
 os.environ["LD_LIBRARY_PATH"] = os.environ["HOME"] + "/app/php/lib"
 os.environ["PATH"] = os.environ["PATH"] + ":/home/vcap/app/bin/pgsql/bin"
 
-os.system("cd /home/vcap/app && sh drupal-update.sh /home/vcap/app")
+os.system("cd /home/vcap/app/web && ../vendor/drush/drush/drush pcw")
 
 
