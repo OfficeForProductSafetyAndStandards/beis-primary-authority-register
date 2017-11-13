c2fd7071ad4e9c7f04f5143dc11a05c635105312
Chris Moreton <chris@netsensia.com>
2017-11-13 16:35:22 +0000

    Futher work on monitoring script

--- devops/monitoring/monitor.py
+++ devops/monitoring/monitor.py
@@ -1,11 +1,32 @@
 import os
 import urllib2
-import json
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+from time import sleep
 
-req = urllib2.Request('https://api.cloud.service.gov.uk/v2/apps/' + os.environ['BEIS_CF_APP_KEY'] + '/stats')
-req.add_header('Authorization', 'bearer ' + os.environ['BEIS_CF_API_TOKEN'])
+for i in range(1000):
+    pnconfig = PNConfiguration()
+    pnconfig.subscribe_key = os.environ['BEIS_PAR_PUBNUB_SUBSCRIBE_KEY']
+    pnconfig.publish_key = os.environ['BEIS_PAR_PUBNUB_PUBLISH_KEY']
+    pnconfig.ssl = False
 
-resp = urllib2.urlopen(req)
-content = resp.read()
+    pubnub = PubNub(pnconfig)
 
-print content
+    req = urllib2.Request('https://api.cloud.service.gov.uk/v2/apps/' + os.environ['BEIS_PAR_CF_APP_KEY'] + '/stats')
+    req.add_header('Authorization', os.environ['BEIS_PAR_CF_API_AUTH_TOKEN'])
+
+    resp = urllib2.urlopen(req)
+    content = resp.read()
+
+    def publish_callback(result, status):
+        pass
+        # Handle PNPublishResult and PNStatus
+
+    pubnub.publish().channel('cloud_foundry_6').message(content).async(publish_callback)
+
+    print content
+    
+    file = open("/tmp/tmp.json", "w")
+    file.write(content)
+
+    sleep(60)
