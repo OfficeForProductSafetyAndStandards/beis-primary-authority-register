92c42cbe49d64845a39ac13b2e2bd1a7712abe6f
Chris Moreton <chris@netsensia.com>
2017-11-14 16:19:03 +0000

    Final work on monitoring script

--- devops/monitoring/monitor.py
+++ devops/monitoring/monitor.py
@@ -1,32 +1,47 @@
 import os
+import urllib
 import urllib2
+import json
+import sys
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 from time import sleep
 
-for i in range(1000):
+while True:
     pnconfig = PNConfiguration()
-    pnconfig.subscribe_key = os.environ['BEIS_PAR_PUBNUB_SUBSCRIBE_KEY']
-    pnconfig.publish_key = os.environ['BEIS_PAR_PUBNUB_PUBLISH_KEY']
+    pnconfig.subscribe_key = os.environ['PUBNUB_SUBSCRIBE_KEY']
+    pnconfig.publish_key = os.environ['PUBNUB_PUBLISH_KEY']
     pnconfig.ssl = False
 
     pubnub = PubNub(pnconfig)
 
-    req = urllib2.Request('https://api.cloud.service.gov.uk/v2/apps/' + os.environ['BEIS_PAR_CF_APP_KEY'] + '/stats')
-    req.add_header('Authorization', os.environ['BEIS_PAR_CF_API_AUTH_TOKEN'])
+    print "Requesting token..."
+    token_data = urllib.urlencode({'username'   : os.environ['CF_LOGIN_EMAIL'],
+                                   'password'   : os.environ['CF_LOGIN_PASSWORD'],
+                                   'grant_type' : 'password',
+                                   'scopes'     : '*'})
+                         
+    token_req = urllib2.Request('https://' + os.environ['CF_LOGIN_ENDPOINT'] + '/oauth/token', token_data)
+    token_req.add_header('Authorization', 'Basic Y2Y6')
 
-    resp = urllib2.urlopen(req)
-    content = resp.read()
+    token_resp = urllib2.urlopen(token_req)
+    token_content = json.load(token_resp)
+    
+    access_token = token_content['access_token']
+    refresh_token = token_content['refresh_token']
+    
+    stats_req = urllib2.Request('https://' + os.environ['CF_ENDPOINT'] + '/v2/apps/' + os.environ['CF_APP_KEY'] + '/stats')
+    stats_req.add_header('Authorization', 'Bearer ' + access_token)
 
+    stats_resp = urllib2.urlopen(stats_req)
+    body = json.load(stats_resp)
+    
     def publish_callback(result, status):
         pass
+        print "Published"
         # Handle PNPublishResult and PNStatus
 
-    pubnub.publish().channel('cloud_foundry_6').message(content).async(publish_callback)
-
-    print content
+    print "Publishing..."
+    pubnub.publish().channel('cloud_foundry_8').message(body).async(publish_callback)
     
-    file = open("/tmp/tmp.json", "w")
-    file.write(content)
-
-    sleep(60)
+    sleep(5)
