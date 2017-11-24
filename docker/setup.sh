214ad2af6e2e969dec76af5225df2cbad129365d
Stephen Mount <stemount@gmail.com>
2017-11-24 01:13:21 +0000

    yarn local [ci-skip]

--- docker/setup.sh
+++ docker/setup.sh
@@ -7,10 +7,20 @@
 
 # Install dependencies
 
+    # Update to current node LTS.
+    docker exec -it par_beta_web bash -c "n 8"
+
+    # Install yarn.
+    docker exec -i par_beta_web bash -c "curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -"
+    docker exec -i par_beta_web bash -c "echo 'deb https://dl.yarnpkg.com/debian/ stable main' | tee /etc/apt/sources.list.d/yarn.list"
+    docker exec -i par_beta_web bash -c "apt-get install apt-transport-https -y"
+    docker exec -i par_beta_web bash -c "apt-get update -y"
+    docker exec -i par_beta_web bash -c "apt-get install yarn -y"
+
     docker exec -i par_beta_web bash -c 'su - composer -c "cd ../../var/www/html && php composer.phar install"'
-    docker exec -i par_beta_web bash -c "cd /var/www/html/tests && ../../../../usr/local/n/versions/node/7.2.1/bin/npm install"
-    docker exec -i par_beta_web bash -c "../../../usr/local/n/versions/node/7.2.1/bin/npm install"
-    docker exec -i par_beta_web bash -c "../../../usr/local/n/versions/node/7.2.1/bin/npm run gulp"
+    docker exec -i par_beta_web bash -c "cd /var/www/html/tests && yarn install --ignore-engines"
+    docker exec -i par_beta_web bash -c "yarn install"
+    docker exec -i par_beta_web bash -c "npm run gulp"
 
 # Setup the development settings file:
 
