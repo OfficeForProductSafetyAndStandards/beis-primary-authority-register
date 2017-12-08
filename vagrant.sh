f1378a614485354a6e8cc16056441d30e29dd17b
Chris Moreton <chris@netsensia.com>
2017-12-08 16:18:46 +0000

    Correct typo

--- vagrant.sh
+++ vagrant.sh
@@ -1,6 +1,6 @@
 cd ..
 rm -rf beis-primary-authority-register
-git clone git@githib.com:UKGovernmentBEIS/beis-primary-authority-register
+git clone git@github.com:UKGovernmentBEIS/beis-primary-authority-register
 cd beis-primary-authority-register
 php composer.phar install
 chmod -R 777 web/modules/contrib
