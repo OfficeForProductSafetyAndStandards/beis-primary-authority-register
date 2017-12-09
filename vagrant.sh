f36d1badba88582dd3ee61d8b9a693e7cc0fd1b3
Chris Moreton <chris@netsensia.com>
2017-12-09 11:43:50 +0000

    Copy .env file during install

--- vagrant.sh
+++ vagrant.sh
@@ -1,7 +1,10 @@
+# Have a copy of your .env file in the directory directly above the cloned repo
+
 cd ..
 rm -rf beis-primary-authority-register
 git clone git@github.com:UKGovernmentBEIS/beis-primary-authority-register
 cd beis-primary-authority-register
+cp ../.env .
 php composer.phar install
 chmod -R 777 web/modules/contrib
 vagrant up
