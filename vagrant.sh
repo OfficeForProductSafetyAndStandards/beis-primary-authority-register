d2eabb1d71d9fafb90cf26e47a297dfbee17a909
Chris Moreton <chris@netsensia.com>
2017-12-12 00:04:19 +0000

    Don't care which directory is cloned into on vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -1,10 +1,33 @@
 # Run this command in the directory above the repo and Have a copy of your .env file in that directory
 #
-# sh beis-primary-authority-register/vagrant.sh
+# sh <repo directory>/vagrant.sh [branch]
 #
 
+command -v php >/dev/null 2>&1 || { 
+    echo "################################################################################################"
+    echo >&2 "PHP executable not found"
+    echo "################################################################################################"
+    exit 1 
+}
+
+command -v vagrant >/dev/null 2>&1 || { 
+    echo "################################################################################################"
+    echo >&2 "Vagrant executable not found"
+    echo "################################################################################################"
+    exit 1 
+}
+
+command -v git >/dev/null 2>&1 || { 
+    echo "################################################################################################"
+    echo >&2 "Git executable not found"
+    echo "################################################################################################"
+    exit 1 
+}
+
+IFS="/" read -ra REPO_DIR <<< "$0"
+
 # Into the repo
-cd beis-primary-authority-register
+cd $REPO_DIR
 
 # Save a copy of the .env file for later
 cp .env ..
@@ -17,13 +40,16 @@ sudo lsof -ti:8111 | xargs kill
 
 # Up above the repo for the deletion
 cd ..
-rm -rf beis-primary-authority-register
+rm -rf $REPO_DIR
 
 # Clone the repo
-git clone git@github.com:UKGovernmentBEIS/beis-primary-authority-register
+git clone git@github.com:UKGovernmentBEIS/beis-primary-authority-register $REPO_DIR
 
 # Into the repo
-cd beis-primary-authority-register
+cd $REPO_DIR
+
+# Checkout the required branch, or default to master if none specified
+if [ -z $1 ]; then git checkout $1; fi
 
 # Get the .env file we saved earlier
 cp ../.env .
