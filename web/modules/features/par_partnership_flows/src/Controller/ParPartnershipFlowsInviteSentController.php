405d2685dce5f1c392eeb48ad7e7cc5ee27dd9f3
Andrew Hughes-Onslow <andrew.hughes-onslow@transformuk.com>
2018-01-08 14:26:58 +0000

    PAR-803: Added partnership application confirmation flows for coordinated and direct partnerships.

--- web/modules/features/par_partnership_flows/src/Controller/ParPartnershipFlowsInviteSentController.php
+++ web/modules/features/par_partnership_flows/src/Controller/ParPartnershipFlowsInviteSentController.php
@@ -8,7 +8,7 @@ use Drupal\par_flows\Controller\ParBaseController;
 use Drupal\par_partnership_flows\ParPartnershipFlowsTrait;
 
 /**
- * A controller for displaying the application confirmation.
+ * A controller for rendering a list of inspection plan documents.
  */
 class ParPartnershipFlowsInviteSentController extends ParBaseController {
 
