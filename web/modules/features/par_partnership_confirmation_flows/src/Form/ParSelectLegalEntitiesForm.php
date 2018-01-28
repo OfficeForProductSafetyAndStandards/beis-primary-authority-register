af6163ab9c43f6030eb535d08b1d7da270c16ed1
Stephen Mount <stemount@gmail.com>
2018-01-28 17:18:18 +0000

    Legal entities form to actually update the partnership

--- web/modules/features/par_partnership_confirmation_flows/src/Form/ParSelectLegalEntitiesForm.php
+++ web/modules/features/par_partnership_confirmation_flows/src/Form/ParSelectLegalEntitiesForm.php
@@ -9,8 +9,9 @@ use Drupal\par_flows\Form\ParBaseForm;
 use Drupal\par_partnership_confirmation_flows\ParPartnershipFlowsTrait;
 
 /**
- * The primary contact form for the partnership details steps of the
- * 1st Data Validation/Transition User Journey.
+ * Organisation Legal Entities selection form.
+ * This generates a list of legal entities stored on the PAR Organisation as
+ * checkboxes.
  */
 class ParSelectLegalEntitiesForm extends ParBaseForm {
 
@@ -29,6 +30,7 @@ class ParSelectLegalEntitiesForm extends ParBaseForm {
   public function validateForm(array &$form, FormStateInterface $form_state) {
     parent::validateForm($form, $form_state);
 
+    // Retrieve form data.
     $selected_legal_entities = $form_state->getValue('field_legal_entity');
 
     // Check if at least one legal entity has been selected.
@@ -43,17 +45,25 @@ class ParSelectLegalEntitiesForm extends ParBaseForm {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
-//    if ($legal_entity->save()) {
+    // Filter form field data to those specifically checked.
+    $selected_legal_entities = array_filter($this->getFlowDataHandler()->getTempDataValue('field_legal_entity'));
+
+    // Update legal entities on the partnership with those selected on the form.
+    $par_data_partnership = $this->getFlowDataHandler()->getParameter('par_data_partnership');
+    $par_data_partnership->set('field_legal_entity', $selected_legal_entities);
+
+    // Commit changes to partnership entity.
+    if ($par_data_partnership->save()) {
 //      $this->getFlowDataHandler()->deleteStore();
-//    }
-//    else {
-//      $message = $this->t('This %field could not be saved for %form_id');
-//      $replacements = [
-//        '%field' => $this->getFlowDataHandler()->getTempDataValue('registered_name'),
-//        '%form_id' => $this->getFormId(),
-//      ];
-//      $this->getLogger($this->getLoggerChannel())->error($message, $replacements);
-//    }
+    }
+    else {
+      $message = $this->t('This %field could not be saved for %form_id');
+      $replacements = [
+        '%field' => $this->getFlowDataHandler()->getTempDataValue('registered_name'),
+        '%form_id' => $this->getFormId(),
+      ];
+      $this->getLogger($this->getLoggerChannel())->error($message, $replacements);
+    }
 
   }
 
