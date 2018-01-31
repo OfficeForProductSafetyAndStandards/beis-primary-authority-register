09511a2fd220c6ce362622af0a2ca274cf713026
Jon Edery <jon.edery@googlmail.com>
2018-01-31 09:52:14 +0000

    remove event class

--- web/modules/features/par_rd_help_desk_flows/src/Form/ParRdHelpDeskApproveConfirmForm.php
+++ web/modules/features/par_rd_help_desk_flows/src/Form/ParRdHelpDeskApproveConfirmForm.php
@@ -4,7 +4,6 @@ namespace Drupal\par_rd_help_desk_flows\Form;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\par_flows\Form\ParBaseForm;
-use Drupal\par_data\Event\ParDataEvent;
 use Drupal\par_data\Entity\ParDataPartnership;
 use Drupal\Core\Access\AccessResult;
 
