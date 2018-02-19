a0d4dd69300b5f07342e1b8c2ae78bcef4f54d8e
jaffamonkey <paullittlebury@gmail.com>
2018-02-19 14:30:31 +0000

    Updates to revocation journey and step code

--- tests/src/support/action/completeInvitationEmail.js
+++ tests/src/support/action/completeInvitationEmail.js
@@ -8,6 +8,9 @@ module.exports = () => {
      * The command to perform on the browser object (addValue or setValue)
      * @type {String}
      */
+    browser.setValue('#edit-name', 'testrevoking88@example.com');
+    browser.setValue('#edit-pass', 'TestPassword');
+    browser.click('#edit-submit');
     browser.setValue('#edit-pass-pass1', 'TestPassword');
     browser.setValue('#edit-pass-pass2', 'TestPassword');
     browser.click('#edit-next');
