64708bfccba51b27680290d7868d45783ef01e1c
Baber <info@webdevfreak.com>
2018-02-21 11:30:35 +0000

    Member Upload CSV validation - IN PROGRESS

--- web/modules/features/par_member_upload_flows/src/Form/ParMemberUploadFlowsForm.php
+++ web/modules/features/par_member_upload_flows/src/Form/ParMemberUploadFlowsForm.php
@@ -6,10 +6,12 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\par_data\Entity\ParDataPartnership;
 use Drupal\par_flows\Form\ParBaseForm;
 use Drupal\file\Entity\File;
+//use Drupal\file\FileInterface;
 use Drupal\par_member_upload_flows\ParFlowAccessTrait;
-use Drupal\user\Entity\User;
-
-//use Drupal\Core\Entity;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+//use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Encoder;
 
 /**
  * The upload CSV form for importing partnerships.
@@ -30,45 +32,6 @@ class ParMemberUploadFlowsForm extends ParBaseForm {
    */
   public function buildForm(array $form, FormStateInterface $form_state, ParDataPartnership $par_data_partnership = NULL) {
 
-//    $entity_type = $this->getParDataManager()->getEntitiesByProperty('par_data_coordinated_business', 'field_organisation', 1);
-//    dpm($entity_type);
-//    $user = User::load(\Drupal::currentUser()->id());
-//    $uid = $user->get('uid')->value;
-//    dpm('uid: ' . $uid);
-//    $entity_type = \Drupal::service('par_data.manager')->getParEntityType('par_data_coordinated_business');
-//    $entity_type = $this->getParDataManager()->getEntitiesByProperty('par_data_coordinated_business', 'field_organisation', 1);
-//    dpm($entity_type);
-//
-//    dpm($this->getFlowDataHandler()->getParameter('par_data_partnership')->id());
-//
-//    $csv_row_organisation_name = 'MY ORGANISATION NAME';
-//    $csv_row_legal_entity_name = 'MY LEGAL ENTITY NAME';
-//
-//    $conditions = [
-//      'matched' => [
-//        'AND' => [
-//          ['field_partnership', $this->getFlowDataHandler()->getParameter('par_data_partnership')->id(), 'IN'],
-////          ['organisation_name', $csv_row_organisation_name, '='],
-////          ['legal_entity_name', $csv_row_legal_entity_name, '='],
-//        ],
-//      ],
-//    ];
-//    $member = $this->getParDataManager()
-//      ->getEntitiesByQuery('par_data_coordinated_business', $conditions, 1);
-//    dpm($member->label());
-//
-//    dpm($user = \Drupal::currentUser());
-//    dpm($user['id:protected']);
-//    $user = User::load(\Drupal::currentUser()->id());
-//    $uid = $user->get('uid')->value;
-//    dpm($uid);
-//    if ($entity instanceof \Drupal\Core\Entity\ContentEntityInterface) {
-//      dpm('entity: ' . $entity);
-//    }
-//    dpm('ENTITY: ' . Entity::load($uid));
-//    dpm('ENTITY: ' . Entity::get('field_coordinated_business')->getValue());
-//    dpm(ParDataEntity::retrieveEntityIds('field_coordinated_business'));
-//
     // Multiple file field.
     $form['csv'] = [
       '#type' => 'managed_file',
@@ -78,7 +41,7 @@ class ParMemberUploadFlowsForm extends ParBaseForm {
       '#upload_location' => 's3private://member-csv/',
       '#multiple' => FALSE,
       '#required' => TRUE,
-      '#default_value' => $this->getFlowDataHandler()->getDefaultValues("csv"),
+      '#default_value' => $this->getFlowDataHandler()->getDefaultValues('csv'),
       '#upload_validators' => [
         'file_validate_extensions' => [
           0 => 'csv',
@@ -97,22 +60,45 @@ class ParMemberUploadFlowsForm extends ParBaseForm {
 
     // Process uploaded csv file.
     if ($csv = $this->getFlowDataHandler()->getTempDataValue('csv')) {
-
       // Define array variable.
       $rows = [];
 
       // Load the submitted file and process the data.
       $files = File::loadMultiple($csv);
-      foreach ($files as $file) {
-        // Save processed row data in an array.
-        $rows = $this->getParDataManager()->processCsvFile($file, $rows);
-      }
+//      dpm($files);
+//      dpm($files->getFileUri());
+//      foreach ($files as $file) {
+//        if (($handle = fopen($file->getFileUri(), "r")) !== FALSE) {
+//          dpm($handle);
+//        }
+//      }
+//      $serializer = new Serializer([new ObjectNormalizer()], [new CsvEncoder()]);
+      $serializer = new Serializer([new CsvEncoder()]);
 
-      // Save the data in the User's temp private store for later processing.
-      if (!empty($rows)) {
-
-        $form_state->setValue('coordinated_members', $rows);
-      }
+      // instantiation, when using it inside the Symfony framework.
+//      $serializer = $container->get('serializer');
+//      $serializer = $serializer->get('serializer');
+//
+//      // encoding contents in CSV format.
+//      $serializer->encode($data, 'csv');
+//
+      // decoding CSV contents.
+      $file_path = 's3private://member-csv/coordinated_member_upload_template_10000_5.csv';
+      $serializer->encode($file_path, 'csv');
+//      $data = $serializer->decode(file_get_contents($file_path), 'csv');
+//      dpm($data);
+//
+//      foreach ($files as $file) {
+//        // Save processed row data in an array.
+//        $rows[] = $this->getParDataManager()->processCsvFile($file, $rows);
+//      }
+//
+//      // Save the data in the User's temp private store for later processing.
+//      if (!empty($rows)) {
+//
+//        // Set csv data in temporary data storage.
+//        $this->getFlowDataHandler()->setTempDataValue('coordinated_members', $rows);
+//      }
     }
   }
 
