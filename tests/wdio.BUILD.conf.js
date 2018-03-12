19097d18d9ecffdeaa9caf0161401ce11e25e7d0
kalpaitch <kalpaitch@users.noreply.github.com>
2018-03-12 20:27:10 +0000

    Update wdio.BUILD.conf.js

--- tests/wdio.BUILD.conf.js
+++ tests/wdio.BUILD.conf.js
@@ -2,7 +2,7 @@ const wdioConfig = require('./wdio.conf.js');
 wdioConfig.config.capabilities = [{
     browserName: 'chrome',
     chromeOptions: {
-        binary: '/usr/bin/google-chrome ',
+        binary: '/usr/bin/google-chrome',
         args: ['--headless', '--no-sandbox', '--disable-gpu', '--window-size=1200,2000']
     }
 }];
