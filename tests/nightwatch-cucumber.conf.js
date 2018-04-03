1e6fde758c7f7924f8792bc9e1c682dfd9162686
jaffamonkey <paullittlebury@gmail.com>
2018-04-03 17:07:12 +0200

    updates

--- tests/nightwatch-cucumber.conf.js
+++ tests/nightwatch-cucumber.conf.js
@@ -32,7 +32,7 @@ module.exports = {
             desiredCapabilities: {
                 browserName: 'chrome',
                 chromeOptions : {
-                 // binary: electron,
+                 binary: electron,
                     args: ['--headless', '--window-size=1280,1280'],
                   },
                 javascriptEnabled: true,
