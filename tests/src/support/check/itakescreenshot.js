f278eea8e539fd2f7ca22f27e277b4f6b7099b04
jaffamonkey <paullittlebury@gmail.com>
2018-04-06 01:52:08 +0200

    trying something out with test ordering

--- tests/src/support/check/itakescreenshot.js
+++ tests/src/support/check/itakescreenshot.js
@@ -3,5 +3,10 @@ module.exports = (keyword, done) => {
      * The command to perform on the browser object (addValue or setValue)
      * @type {String}
      */
-        browser.saveScreenshot(['xxx.png']);
+        // browser.setViewportSize({
+        //     width: 1024,
+        //     height: 768,
+        // });
+
+        browser.saveScreenshot('./tests/errorShots/travis.png');
 };
