1aef4061950a822283d58f04ccc49d772a5c8811
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2018-04-13 11:14:09 +0100

    WIP3

---
+++ web/modules/features/par_enforcement_raise_flows/src/Form/ParSelectOrganisationForm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\par_enforcement_raise_flows\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\par_data\Entity\ParDataAuthority;
+use Drupal\par_flows\Form\ParBaseForm;
+use Drupal\par_enforcement_raise_flows\ParFlowAccessTrait;
+use Drupal\user\Entity\User;
+
+/**
+ * The member contact form.
+ */
+class ParSelectOrganisationForm extends ParBaseForm {
+
+  use ParFlowAccessTrait;
+
+  /**
+   * Set the page title.
+   */
+  protected $pageTitle = 'Enforce member';
+
+}
