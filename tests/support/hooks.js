25046b6f4c84d617f295ec791f5f3b00dcd3d135
jaffamonkey <paullittlebury@gmail.com>
2018-04-24 01:26:14 +0200

    merged with master to resolve travis issue, and updated relevant test steps

--- tests/support/hooks.js
+++ tests/support/hooks.js
@@ -3,15 +3,15 @@ const reporter = require('cucumber-html-reporter');
 var path = require('path');
 var {After, AfterAll, Before, BeforeAll} = require('cucumber');
 
-BeforeAll(function () {
-    return client
-        .url(client.launch_url + '/user/login')
-        .setValue('#edit-name', 'dadmin')
-        .setValue('#edit-pass', 'TestPassword')
-        .click('#edit-submit')
-        .url(client.launch_url + '/admin/par-data-test-reset')
-        .url(client.launch_url + '/user/logout')
-});
+// BeforeAll(function () {
+//     return client
+//         .url(client.launch_url + '/user/login')
+//         .setValue('#edit-name', 'dadmin')
+//         .setValue('#edit-pass', 'TestPassword')
+//         .click('#edit-submit')
+//         .url(client.launch_url + '/admin/par-data-test-reset')
+//         .url(client.launch_url + '/user/logout')
+// });
 
 After(function () {
     return client
