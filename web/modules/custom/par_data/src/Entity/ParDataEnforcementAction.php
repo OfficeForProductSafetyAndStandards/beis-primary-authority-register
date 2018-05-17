292db196acd13b5f147c86ff800bcba0e22ea761
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2018-05-17 10:53:27 +0100

    PAR-1228: Updated saving of notices.

--- web/modules/custom/par_data/src/Entity/ParDataEnforcementAction.php
+++ web/modules/custom/par_data/src/Entity/ParDataEnforcementAction.php
@@ -283,59 +283,27 @@ class ParDataEnforcementAction extends ParDataEntity {
   /**
    * Clone an enforcement notice action entity to support the referral process.
    *
-   * @return ParDataEnforcementAction $action
+   * @return ParDataEnforcementAction|boolean
    *  cloned action entity if a referral exists or NULL.
    */
-  public function cloneReferredEnforcementAction($referral_authority_id, $action_id) {
-
+  public function cloneAction($referral_authority_id) {
     if ($referral_authority_id) {
       // Duplicate this action before changing the status.
       $cloned_action = $this->createDuplicate();
       // As we are referring an action by cloning the original we need to set the original
       // action id on the copied (referred action) this indicates that the action in question
       // has been referred once and can no longer be referred.
-      $cloned_action->set('field_action_referral', $action_id);
+      $cloned_action->set('field_action_referral', $this->id());
       // If a new duplicate enforcement action has been saved to the system return it.
       if ($cloned_action->save()) {
         return $cloned_action;
       }
       else {
         throw new ParDataException("The referral action for the %action (action) cannot be created, please contact the helpdesk.");
-        return FALSE;
       }
     }
     else {
-    throw new ParDataException("The referral Action for %action (action) could not be created referral id missing, please contact the helpdesk.");
-    return FALSE;
-  }
-  }
-
-  /**
-   * Clone an enforcement notice entity to support the referral process.
-   *
-   * @return ParDataEnforcementAction $action
-   *  cloned action entity if a referral exists or NULL.
-   */
-  public function cloneEnforcementNotice($referral_authority_id, ParDataEnforcementAction $cloned_action, ParDataEnforcementNotice $par_data_enforcement_notice) {
-
-    if ($referral_authority_id) {
-      // Duplicate this enforcement notification and assign it the cloned action.
-      $referral_notice = $par_data_enforcement_notice->createDuplicate();
-      $referral_notice->set('field_primary_authority', $referral_authority_id);
-      $referral_notice->set('field_enforcement_action', $cloned_action->id());
-
-      if ($referral_notice->save()) {
-        return $referral_notice;
-      }
-      else {
-        throw new ParDataException("The referral Enforcement notice for the %action (action) cannot be created, please contact the helpdesk.");
-        return FALSE;
-      }
-
-    }
-    else {
-      throw new ParDataException("The referral Enforcement Notice for the %action action could not be created referral id missing, please contact the helpdesk.");
-      return FALSE;
+      throw new ParDataException("The referral Action for %action (action) could not be created referral id missing, please contact the helpdesk.");
     }
   }
 
--- web/modules/features/par_enforcement_review_flows/src/Form/ParEnforcementReviewActionsForm.php
+++ web/modules/features/par_enforcement_review_flows/src/Form/ParEnforcementReviewActionsForm.php
@@ -61,7 +61,7 @@ class ParEnforcementReviewActionsForm extends ParBaseForm {
     // Create the enforcement actions.
     foreach ($par_data_enforcement_actions as $par_data_enforcement_action) {
       $status = $this->getFlowDataHandler()->getTempDataValue([ParFormBuilder::PAR_COMPONENT_PREFIX . 'enforcement_action_review', $delta, 'action', 'primary_authority_status'], $enforcement_actions_cid);
-var_dump($status);
+
       switch ($status) {
         case ParDataEnforcementAction::APPROVED:
           $par_data_enforcement_action->approve(FALSE);
@@ -100,9 +100,54 @@ var_dump($status);
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
-    $par_data_enforcement_notice = $this->getFlowDataHandler()->getParameter('par_data_enforcement_notice');
+    // Set the correct values for the entities to be saved.
+    $entities = $this->createEntities();
+    extract($entities);
+    /** @var ParDataEnforcementNotice $par_data_enforcement_notice */
+    /** @var ParDataEnforcementAction[] $par_data_enforcement_actions */
 
-    if ($this->referral_cloning($par_data_enforcement_notice)) {
+    $action_referral_cid = $this->getFlowNegotiator()->getFormKey('par_enforcement_referred_authority');
+
+    // Iterate through the actions and perform any necessary updates.
+    foreach ($par_data_enforcement_actions as $delta => $par_data_enforcement_action) {
+      $action_saved = $par_data_enforcement_action->save();
+
+      // Skip any actions that are not being referred.
+      if (!$par_data_enforcement_action->isReferred()) {
+        continue;
+      }
+
+      // The authority to refer the current action to.
+      $referral_authority_id = $this->getFlowDataHandler()->getTempDataValue([ParFormBuilder::PAR_COMPONENT_PREFIX . 'enforcement_action_refer', $delta, 'referred_to'], $action_referral_cid);
+
+      try {
+        if ($cloned_action = $par_data_enforcement_action->cloneAction($referral_authority_id)) {
+          $cloned_notice = $par_data_enforcement_notice->cloneNotice($referral_authority_id, $cloned_action);
+        }
+      }
+      catch (ParDataException $e) {
+        $replacements = [
+          '%action' => $par_data_enforcement_action->id(),
+        ];
+        $this->getLogger($this->getLoggerChannel())->error($e->getMessage(), $replacements);
+        return FALSE;
+      }
+
+      if (isset($cloned_notice) && !empty($cloned_notice) && $action_saved) {
+        $this->getFlowDataHandler()->deleteStore();
+      }
+      else {
+        $message = $this->t('The enforcement notice %confirm could not be approved for %form_id');
+        $replacements = [
+          '%confirm' => $par_data_enforcement_notice->id(),
+          '%form_id' => $this->getFormId(),
+        ];
+        $this->getLogger($this->getLoggerChannel())->error($message, $replacements);
+      }
+    }
+
+    // Save the enforcement notice.
+    if ($par_data_enforcement_notice->save()) {
       $this->getFlowDataHandler()->deleteStore();
     }
     else {
@@ -114,92 +159,4 @@ var_dump($status);
       $this->getLogger($this->getLoggerChannel())->error($message, $replacements);
     }
   }
-
-  /**
-   * Helper function to save the notice.
-   */
-  public function referral_cloning($par_data_enforcement_notice) {
-
-    foreach ($par_data_enforcement_notice->get('field_enforcement_action')->referencedEntities() as $delta => $action) {
-
-      $cid = $this->getFlowNegotiator()->getFormKey('par_enforcement_notice_approve');
-      $form_data = $this->getFlowDataHandler()->getTempDataValue(['actions', $delta], $cid);
-      $action_id = $action->id();
-
-      switch ($form_data['primary_authority_status']) {
-        case ParDataEnforcementAction::APPROVED:
-          if (!$action->approve()) {
-            $message = $this->t('The enforcement notification action entity %entity_id could not be updated to a approved state within %form_id');
-            $replacements = [
-              '%entity_id' => $action_id,
-              '%form_id' => $this->getFormId(),
-            ];
-            $this->getLogger($this->getLoggerChannel())->error($message, $replacements);
-            return FALSE;
-          }
-          break;
-
-        case ParDataEnforcementAction::BLOCKED:
-          if (!$action->block($form_data['primary_authority_notes'])) {
-            $message = $this->t('The enforcement notification action entity %entity_id could not be updated to a blocked state within %form_id');
-            $replacements = [
-              '%entity_id' => $action_id,
-              '%form_id' => $this->getFormId(),
-            ];
-            $this->getLogger($this->getLoggerChannel())->error($message, $replacements);
-            return FALSE;
-          }
-          break;
-
-        case ParDataEnforcementAction::REFERRED:
-
-          // The authority to refer the current action to.
-          $cid = $this->getFlowNegotiator()->getFormKey('par_enforcement_referred_authority');
-          $referral_authority_id = $this->getFlowDataHandler()->getTempDataValue([$action_id], $cid);
-
-          try {
-            $cloned_action = $action->cloneReferredEnforcementAction($referral_authority_id, $action_id);
-          }
-          catch (ParDataException $e) {
-            $replacements = [
-              '%action' => $action_id,
-            ];
-            $this->getLogger($this->getLoggerChannel())->error($e->getMessage(), $replacements);
-            return FALSE;
-          }
-
-          if (isset($cloned_action)) {
-            try {
-              // Create a new Enforcement notice for every referred action.
-              $referral_notice = $action->cloneEnforcementNotice($referral_authority_id, $cloned_action, $par_data_enforcement_notice);
-            }
-            catch (ParDataException $e) {
-              $replacements = [
-                '%action' => $action_id,
-              ];
-              $this->getLogger($this->getLoggerChannel())->error($e->getMessage(), $replacements);
-              return FALSE;
-            }
-            if (isset($referral_notice)) {
-              // Persist the information in the temporary store here
-              // because the application is not yet complete.
-              $this->getFlowDataHandler()->setTempDataValue('referral_id' . $action_id, $referral_notice->id());
-
-              if (!$action->refer($form_data['referral_notes'])) {
-                $message = $this->t('The enforcement notification action entity %entity_id could not be updated to a referred state within %form_id');
-                $replacements = [
-                  '%entity_id' => $action_id,
-                  '%form_id' => $this->getFormId(),
-                ];
-                $this->getLogger($this->getLoggerChannel())->error($message, $replacements);
-                return FALSE;
-              }
-            }
-          }
-          break;
-      }
-    }
-    return TRUE;
-  }
-
 }
