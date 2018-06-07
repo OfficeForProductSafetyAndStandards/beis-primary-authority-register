f98f1c5fdbdb1b9265fa62e5c459b2d88e2af4ed
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2018-06-07 12:49:29 +0100

    PAR-1233: Updated member form.

--- web/modules/features/par_member_add_flows/src/Form/ParOrganisationNameForm.php
+++ web/modules/features/par_member_add_flows/src/Form/ParOrganisationNameForm.php
@@ -5,18 +5,18 @@ namespace Drupal\par_member_add_flows\Form;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\par_data\Entity\ParDataPartnership;
 use Drupal\par_flows\Form\ParBaseForm;
+use Drupal\par_member_add_flows\ParFlowAccessTrait;
 
 /**
  * Enter the member organisation name.
  */
 class ParOrganisationNameForm extends ParBaseForm {
 
-  protected $formItems = [
-    'par_data_organisation:organisation' => [
-      'organisation_name' => 'organisation_name',
-    ],
-  ];
+  use ParFlowAccessTrait;
 
+  /**
+   * Set the page title.
+   */
   protected $pageTitle = 'Add member organisation name';
 
 }
