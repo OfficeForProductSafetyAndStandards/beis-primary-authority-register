04a11b9cf2c93e66d3aa223281b1ffe6591a96a9
Chris Moreton <chris@netsensia.com>
2018-08-15 13:21:16 +0100

    Add script for copying bucket contents to BEIS AWS

---
+++ devops/tools/awscp.sh
@@ -0,0 +1,6 @@
+aws s3 cp s3://transform-par-beta-artifacts/ s3://beis-par-artifacts/ --recursive
+aws s3 cp s3://transform-par-beta-development-public/ s3://beis-par-development-public/ --recursive
+aws s3 cp s3://transform-par-beta-development-private/ s3://beis-par-development-private/ --recursive
+aws s3 cp s3://transform-par-beta-production-public/ s3://beis-par-production-public/ --recursive
+aws s3 cp s3://transform-par-beta-production-private/ s3://beis-par-production-private/ --recursive
+aws s3 cp s3://transform-par-beta-vault/ s3://beis-par-hashicorp-vault/ --recursive
