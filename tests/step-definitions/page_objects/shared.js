323c6a7018b16f670e5ab4ac046735c58cc141c3
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2018-09-06 14:57:01 +0100

    PAR-424: Updated tests.

--- tests/step-definitions/page_objects/shared.js
+++ tests/step-definitions/page_objects/shared.js
@@ -21,10 +21,9 @@ module.exports = {
             return this.click('link text', linkText);
         },
         clickLinkByPartialText: function(partialLinkText){
-            return client
-                .useXpath()
+            return this.useXpath()
                 .click("//*[contains(text(), '" + partialLinkText + "')]")
-                .useCss()
+                .useCss();
         },
         putTextFromSelectorToAnotherSelector: function(selector1, input1){ 
             var text;
