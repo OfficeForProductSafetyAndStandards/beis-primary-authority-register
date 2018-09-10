4c4704c955482c9eed98b35549d7e84efdc32161
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2018-09-11 00:50:28 +0100

    Bucket update.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -979,4 +979,3 @@ if (!empty($appEnv) && file_exists("{$app_root}/{$site_path}/settings.local.{$ap
 if (file_exists("{$app_root}/{$site_path}/settings.local.php")) {
   include "{$app_root}/{$site_path}/settings.local.php";
 }
-
