1f17e476471d8742853fd01529450c8780e7beed
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2018-10-09 13:37:08 +0100

    PAR-1319: Updated locale transiation string settings.

--- web/modules/features/par_partnership_confirmation_flows/src/Form/ParChecklistForm.php
+++ web/modules/features/par_partnership_confirmation_flows/src/Form/ParChecklistForm.php
@@ -58,6 +58,8 @@ class ParChecklistForm extends ParBaseForm {
       '#return_value' => 'on',
     ];
 
+    // There is a legal requirement to mention that the PA is the processor (under GDPR) and as such they must
+    // adhere to the Office for Safety and Standards procedures concerning anything that could constitute personal data.
     $form['help'] = [
       '#type' => 'markup',
       '#markup' => '<p>You are required to have obtained written permission from the organisation to fill in the details on their behalf and submit the partnership for nomination.</p>',
