50952430180719abede3dfcad819373b587bdf5b
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2019-02-11 00:43:21 +0000

    Updated test config.

--- tests/nightwatch-cucumber-circleci.conf.js
+++ tests/nightwatch-cucumber-circleci.conf.js
@@ -28,7 +28,7 @@ module.exports = {
     },
     test_settings: {
         default: {
-            launch_url: 'http://127.0.0.1:80',
+            launch_url: 'http://par.localhost',
             selenium_port: 4444,
             // selenium_host: 'localhost',
             screenshots : {
