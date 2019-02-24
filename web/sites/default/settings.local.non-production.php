b74da98ca76abb3bf7c4f3b09cf9f712e6a92d10
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2019-02-24 16:24:34 +0000

    Updated non-production settings.

--- web/sites/default/settings.local.non-production.php
+++ web/sites/default/settings.local.non-production.php
@@ -1,15 +1,5 @@
 <?php
-
-$databases['default']['default'] = array (
-  'database' => 'par',
-  'username' => 'par',
-  'password' => '123456',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '5432',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\pgsql',
-  'driver' => 'pgsql',
-);
+// A generic settings file for use on all GovUK PaaS non-production instances.
 
 $settings['trusted_host_patterns'] = [''];
 
