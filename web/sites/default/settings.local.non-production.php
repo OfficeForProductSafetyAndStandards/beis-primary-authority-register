e856d05965b03caf247525726fdceb6da7f8ea09
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2019-04-10 12:05:40 +0100

    Updated redis configuration.

--- web/sites/default/settings.local.non-production.php
+++ web/sites/default/settings.local.non-production.php
@@ -1,18 +1,17 @@
 <?php
 // A generic settings file for use on all GovUK PaaS non-production instances.
 
+// Remove url restrictions.
 $settings['trusted_host_patterns'] = [''];
 
+// Enable all dev and testing config.
 $config['config_split.config_split.dev_config']['status'] = TRUE;
 $config['config_split.config_split.test_config']['status'] = TRUE;
 
+// Enable the local services configuration which enables debugging.
 if (file_exists($app_root . '/' . $site_path . '/services.local.yml')) {
   $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.local.yml';
 }
-
-// Ensure travis always runs with the same memory that other environments do.
-ini_set('memory_limit', '2048M');
-ini_set('max_execution_time', 300);
 
 // Enable tota11y library.
 $settings['enable_tota11y'] = TRUE;
