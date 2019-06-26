c06826d85a547cc37e4b5220244284591d4677bd
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2019-06-26 22:41:54 +0100

    Advice: Updated tests.

--- tests/features/upload_advice.feature
+++ tests/features/upload_advice.feature
@@ -1,96 +1,98 @@
 Feature: upload advice
-As a user
-I should be able to upload advice
+  As a user
+  I should be able to upload advice
 
-@upload-advice @ci
-Scenario: Verify upload advice is not available for non-active partnerships
-Given I am logged in as "par_authority@example.com"
-When I go to detail page for partnership with authority "City Enforcement Squad"
-And I click "See all Advice"
-Then I should see advice page
-And I should not see the link "Upload advice"
+  @upload-advice @ci
+  Scenario: Verify upload advice is not available for non-active partnerships
+    Given I am logged in as "par_authority@example.com"
+    When I go to detail page for partnership with authority "City Enforcement Squad"
+    And I click "See all Advice"
+    Then I should see advice page
+    And I should not see the link "Upload advice"
 
-@upload-advice @ci
-Scenario: Verify upload advice is available for active partnerships only
-Given I am logged in as "par_authority@example.com"
-When I click on "See your partnerships"
-And I click on "Upper West Side Borough Council"
-And I click "See all Advice"
-Then I should see advice page
-And I see the link "Upload advice"
+  @upload-advice @ci
+  Scenario: Verify upload advice is available for active partnerships only
+    Given I am logged in as "par_authority@example.com"
+    And I navigate to an active partnership "Upper West Side Borough Council"
+    And I click "See all Advice"
+    Then I should see advice page
+    And I see the link "Upload advice"
 
-@upload-advice @ci
-Scenario: Upload-advice type business
-Given I am logged in as "par_authority@example.com"
-And I navigate to an active partnership "33968"
-When I open advice page
-Then I should be able to confirm the guidelines
-And I upload the file "files/test.png" to field "#edit-files-upload"
-And I click on the button "#edit-upload"
-And enter advice title
-And I enter summary of advice
-And I select advice type "business-advice"
-And I select "Cookie control" regulatory function
-And I click save
-And I see advice uploaded successfully
+  @upload-advice @ci
+  Scenario: Upload-advice type business
+    Given I am logged in as "par_authority@example.com"
+    And I navigate to an active partnership "Lower East Side Borough Council"
+    When I open advice page
+    Then I should be able to confirm the guidelines
+    And I upload the file "files/test.png" to field "#edit-files-upload"
+    And I click on the button "#edit-upload"
+    And enter advice title
+    And I enter summary of advice
+    And I select advice type "business-advice"
+    And I select "Cookie control" regulatory function
+    And I click save
+    And I see advice uploaded successfully
 
-@upload-advice @ci
-Scenario: Upload-advice type background information
-Given I am logged in as "par_authority@example.com"
-And I navigate to an active partnership "33968"
-When I open advice page
-Then I should be able to confirm the guidelines
-And I upload the file "files/test.png" to field "#edit-files-upload"
-And I click on the button "#edit-upload"
-And enter advice title
-And I enter summary of advice
-And I select advice type "background-information"
-And I select "Cookie control" regulatory function
-And I click save
-And I see advice uploaded successfully
+  @upload-advice @ci
+  Scenario: Upload-advice type background information
+    Given I am logged in as "par_authority@example.com"
+    And I navigate to an active partnership "Lower East Side Borough Council"
+    When I open advice page
+    Then I should be able to confirm the guidelines
+    And I upload the file "files/test.png" to field "#edit-files-upload"
+    And I click on the button "#edit-upload"
+    And enter advice title
+    And I enter summary of advice
+    And I select advice type "background-information"
+    And I select "Cookie control" regulatory function
+    And I click save
+    And I see advice uploaded successfully
 
-@upload-advice @ci
-Scenario: Helpdesk user able to upload advice for an active partnership
-Given I am logged in as "par_helpdesk@example.com"
-And I navigate to an active partnership "3316"
-When I open advice page
-Then I should be able to confirm the guidelines
-And I upload the file "files/test.png" to field "#edit-files-upload"
-And I click on the button "#edit-upload"
-And enter advice title
-And I enter summary of advice
-And I select advice type "business-advice"
-And I select "Environmental health" regulatory function
-And I click save
-And I see advice uploaded successfully
+  @upload-advice @ci
+  Scenario: Helpdesk user able to upload advice for an active partnership
+    Given I am logged in as "par_helpdesk@example.com"
+    And I navigate to an active partnership "Lower East Side Borough Council"
+    When I open advice page
+    Then I should be able to confirm the guidelines
+    And I upload the file "files/test.png" to field "#edit-files-upload"
+    And I click on the button "#edit-upload"
+    And enter advice title
+    And I enter summary of advice
+    And I select advice type "business-advice"
+    And I select "Environmental health" regulatory function
+    And I click save
+    And I see advice uploaded successfully
 
-@upload-advice @ci
-Scenario: Edit advice
-Given I am logged in as "par_authority@example.com"
-And I navigate to an active partnership "33968"
-And I click "See all Advice"
-When I click on edit against an advice
-And I select file to remove
-And I click on the button "#edit-upload"
-Then I upload the file "files/Advice Issued - 01 - Attic Descriptions v2_1.pdf" to field "#edit-files-upload"
-And I click on the button "#edit-upload"
-And I see new file in the advice detail page
+  @upload-advice @ci
+  Scenario: Edit advice
+    Given I am logged in as "par_authority@example.com"
+    And I navigate to an active partnership "Lower East Side Borough Council"
+    And I click "See all Advice"
+    When I click on edit against an advice
+    And I select file to remove
+    And I click on the button "#edit-upload"
+    Then I upload the file "files/Advice Issued - 01 - Attic Descriptions v2_1.pdf" to field "#edit-files-upload"
+    And I click on the button "#edit-upload"
+    And I see new file in the advice detail page
 
-@upload-advice @ci
-Scenario: Archive advice
-Given I am logged in as "par_authority@example.com"
-And I navigate to an active partnership "33968"
-And I click "See all Advice"
-When I click on archive against an advice
-When I enter reason "new advice is ready"
-And I click save
-Then I should archive successfully
+  @upload-advice @ci
+  Scenario: Archive advice
+    Given I am logged in as "par_authority@example.com"
+    And I navigate to an active partnership "Lower East Side Borough Council"
+    And I click "See all Advice"
+    When I click on archive against an advice
+    When I enter reason "new advice is ready"
+    And I click save
+    Then I should archive successfully
 
 
-Scenario: validate error when guidline checkbox is not selected
-Scenario: valdiate error message when title, summary or type of advice missing
-Scenario: Verify search advice
-Scenario: Verify filter advice
+  Scenario: validate error when guidline checkbox is not selected
+
+  Scenario: valdiate error message when title, summary or type of advice missing
+
+  Scenario: Verify search advice
+
+  Scenario: Verify filter advice
 
 
 
--- tests/step-definitions/upload_advice_steps.js
+++ tests/step-definitions/upload_advice_steps.js
@@ -1,148 +1,149 @@
-const { client } = require('nightwatch-cucumber')
-const { Given, Then, When } = require('cucumber')
+const {client} = require('nightwatch-cucumber')
+const {Given, Then, When} = require('cucumber')
 const shared = client.page.shared()
 
 
-
-Given('I navigate to an active partnership {string}', function (string){
-            return client
-           .url(client.launch_url+'partnership/'+string+'/authority-details')
-         });
+Given('I navigate to an active partnership {string}', function (string) {
+  return client
+      .url(client.launch_url + 'partnerships')
+      .setValue('#edit-keywords', authority)
+      .click('#edit-submit-par-user-partnerships')
+      .clickLinkByPureText(authority)
+});
 
 Then('I should see advice page', function () {
-            return shared
-            .assert.containsText('h1.heading-xlarge','Advice')
-          });
+  return shared
+      .assert.containsText('h1.heading-xlarge', 'Advice')
+});
 
 Then('I should not see the link {string}', function (string) {
-            return client
-            .expect.element('a.flow-link').to.be.not.present
+  return client
+      .expect.element('a.flow-link').to.be.not.present
 
-         });
+});
 
 
 Then('I see the link {string}', function (string) {
-           return client
-.assert.containsText('a.flow-link','Upload advice')
-           });
+  return client
+      .assert.containsText('a.flow-link', 'Upload advice')
+});
 
 Given('I open an active partnership {string}', function (string) {
-          return shared
-          .goToPartnershipDetailPage(string,'active')
-         });
+  return shared
+      .goToPartnershipDetailPage(string, 'active')
+});
 
 Given('I open advice page', function () {
-           return shared
-            .clickLinkByPureText('See all Advice')
-            .clickLinkByPureText('Upload advice')
-         });
+  return shared
+      .clickLinkByPureText('See all Advice')
+      .clickLinkByPureText('Upload advice')
+});
 
 Then('I should be able to confirm the guidelines', function () {
-      return client
+  return client
       .click('#edit-declaration')
-     .click('#edit-save')
-              });
+      .click('#edit-save')
+});
 
 
- Then('I able to upload advice document', function () {
-       return client
-       .setValue('#edit-files-upload','files/test.png')
-       .click('#edit-upload')
-         });
+Then('I able to upload advice document', function () {
+  return client
+      .setValue('#edit-files-upload', 'files/test.png')
+      .click('#edit-upload')
+});
 
 
 Then('enter advice title', function () {
- return client
-  .clearValue('#edit-advice-title')
-  .setValue('#edit-advice-title','Auto-test-NewAdvice')
+  return client
+      .clearValue('#edit-advice-title')
+      .setValue('#edit-advice-title', 'Auto-test-NewAdvice')
 
-         });
+});
 Then('I enter summary of advice', function () {
-           return client
-           .clearValue('#edit-notes')
-           .setValue('#edit-notes','Auto-advice-Summary')
+  return client
+      .clearValue('#edit-notes')
+      .setValue('#edit-notes', 'Auto-advice-Summary')
 
-         });
+});
 
-   Then('I select advice type {string}', function (string) {
-       return client
-        .click('#edit-advice-type-'+string)
+Then('I select advice type {string}', function (string) {
+  return client
+      .click('#edit-advice-type-' + string)
 
-         });
+});
 
-  Then('I select {string} regulatory function', function (string) {
-                   return client
-                   .useXpath()
-                   .click('//label[contains(text(),\"'+string+'"\)]')
-                   .useCss()
-                  });
-
-
- Then('I select type of advice', function () {
-           return client
-           .click('#edit-advice-type-business-advice')
-         });
-
- Then('I select regulatory function', function () {
-           return client
-           .clickCheckboxIfUnselected('#edit-regulatory-functions-13')
-         });
+Then('I select {string} regulatory function', function (string) {
+  return client
+      .useXpath()
+      .click('//label[contains(text(),\"' + string + '"\)]')
+      .useCss()
+});
 
 
-         Then('I see advice uploaded successfully', function () {
-           return shared
-           .clickLinkByPureText('Auto-test-NewAdvice')
+Then('I select type of advice', function () {
+  return client
+      .click('#edit-advice-type-business-advice')
+});
 
-           .assert.containsText('.filename','test')
-         });
-
-
-         When('I click on edit against an advice', function () {
-           return shared
-           .clickLinkByPureText('Edit')
-           .assert.containsText('h1.heading-xlarge','Edit document type')
-
-         });
+Then('I select regulatory function', function () {
+  return client
+      .clickCheckboxIfUnselected('#edit-regulatory-functions-13')
+});
 
 
-         When('I click on archive against an advice', function () {
-                    return shared
-                    .clickLinkByPureText('Archive')
-                    .assert.containsText('h1.heading-xlarge','Are you sure you want to archive this advice?')
+Then('I see advice uploaded successfully', function () {
+  return shared
+      .clickLinkByPureText('Auto-test-NewAdvice')
 
-                  });
-
-
-         When('I select file to remove', function () {
-         return client
-         .click('.form-checkbox')
-         .click('#edit-files-remove-button')
-         .pause(6000)
-         });
+      .assert.containsText('.filename', 'test')
+});
 
 
-         Then('I should be able to upload new file', function () {
+When('I click on edit against an advice', function () {
+  return shared
+      .clickLinkByPureText('Edit')
+      .assert.containsText('h1.heading-xlarge', 'Edit document type')
 
-         });
-
-
-         Then('I see new file in the advice detail page', function () {
-           return client
-          .assert.containsText('.file','Advice Issued - 01 - Attic Descriptions v2_1')
-          .click('#edit-save')
-
-         });
-
-         When('I enter reason {string}', function (string) {
-                    return client
-                    .setValue('#edit-archive-reason',string)
-                  });
+});
 
 
+When('I click on archive against an advice', function () {
+  return shared
+      .clickLinkByPureText('Archive')
+      .assert.containsText('h1.heading-xlarge', 'Are you sure you want to archive this advice?')
 
-                  Then('I should archive successfully', function () {
-                    return client
-                    .assert.containsText('h1.heading-xlarge','Advice')
-                   //.assert.containsText('.views-field views-field-par-status','Archived')
-                  });
+});
+
+
+When('I select file to remove', function () {
+  return client
+      .click('.form-checkbox')
+      .click('#edit-files-remove-button')
+      .pause(6000)
+});
+
+
+Then('I should be able to upload new file', function () {
+
+});
+
+
+Then('I see new file in the advice detail page', function () {
+  return client
+      .assert.containsText('.file', 'Advice Issued - 01 - Attic Descriptions v2_1')
+      .click('#edit-save')
+
+});
+
+When('I enter reason {string}', function (string) {
+  return client
+      .setValue('#edit-archive-reason', string)
+});
+
+
+Then('I should archive successfully', function () {
+  return client
+      .assert.containsText('h1.heading-xlarge', 'Advice')
+  //.assert.containsText('.views-field views-field-par-status','Archived')
+});
 
