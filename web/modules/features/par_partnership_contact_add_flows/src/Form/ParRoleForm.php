421f3d1a7cf2bf0faf1cc14358e8cecfcf5d592f
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2019-07-05 18:15:28 +0100

    PAR-1412: Added new contact flows.

---
+++ web/modules/features/par_partnership_contact_add_flows/src/Form/ParRoleForm.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\par_partnership_contact_add_flows\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\par_data\Entity\ParDataPerson;
+use Drupal\par_data\Entity\ParDataPremises;
+use Drupal\par_flows\Form\ParBaseForm;
+use Drupal\par_forms\Plugin\ParForm\ParChooseAccount;
+use Drupal\par_partnership_contact_add_flows\ParFlowAccessTrait;
+use Drupal\user\Entity\Role;
+use Drupal\user\Entity\User;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * The form for choosing which role to grant a user.
+ */
+class ParRoleForm extends ParBaseForm {
+
+  use ParFlowAccessTrait;
+
+  /**
+   * Set the page title.
+   */
+  protected $pageTitle = 'What type of user would you like to create?';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadData() {
+    // Set the user account that is being updated as a parameter for plugins to access
+    $choose_account_cid = $this->getFlowNegotiator()->getFormKey('choose_account');
+    $account_selection = $this->getFlowDataHandler()->getDefaultValues('account', NULL, $choose_account_cid);
+    $account = ParChooseAccount::getUserAccount($account_selection);
+
+    if ($account) {
+      $this->getFlowDataHandler()->setParameter('user', $account);
+    }
+
+    parent::loadData();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Skip the invitation process if a user id has already been matched
+    // or the user has chosen not to add a user.
+    $choose_account_cid = $this->getFlowNegotiator()->getFormKey('choose_account');
+    $account_selection = $this->getFlowDataHandler()->getDefaultValues('account', NULL, $choose_account_cid);
+    $account = $this->getFlowDataHandler()->getParameter('user');
+
+    if (!$account && $account_selection !== ParChooseAccount::CREATE) {
+      $url = $this->getUrlGenerator()
+        ->generateFromRoute($this->getFlowNegotiator()
+          ->getFlow()
+          ->getNextRoute('next'), $this->getRouteParams());
+      return new RedirectResponse($url);
+    }
+
+    return parent::buildForm($form, $form_state);
+  }
+}
