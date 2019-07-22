1dca026016eef9dfe7794b6ad50fe1dfade03446
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2019-07-22 20:20:13 +0100

    PAR-1412: Added limited contact form.

--- web/modules/custom/par_forms/src/Plugin/ParForm/ParContactDetailsForm.php
+++ web/modules/custom/par_forms/src/Plugin/ParForm/ParContactDetailsForm.php
@@ -6,7 +6,12 @@ use Drupal\par_forms\ParFormBuilder;
 use Drupal\par_forms\ParFormPluginBase;
 
 /**
- * Contact details form plugin.
+ * Contact details form plugin with limited email field.
+ *
+ * This form should be used in any situation the contact details are being
+ * updated indirectly as part of the contact record on another data item and
+ * where new contact records can be added to this data item, for example
+ * when updating the partnership contact records.
  *
  * @ParForm(
  *   id = "contact_details",
