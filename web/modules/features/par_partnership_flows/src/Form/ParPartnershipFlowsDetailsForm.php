9476db7f591c34b89ecbb7dcac805f4fcb9807e3
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2019-07-22 10:41:51 +0100

    PAR-1412: Debugging.

--- web/modules/features/par_partnership_flows/src/Form/ParPartnershipFlowsDetailsForm.php
+++ web/modules/features/par_partnership_flows/src/Form/ParPartnershipFlowsDetailsForm.php
@@ -59,149 +59,7 @@ class ParPartnershipFlowsDetailsForm extends ParBaseForm {
     // Display all the information that can be modified by the organisation.
     $par_data_organisation = $par_data_partnership->getOrganisation(TRUE);
 
-    // Display the primary address along with the link to edit it.
-    $form['registered_address'] = $this->renderSection('Organisation address', $par_data_organisation, ['field_premises' => 'summary'], ['edit-entity', 'add'], TRUE, TRUE);
 
-    // View and perform operations on the information about the business.
-    $form['about_business'] = $this->renderSection('About the organisation', $par_data_organisation, ['comments' => 'about'], ['edit-field']);
-
-    // Only show SIC Codes and Employee number if the partnership is a direct
-    // partnership.
-    if ($par_data_partnership->isDirect()) {
-      // Add the SIC Codes with the relevant operational links.
-      $form['sic_codes'] = $this->renderSection('Standard industrial classification (SIC) codes', $par_data_organisation, ['field_sic_code' => 'full'], ['edit-field', 'add']);
-
-      // Add the number of employees with a link to edit the field.
-      $form['employee_no'] = $this->renderSection('Number of Employees', $par_data_organisation, ['employees_band' => 'full'], ['edit-field']);
-    }
-
-    // Only show Members for coordinated partnerships.
-    if ($par_data_partnership->isCoordinated()) {
-      $membership_count = $par_data_partnership->countMembers();
-
-      // If the organisation details, and there are already some members.
-      if ($this->getFlowNegotiator()->getFlowName() === 'partnership_coordinated'
-        && $membership_count >= 1) {
-        $form['members_link'] = [
-          '#type' => 'fieldset',
-          '#title' => t('Number of members'),
-          '#attributes' => ['class' => 'form-group'],
-          '#collapsible' => FALSE,
-          '#collapsed' => FALSE,
-        ];
-        $form['members_link']['count'] = [
-          '#type' => 'markup',
-          '#markup' => "<p>{$membership_count}</p>",
-        ];
-        $form['members_link']['link'] = [
-          '#type' => 'markup',
-          '#markup' => t('@link', [
-            '@link' => Link::createFromRoute('Show members list', 'view.members_list.member_list_coordinator', $this->getRouteParams())->toString(),
-          ]),
-          '#prefix' => '<p>',
-          '#suffix' => '</p>',
-        ];
-      }
-      // If the organisation details and there aren't yet any members.
-      elseif ($this->getFlowNegotiator()->getFlowName() === 'partnership_coordinated') {
-        $form['associations'] = $this->renderSection('Number of members', $par_data_organisation, ['size' => 'full'], ['edit-field']);
-
-        $form['associations']['add_link'] = [
-          '#type' => 'markup',
-          '#markup' => t('@link', [
-            '@link' => Link::createFromRoute('Add a member', 'par_member_add_flows.add_organisation_name', $this->getRouteParams())->toString(),
-          ]),
-          '#weight' => -100,
-          '#prefix' => '<p>',
-          '#suffix' => '</p>',
-        ];
-        $form['associations']['upload_link'] = [
-          '#type' => 'markup',
-          '#markup' => t('@link', [
-            '@link' => Link::createFromRoute('Upload a Member List (CSV)', 'par_member_upload_flows.member_upload', $this->getRouteParams())->toString(),
-          ]),
-          '#weight' => -100,
-          '#prefix' => '<p>',
-          '#suffix' => '</p>',
-        ];
-      }
-      // In all other cases show the inline member summary.
-      else {
-        // Display all the members in basic form for authority users.
-        $form['members'] = $this->renderSection('Members', $par_data_partnership, ['field_coordinated_business' => 'title']);
-      }
-    }
-
-    // Display all the legal entities along with the links for the allowed
-    // operations on these.
-    $operations = [];
-    $checkbox = $this->getInformationCheckbox();
-
-    if ($checkbox === 'partnership_info_agreed_business' && !$par_data_partnership->getBoolean($checkbox)) {
-      // PAR-1354 - active partnerships do not allow for associated legal entities to be altered or new entities to be added.
-      if ($par_data_partnership->inProgress()) {
-        $operations = ['edit-entity','add'];
-      }
-    }
-    $form['legal_entities'] = $this->renderSection('Legal entities', $par_data_partnership, ['field_legal_entity' => 'summary'], $operations);
-
-    // Display all the trading names along with the links for the allowed
-    // operations on these.
-    $form['trading_names'] = $this->renderSection('Trading names', $par_data_organisation, ['trading_name' => 'full'], ['edit-field', 'add']);
-
-    // Everything below is for the authority to edit and add to.
-    $par_data_authority = $par_data_partnership->getAuthority(TRUE);
-    $form['authority'] = [
-      '#type' => 'markup',
-      '#markup' => $par_data_authority ? $par_data_authority->get('authority_name')->getString() : '',
-      '#prefix' => '<h1>',
-      '#suffix' => '</h1>',
-    ];
-
-    // Display details about the partnership for information.
-    $form['partnership_since'] = $this->renderSection('In partnership since', $par_data_partnership, ['approved_date' => 'full']);
-
-    // Display details about the partnership for information.
-    $form['regulatory_functions'] = $this->renderSection('Partnered for', $par_data_partnership, ['field_regulatory_function' => 'full']);
-
-    // Display details about the partnership for information.
-    $form['about_partnership'] = $this->renderSection('About the partnership', $par_data_partnership, ['about_partnership' => 'about'], ['edit-field']);
-
-    $form['inspection_plans'] = [
-      '#type' => 'fieldset',
-      '#title' => t('Inspection plans'),
-      '#attributes' => ['class' => 'form-group'],
-      '#collapsible' => FALSE,
-      '#collapsed' => FALSE,
-    ];
-
-    $form['inspection_plans']['link'] = [
-      '#type' => 'markup',
-      '#markup' => t('@link', [
-        '@link' => $this->getFlowNegotiator()->getFlow()
-          ->getNextLink('inspection_plans')
-          ->setText('See all Inspection Plans')
-          ->toString(),
-      ]),
-    ];
-
-    $form['advice'] = [
-      '#type' => 'fieldset',
-      '#title' => t('Advice and Documents'),
-      '#attributes' => ['class' => 'form-group'],
-      '#collapsible' => FALSE,
-      '#collapsed' => FALSE,
-    ];
-
-    $form['advice']['link'] = [
-      '#type' => 'markup',
-      '#markup' => t('@link', [
-        '@link' => $this->getFlowNegotiator()->getFlow()
-          ->getNextLink('advice')
-          ->setText('See all Advice')
-          ->toString(),
-      ]),
-    ];
 
 
     // Display the authority contacts for information.
@@ -432,6 +290,153 @@ class ParPartnershipFlowsDetailsForm extends ParBaseForm {
         ],
       ];
     }
+    // Contacts stop here.
+
+    // Display the primary address along with the link to edit it.
+    $form['registered_address'] = $this->renderSection('Organisation address', $par_data_organisation, ['field_premises' => 'summary'], ['edit-entity', 'add'], TRUE, TRUE);
+
+    // View and perform operations on the information about the business.
+    $form['about_business'] = $this->renderSection('About the organisation', $par_data_organisation, ['comments' => 'about'], ['edit-field']);
+
+    // Only show SIC Codes and Employee number if the partnership is a direct
+    // partnership.
+    if ($par_data_partnership->isDirect()) {
+      // Add the SIC Codes with the relevant operational links.
+      $form['sic_codes'] = $this->renderSection('Standard industrial classification (SIC) codes', $par_data_organisation, ['field_sic_code' => 'full'], ['edit-field', 'add']);
+
+      // Add the number of employees with a link to edit the field.
+      $form['employee_no'] = $this->renderSection('Number of Employees', $par_data_organisation, ['employees_band' => 'full'], ['edit-field']);
+    }
+
+    // Only show Members for coordinated partnerships.
+    if ($par_data_partnership->isCoordinated()) {
+      $membership_count = $par_data_partnership->countMembers();
+
+      // If the organisation details, and there are already some members.
+      if ($this->getFlowNegotiator()->getFlowName() === 'partnership_coordinated'
+        && $membership_count >= 1) {
+        $form['members_link'] = [
+          '#type' => 'fieldset',
+          '#title' => t('Number of members'),
+          '#attributes' => ['class' => 'form-group'],
+          '#collapsible' => FALSE,
+          '#collapsed' => FALSE,
+        ];
+        $form['members_link']['count'] = [
+          '#type' => 'markup',
+          '#markup' => "<p>{$membership_count}</p>",
+        ];
+        $form['members_link']['link'] = [
+          '#type' => 'markup',
+          '#markup' => t('@link', [
+            '@link' => Link::createFromRoute('Show members list', 'view.members_list.member_list_coordinator', $this->getRouteParams())->toString(),
+          ]),
+          '#prefix' => '<p>',
+          '#suffix' => '</p>',
+        ];
+      }
+      // If the organisation details and there aren't yet any members.
+      elseif ($this->getFlowNegotiator()->getFlowName() === 'partnership_coordinated') {
+        $form['associations'] = $this->renderSection('Number of members', $par_data_organisation, ['size' => 'full'], ['edit-field']);
+
+        $form['associations']['add_link'] = [
+          '#type' => 'markup',
+          '#markup' => t('@link', [
+            '@link' => Link::createFromRoute('Add a member', 'par_member_add_flows.add_organisation_name', $this->getRouteParams())->toString(),
+          ]),
+          '#weight' => -100,
+          '#prefix' => '<p>',
+          '#suffix' => '</p>',
+        ];
+        $form['associations']['upload_link'] = [
+          '#type' => 'markup',
+          '#markup' => t('@link', [
+            '@link' => Link::createFromRoute('Upload a Member List (CSV)', 'par_member_upload_flows.member_upload', $this->getRouteParams())->toString(),
+          ]),
+          '#weight' => -100,
+          '#prefix' => '<p>',
+          '#suffix' => '</p>',
+        ];
+      }
+      // In all other cases show the inline member summary.
+      else {
+        // Display all the members in basic form for authority users.
+        $form['members'] = $this->renderSection('Members', $par_data_partnership, ['field_coordinated_business' => 'title']);
+      }
+    }
+
+    // Display all the legal entities along with the links for the allowed
+    // operations on these.
+    $operations = [];
+    $checkbox = $this->getInformationCheckbox();
+
+    if ($checkbox === 'partnership_info_agreed_business' && !$par_data_partnership->getBoolean($checkbox)) {
+      // PAR-1354 - active partnerships do not allow for associated legal entities to be altered or new entities to be added.
+      if ($par_data_partnership->inProgress()) {
+        $operations = ['edit-entity','add'];
+      }
+    }
+    $form['legal_entities'] = $this->renderSection('Legal entities', $par_data_partnership, ['field_legal_entity' => 'summary'], $operations);
+
+    // Display all the trading names along with the links for the allowed
+    // operations on these.
+    $form['trading_names'] = $this->renderSection('Trading names', $par_data_organisation, ['trading_name' => 'full'], ['edit-field', 'add']);
+
+    // Everything below is for the authority to edit and add to.
+    $par_data_authority = $par_data_partnership->getAuthority(TRUE);
+    $form['authority'] = [
+      '#type' => 'markup',
+      '#markup' => $par_data_authority ? $par_data_authority->get('authority_name')->getString() : '',
+      '#prefix' => '<h1>',
+      '#suffix' => '</h1>',
+    ];
+
+    // Display details about the partnership for information.
+    $form['partnership_since'] = $this->renderSection('In partnership since', $par_data_partnership, ['approved_date' => 'full']);
+
+    // Display details about the partnership for information.
+    $form['regulatory_functions'] = $this->renderSection('Partnered for', $par_data_partnership, ['field_regulatory_function' => 'full']);
+
+    // Display details about the partnership for information.
+    $form['about_partnership'] = $this->renderSection('About the partnership', $par_data_partnership, ['about_partnership' => 'about'], ['edit-field']);
+
+    $form['inspection_plans'] = [
+      '#type' => 'fieldset',
+      '#title' => t('Inspection plans'),
+      '#attributes' => ['class' => 'form-group'],
+      '#collapsible' => FALSE,
+      '#collapsed' => FALSE,
+    ];
+
+    $form['inspection_plans']['link'] = [
+      '#type' => 'markup',
+      '#markup' => t('@link', [
+        '@link' => $this->getFlowNegotiator()->getFlow()
+          ->getNextLink('inspection_plans')
+          ->setText('See all Inspection Plans')
+          ->toString(),
+      ]),
+    ];
+
+    $form['advice'] = [
+      '#type' => 'fieldset',
+      '#title' => t('Advice and Documents'),
+      '#attributes' => ['class' => 'form-group'],
+      '#collapsible' => FALSE,
+      '#collapsed' => FALSE,
+    ];
+
+    $form['advice']['link'] = [
+      '#type' => 'markup',
+      '#markup' => t('@link', [
+        '@link' => $this->getFlowNegotiator()->getFlow()
+          ->getNextLink('advice')
+          ->setText('See all Advice')
+          ->toString(),
+      ]),
+    ];
+
+    // Contacts go here.
 
     // Helptext.
     $form['help_text'] = [
