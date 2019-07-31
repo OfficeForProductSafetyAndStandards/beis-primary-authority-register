c0bb398f4d3578c0e433eb77aa294aefacaec91d
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2019-07-31 08:43:40 +0100

    PAR-1450: Added extra contact validation for partnerships.

--- web/modules/features/par_partnership_contact_add_flows/src/Form/ParContactForm.php
+++ web/modules/features/par_partnership_contact_add_flows/src/Form/ParContactForm.php
@@ -35,4 +35,36 @@ class ParContactForm extends ParBaseForm {
     parent::loadData();
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $par_data_partnership = $this->getFlowDataHandler()->getParameter('par_data_partnership');
+    switch ($this->getFlowDataHandler()->getParameter('type')) {
+      case 'authority':
+        $contacts = $par_data_partnership->getAuthorityPeople();
+        break;
+
+      case 'organisation':
+        $contacts = $par_data_partnership->getOrganisationPeople();
+
+        break;
+
+      default:
+        $contacts = [];
+
+    }
+
+    foreach ($contacts as $contact) {
+      if (!empty($form_state->getValue('email'))
+          && $contact->getEmail() === $form_state->getValue('email')) {
+        $id = $this->getElementId(['email'], $form);
+        $form_state->setErrorByName($this->getElementName('email'), $this->wrapErrorMessage('This person has already been added, you cannot add the same person again. Please change the e-mail address and try again..', $id));
+
+      }
+    }
+
+    parent::validateForm($form, $form_state);
+  }
+
 }
