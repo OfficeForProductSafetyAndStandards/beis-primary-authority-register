b3969dbbfcf68a2c892dbafa024acb3c23cfc1a8
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2019-09-26 09:54:40 +0100

    PAR-1462: Improved performance.

--- web/modules/custom/par_data/src/ParDataManager.php
+++ web/modules/custom/par_data/src/ParDataManager.php
@@ -783,14 +783,19 @@ class ParDataManager implements ParDataManagerInterface {
    *   An array of entities to turn into options.
    * @param array $options
    *   An optional array of options to append to.
+   * @param string $view_mode
+   *   A view mode to render the entity as.
+   * @param bool $access_check
+   *   Whether to check all entities for access, this is an expensive operation so not enabled by default.
    *
    * @return []
    *   An array of options keyed by entity id.
    */
-  public function getEntitiesAsOptions($entities, $options = [], $view_mode = NULL) {
+  public function getEntitiesAsOptions($entities, $options = [], $view_mode = NULL, $access_check = FALSE) {
     foreach ($entities as $entity) {
       if ($entity instanceof EntityInterface) {
-        if ($entity instanceof ParDataEntityInterface && !$entity->access('view', $this->getCurrentUser())) {
+        if ($entity instanceof ParDataEntityInterface && !$entity->isDeleted()
+          && (!$access_check || !$entity->access('view', $this->getCurrentUser()))) {
           continue;
         }
 
