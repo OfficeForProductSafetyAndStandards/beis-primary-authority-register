329edddc46f4a82a35785dcd32018c904aeb2615
Jon <jon.edery@googlemail.com>
2019-10-02 12:25:26 +0100

    PAR-19 text fix.

--- web/modules/features/par_partnership_flows/src/Controller/ParPartnershipFlowsInspectionPlanPageController.php
+++ web/modules/features/par_partnership_flows/src/Controller/ParPartnershipFlowsInspectionPlanPageController.php
@@ -13,7 +13,7 @@ use Drupal\par_partnership_flows\ParPartnershipFlowAccessTrait;
 use Drupal\par_partnership_flows\ParPartnershipFlowsTrait;
 
 /**
- * The Advice entity details page.
+ * The inspection plan entity details page.
  */
 class ParPartnershipFlowsInspectionPlanPageController extends ParBaseController {
 
@@ -46,7 +46,7 @@ class ParPartnershipFlowsInspectionPlanPageController extends ParBaseController 
       ];
       $build['inspection_plan_details']['archived'] = [
         '#type' => 'markup',
-        '#markup' => 'Inspection plan advice',
+        '#markup' => 'Archived inspection plan',
         '#prefix' => '<h2>',
         '#suffix' => '</h2>',
       ];
