ac4aa42554d40934593ede505fb0333b4563a74b
Jon <jon.edery@googlemail.com>
2019-10-22 16:51:36 +0100

    PAR-19 extend access automation test functionality for uploading..

--- tests/step-definitions/inspection_plan_access_steps.js
+++ tests/step-definitions/inspection_plan_access_steps.js
@@ -12,3 +12,30 @@ Then('I see the inspection plan link {string}', function (string) {
     return client
         .assert.containsText('a.flow-link', string)
 });
+
+Given('I open inspection plan add page', function () {
+    return shared
+        .clickLinkByPureText('See all Inspection Plans')
+        .clickLinkByPureText('Upload inspection plan')
+});
+
+Then('I enter inspection plan title', function () {
+    return client
+        .clearValue('#edit-title')
+        .setValue('#edit-title', 'Auto-test-NewInspectionPlan')
+
+});
+
+Then('I enter summary of inspection plan', function () {
+    return client
+        .clearValue('#edit-summary')
+        .setValue('#edit-summary', 'Auto-advice-NewInspectionPlan')
+
+});
+
+Then('I see inspection plan uploaded successfully', function () {
+    return shared
+        .clickLinkByPureText('Auto-test-NewInspectionPlan')
+        .assert.containsText('.filename', 'test')
+});
+
