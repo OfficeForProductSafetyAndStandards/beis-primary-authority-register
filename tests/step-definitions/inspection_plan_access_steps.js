278e95e03ae45d76f51767c5a19759b887d9fb44
Jon <jon.edery@googlemail.com>
2019-10-22 20:45:08 +0100

    PAR-19 automated tests extend JS functions.

--- tests/step-definitions/inspection_plan_access_steps.js
+++ tests/step-definitions/inspection_plan_access_steps.js
@@ -39,3 +39,34 @@ Then('I see inspection plan uploaded successfully', function () {
         .assert.containsText('.filename', 'test')
 });
 
+When('I click on edit against an inspection plan', function () {
+    return shared
+        .clickLinkByPureText('Edit')
+        .assert.containsText('h1.heading-xlarge', 'Edit inspection plan details')
+
+});
+
+Then('I enter new inspection plan title', function () {
+    return client
+        .clearValue('#edit-title')
+        .setValue('#edit-title', 'Auto-test-NewInspectionPlan-retest')
+
+});
+
+Then('I enter new summary for an inspection plan', function () {
+    return client
+        .clearValue('#edit-summary')
+        .setValue('#edit-summary', 'Auto-inspection-plan-Summary-retest')
+
+});
+
+Then('I see the inspection plan has updated successfully', function () {
+    return shared
+        .clickLinkByPureText('Auto-test-NewInspectionPlan-retest')
+        .assert.containsText('h1.heading-xlarge', 'Auto-test-NewInspectionPlan-retest')
+        .assert.containsText('#content', 'Auto-inspection-plan-Summary-retest')
+});
+
+
+
+
