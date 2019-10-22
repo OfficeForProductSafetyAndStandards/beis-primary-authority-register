669c0b6daf536606326a10979494113fbc76e152
Jon <jon.edery@googlemail.com>
2019-10-22 20:47:15 +0100

    PAR-19 automated tests extend JS functions.

--- tests/step-definitions/inspection_plan_access_steps.js
+++ tests/step-definitions/inspection_plan_access_steps.js
@@ -70,8 +70,7 @@ Then('I see the inspection plan has updated successfully', function () {
 When('I click on revoke against an inspection plan', function () {
     return shared
         .clickLinkByPureText('Revoke')
-        .assert.containsText('h1.heading-xlarge', 'Are you sure you want to archive this advice?')
-
+        .assert.containsText('h1.heading-xlarge', 'Are you sure you want to revoke this inspection plan?')
 });
 
 
