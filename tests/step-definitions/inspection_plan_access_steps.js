ed84b403cc5e5e9d2f4d20807828bb94eb807623
Jon <jon.edery@googlemail.com>
2019-10-23 00:26:04 +0100

    PAR-19 fix automated test

--- tests/step-definitions/inspection_plan_access_steps.js
+++ tests/step-definitions/inspection_plan_access_steps.js
@@ -10,7 +10,7 @@ Then('I should see inspection plans list page', function () {
 
 Then('I should see inspection plans search list page', function () {
     return shared
-        .assert.containsText('h1.heading-xlarge', 'Partnership Search Inspection plans')
+        .assert.containsText('h1.heading-xlarge', 'Inspection plans')
 });
 
 Then('I see the inspection plan link {string}', function (string) {
