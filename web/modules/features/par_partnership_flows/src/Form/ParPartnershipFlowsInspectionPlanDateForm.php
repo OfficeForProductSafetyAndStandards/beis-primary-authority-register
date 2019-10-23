5fc31b4eaede1a356237b851879311970c3e3eee
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2019-10-23 13:54:49 +0100

    Inspection plan saving update.

--- web/modules/features/par_partnership_flows/src/Form/ParPartnershipFlowsInspectionPlanDateForm.php
+++ web/modules/features/par_partnership_flows/src/Form/ParPartnershipFlowsInspectionPlanDateForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\par_partnership_flows\Form;
 
+use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\par_data\Entity\ParDataInspectionPlan;
 use Drupal\par_data\Entity\ParDataPartnership;
@@ -29,6 +30,78 @@ class ParPartnershipFlowsInspectionPlanDateForm extends ParBaseForm {
     $this->pageTitle = $inspection_plan ? 'Change the expiry date' : 'When does this inspeciton plan expire?';
 
     return parent::titleCallback();
+  }
+
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    // Get the inspection plan entity from the URL.
+    $par_data_inspection_plan = $this->getFlowDataHandler()->getParameter('par_data_inspection_plan');
+
+    // Get files from "upload" step.
+    $file_upload_cid = $this->getFlowNegotiator()->getFormKey('upload');
+    $inspection_details_cid = $this->getFlowNegotiator()->getFormKey('details');
+
+    // Create new inspection plan if needed.
+    if (!$par_data_inspection_plan) {
+      $par_data_inspection_plan = ParDataInspectionPlan::create([
+        'type' => 'inspection_plan',
+      ]);
+    }
+
+    // Set the inspection plan title.
+    $par_data_inspection_plan->set('title', $this->getFlowDataHandler()->getTempDataValue('title'));
+
+    // Set the inspection plan summary.
+    $par_data_inspection_plan->set('summary', $this->getFlowDataHandler()->getTempDataValue('summary'));
+
+    // Set the status to active for the inspection plan entity.
+    $par_data_inspection_plan->setParStatus('current');
+
+    // Add files if required.
+    $file_ids = $this->getFlowDataHandler()->getDefaultValues('inspection_plan_files', [], $file_upload_cid);
+    $files = !empty($file_ids) ? File::loadMultiple((array) $file_ids) : NULL;
+    if ($files) {
+      $par_data_inspection_plan->set('document', $files);
+    }
+
+    $request_date = DrupalDateTime::createFromTimestamp(time(), NULL, ['validate_format' => FALSE]);
+    if ($par_data_inspection_plan->isNew()) {
+      $inspection_plan_start_date =  $request_date->format("Y-m-d");
+    }
+    else {
+      $inspection_plan_start_date =  $par_data_inspection_plan->get('valid_date')->value;
+    }
+
+    // set the expire time for inspection plan entities.
+    $inspection_plan_end_date = $this->getFlowDataHandler()->getTempDataValue('inspection_plan_expire');
+    // Set date range values for inspection plans.
+    $par_data_inspection_plan->set('valid_date', ['value' => $inspection_plan_start_date, 'end_value' => $inspection_plan_end_date]);
+
+    // Save and attach new inspection plan entities.
+    if ($par_data_inspection_plan->save()) {
+      if ($par_data_inspection_plan->isNew()) {
+        $par_data_partnership = $this->getFlowDataHandler()->getParameter('par_data_partnership');
+        $par_data_partnership->get('field_inspection_plan')->appendItem($par_data_inspection_plan->id());
+        $par_data_partnership->save();
+      }
+
+      $this->getFlowDataHandler()->deleteStore();
+    }
+    // Log an error.
+    else {
+      $message = $this->t('This %inspection plan could not be saved for %form_id');
+      $replacements = [
+        '%inspection plan' => $par_data_inspection_plan->label(),
+        '%form_id' => $this->getFormId(),
+      ];
+      $this->getLogger($this->getLoggerChannel())->error($message, $replacements);
+    }
   }
 
 }
