ac756b55a64688619c7b2892d4e794384b6b974a
Jon <jon.edery@googlemail.com>
2019-11-07 11:06:17 +0000

    PAR-1497 comment update.

--- web/modules/custom/par_data/src/Entity/ParDataInspectionPlan.php
+++ web/modules/custom/par_data/src/Entity/ParDataInspectionPlan.php
@@ -155,7 +155,7 @@ class ParDataInspectionPlan extends ParDataEntity {
 
   /**
    * Helper function for entities that need to update a date value to be inline with a revoke timestamp.
-   * when a parent entity is is re-activated. i.e. when a partnership is revoked and re-activated we want
+   *  i.e. when a partnership is revoked and re-activated we want
    * to ensure that historically revoked inspection plans are not also re-activate.
    *
    */
