a259eac5c111e4e34e32dbfb0a88f018c6ea84cc
Jon <jon.edery@googlemail.com>
2020-03-03 11:19:43 +0000

    PAR-1540 comment update.

--- web/modules/custom/par_data/src/Entity/ParDataLegalEntity.php
+++ web/modules/custom/par_data/src/Entity/ParDataLegalEntity.php
@@ -180,7 +180,8 @@ class ParDataLegalEntity extends ParDataEntity {
    *  Helper function to determine if a legal entity is referenced by more then one partnership entities.
    *
    * @return Boolean
-   *   TRUE if the legal entity is being referenced in another partnership.
+   *  TRUE if the legal entity is being referenced in another partnership.
+   *  FALSE if the  
    */
   public function HasExistingPartnershipReferences() {
     $relationships = $this->getRelationships('par_data_partnership', NULL, TRUE);
