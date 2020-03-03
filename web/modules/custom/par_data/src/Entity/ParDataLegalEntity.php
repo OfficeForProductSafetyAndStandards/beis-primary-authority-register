d9840fea8c16a30cac0b17c156df6d1215fd1475
Jon <jon.edery@googlemail.com>
2020-03-03 11:20:40 +0000

    PAR-1540 comment update.

--- web/modules/custom/par_data/src/Entity/ParDataLegalEntity.php
+++ web/modules/custom/par_data/src/Entity/ParDataLegalEntity.php
@@ -181,7 +181,7 @@ class ParDataLegalEntity extends ParDataEntity {
    *
    * @return Boolean
    *  TRUE if the legal entity is being referenced in another partnership.
-   *  FALSE if the legal entity has no 
+   *  FALSE if the legal entity has no references
    */
   public function HasExistingPartnershipReferences() {
     $relationships = $this->getRelationships('par_data_partnership', NULL, TRUE);
