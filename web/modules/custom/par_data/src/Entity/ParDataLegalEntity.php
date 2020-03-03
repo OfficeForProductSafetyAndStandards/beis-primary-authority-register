4c34ac237ad39cc6c3de35e96df1776593ad232a
Jon <jon.edery@googlemail.com>
2020-03-03 11:21:13 +0000

    PAR-1540 comment update.

--- web/modules/custom/par_data/src/Entity/ParDataLegalEntity.php
+++ web/modules/custom/par_data/src/Entity/ParDataLegalEntity.php
@@ -177,7 +177,7 @@ class ParDataLegalEntity extends ParDataEntity {
   }
 
   /**
-   *  Helper function to determine if a legal entity is referenced by more then one partnership entities.
+   *  Helper function to determine if a legal entity is referenced by more then one partnership entity.
    *
    * @return Boolean
    *  TRUE if the legal entity is being referenced in another partnership.
