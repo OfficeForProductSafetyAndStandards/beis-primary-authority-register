b2e42dd8b32bfbeff8bb5083603c0fffd3a39166
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2020-03-09 19:57:04 +0000

    Test

--- scripts/db-backup.sh
+++ scripts/db-backup.sh
@@ -103,12 +103,12 @@ mkdir -p $DIRECTORY
 rm -f $DIRECTORY/$FILE_NAME.sql
 
 printf "Exporting database dump...\n"
-printf "Running: drush $DRUPAL_ALIAS sql-dump --result-file='$DIRECTORY/${FILE_NAME}.sql'\n"
+printf "Running: drush $DRUPAL_ALIAS sql-dump --result-file='$DIRECTORY/$FILE_NAME.sql'\n"
 ../vendor/drush/drush/drush $DRUPAL_ALIAS sql-dump --result-file="$DIRECTORY/${FILE_NAME}_raw.sql"
 
 ## Remove erroneous pg_dump roles applied despite --no-owner flags.
 ## https://stackoverflow.com/questions/60605491/using-pg-dump-with-no-owner-information-still-outputting-role-information
-sed -e '/-- do not execute if not member of manager role/,+4d' $DIRECTORY/$FILE_NAME_raw.sql > $DIRECTORY/$FILE_NAME.sql
+sed -e '/-- do not execute if not member of manager role/,+4d' $DIRECTORY/${FILE_NAME}_raw.sql > $DIRECTORY/$FILE_NAME.sql
 
 printf "Packaging database dump...\n"
 tar -zcvf $DIRECTORY/$FILE_NAME-latest.tar.gz -C $DIRECTORY "$FILE_NAME.sql"
