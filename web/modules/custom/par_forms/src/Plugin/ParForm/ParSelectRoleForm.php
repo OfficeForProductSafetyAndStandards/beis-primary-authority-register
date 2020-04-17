9a84ecaccb8c58b4c7909d07ff5a4c6fdcde50f6
Jon <jon.edery@googlemail.com>
2020-04-17 12:02:34 +0100

    PAR-1582 updating deprecated internal method.

--- web/modules/custom/par_forms/src/Plugin/ParForm/ParSelectRoleForm.php
+++ web/modules/custom/par_forms/src/Plugin/ParForm/ParSelectRoleForm.php
@@ -106,7 +106,7 @@ class ParSelectRoleForm extends ParFormPluginBase {
     }
     // If there isn't a choice go to the next page.
     if (count($role_options) <= 1) {
-      $url = $this->getUrlGenerator()->generateFromRoute($this->getFlowNegotiator()->getFlow()->getNextRoute('next'), $this->getRouteParams());
+      $url = $this->getUrlGenerator()->generateFromRoute($this->getFlowNegotiator()->getFlow()->progressRoute('next'), $this->getRouteParams());
       return new RedirectResponse($url);
     }
 
