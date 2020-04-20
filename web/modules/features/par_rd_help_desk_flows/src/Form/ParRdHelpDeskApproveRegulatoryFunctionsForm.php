4f5e6554413d1e42985274d34740c4eb16927215
Jon <jon.edery@googlemail.com>
2020-04-20 17:02:59 +0100

    PAR-1583  deprecated method updated,

--- web/modules/features/par_rd_help_desk_flows/src/Form/ParRdHelpDeskApproveRegulatoryFunctionsForm.php
+++ web/modules/features/par_rd_help_desk_flows/src/Form/ParRdHelpDeskApproveRegulatoryFunctionsForm.php
@@ -78,7 +78,7 @@ class ParRdHelpDeskApproveRegulatoryFunctionsForm extends ParBaseForm {
         // If the partnership could not be saved the application can't be progressed.
         // @TODO Find a better way to alert the user without redirecting them away from the form.
         $this->messenger()->addMessage('There was an error approving this partnership, please check it is ready to be approved.');
-        $form_state->setRedirect($this->getFlowNegotiator()->getFlow()->getPrevRoute('cancel'));
+        $form_state->setRedirect($this->getFlowNegotiator()->getFlow()->progressRoute('cancel'));
         return;
       }
 
