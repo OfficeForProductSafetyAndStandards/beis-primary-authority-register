66c12732ccd7382a24408d5875f6f5a4269dc3ef
Jon <jon.edery@googlemail.com>
2020-04-20 17:05:59 +0100

    PAR-1583  deprecated method updated,

--- web/modules/custom/par_forms/src/Plugin/ParForm/ParSelectOrganisationForm.php
+++ web/modules/custom/par_forms/src/Plugin/ParForm/ParSelectOrganisationForm.php
@@ -46,7 +46,7 @@ class ParSelectOrganisationForm extends ParFormPluginBase {
 
     // If no suggestions were found cancel out of the journey.
     if ($required && count($organisations) <= 0) {
-      $url = $this->getUrlGenerator()->generateFromRoute($this->getFlowNegotiator()->getFlow()->getPrevRoute('prev'), $this->getRouteParams());
+      $url = $this->getUrlGenerator()->generateFromRoute($this->getFlowNegotiator()->getFlow()->progressRoute('prev'), $this->getRouteParams());
       return new RedirectResponse($url);
     }
 
