8fef824d0f7136c09ec678265f80f9be32893e58
Jon <jon.edery@googlemail.com>
2020-05-05 13:04:22 +0100

    PAR-1582 update dep function.

--- web/modules/features/par_person_create_flows/src/Form/ParRoleForm.php
+++ web/modules/features/par_person_create_flows/src/Form/ParRoleForm.php
@@ -54,7 +54,7 @@ class ParRoleForm extends ParBaseForm {
       $url = $this->getUrlGenerator()
         ->generateFromRoute($this->getFlowNegotiator()
           ->getFlow()
-          ->getNextRoute('next'), $this->getRouteParams());
+          ->progressRoute(), $this->getRouteParams());
       return new RedirectResponse($url);
     }
 
