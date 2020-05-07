1b7bc1c4a0995df85cd275c2a03f4a8bdee684ac
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2020-05-07 14:05:26 +0100

    PAR-1504: Updated member display.

--- web/modules/custom/par_forms/src/Plugin/ParForm/ParPartnershipMembers.php
+++ web/modules/custom/par_forms/src/Plugin/ParForm/ParPartnershipMembers.php
@@ -27,12 +27,15 @@ class ParPartnershipMembers extends ParFormPluginBase {
    */
   const MEMBER_FORMAT_INLINE = 'member_list';
   const MEMBER_FORMAT_LINK = 'member_link_view';
+  const MEMBER_FORMAT_REQUEST = 'member_link_request';
 
   /**
    * {@inheritdoc}
    */
   public function loadData($cardinality = 1) {
     $par_data_partnership = $this->getFlowDataHandler()->getParameter('par_data_partnership');
+
+
 
     if ($par_data_partnership instanceof ParDataEntityInterface && $par_data_partnership->isCoordinated()) {
       // If there is a members list uploaded already.
@@ -41,17 +44,20 @@ class ParPartnershipMembers extends ParFormPluginBase {
         // Display only active members.
         $this->getFlowDataHandler()->setFormPermValue("members", $par_data_partnership->getCoordinatedMember(FALSE, TRUE));
         $this->getFlowDataHandler()->setFormPermValue("number_of_members", $membershipCount);
+
+        // Set display configuration options.
+        $available_formats = [self::MEMBER_FORMAT_INLINE, self::MEMBER_FORMAT_LINK];
+        $format = isset($this->getConfiguration()['format']) && array_search($this->getConfiguration()['format'], $available_formats) !== FALSE
+          ? $this->getConfiguration()['format'] : self::MEMBER_FORMAT_INLINE;
       }
       elseif ($numberOfMembers = $par_data_partnership->numberOfMembers()) {
         $this->getFlowDataHandler()->setFormPermValue("number_of_members", $numberOfMembers);
       }
     }
 
-    // Set display configuration options.
-    $available_formats = [self::MEMBER_FORMAT_INLINE, self::MEMBER_FORMAT_LINK];
-    $format = isset($this->getConfiguration()['format']) && array_search($this->getConfiguration()['format'], $available_formats) !== FALSE
-      ? $this->getConfiguration()['format'] : self::MEMBER_FORMAT_INLINE;
-    $this->getFlowDataHandler()->setFormPermValue("member_format", $format);
+    // If there are no members the format must be to request the member list
+    // from the coordinator.
+    $this->getFlowDataHandler()->setFormPermValue("member_format", $format ?? self::MEMBER_FORMAT_REQUEST);
 
     parent::loadData();
   }
@@ -81,123 +87,121 @@ class ParPartnershipMembers extends ParFormPluginBase {
       '#weight' => -5,
     ];
 
-    // Display the members if this partnership has any.
     $members = $this->getDefaultValuesByKey('members', $cardinality, NULL);
-    if (!empty($members)) {
-      // Show the link to view the full membership list.
-      if ($this->getFlowDataHandler()->getFormPermValue("member_format") === self::MEMBER_FORMAT_LINK) {
-        try {
-          $member_link = $this->getLinkByRoute('view.members_list.member_list_coordinator', [], [], TRUE);
-        } catch (ParFlowException $e) {
 
-        }
-        if (isset($member_link) && $member_link instanceof Link) {
-          $form['members']['list'] = [
-            '#type' => 'fieldset',
-            '#title' => t('Members'),
-            '#attributes' => ['class' => 'form-group'],
-            'link' => [
-              '#type' => 'html_tag',
-              '#tag' => 'p',
-              '#value' => $member_link->setText('show members list')->toString(),
-              '#attributes' => ['class' => ['member-list', 'member-list-link']],
-            ],
-          ];
-        }
+    // Show the link to view the full membership list.
+    if ($this->getFlowDataHandler()->getFormPermValue("member_format") === self::MEMBER_FORMAT_LINK) {
+      try {
+        $member_link = $this->getLinkByRoute('view.members_list.member_list_coordinator', [], [], TRUE);
+      } catch (ParFlowException $e) {
+
       }
-      // Show the member list inline.
-      elseif ($this->getFlowDataHandler()->getFormPermValue("member_format") === self::MEMBER_FORMAT_INLINE) {
-        // Initialize pager and get current page.
-        $number_per_page = 5;
-        $pager = $this->getUniquePager()->getPager('partnership_manage_organisation_contacts');
-        $current_pager = $this->getUniquePager()->getPagerManager()->createPager(count($members), $number_per_page, $pager);
+      if (isset($member_link) && $member_link instanceof Link) {
+        $form['members']['count']['link'] = [
+          '#type' => 'html_tag',
+          '#tag' => 'p',
+          '#value' => $member_link->setText('show members list')->toString(),
+          '#attributes' => ['class' => ['member-list', 'member-list-link']],
+        ];
+      }
+    }
+    // Show the member list inline.
+    elseif ($this->getFlowDataHandler()->getFormPermValue("member_format") === self::MEMBER_FORMAT_INLINE) {
+      // Initialize pager and get current page.
+      $number_per_page = 5;
+      $pager = $this->getUniquePager()->getPager('partnership_manage_organisation_contacts');
+      $current_pager = $this->getUniquePager()->getPagerManager()->createPager(count($members), $number_per_page, $pager);
 
-        $form['members']['list'] = [
-          '#type' => 'fieldset',
-          '#title' => t('Members'),
-          '#attributes' => ['class' => ['member-list', 'member-list-inline']],
-          'items' => [
-            '#type' => 'container',
+      $form['members']['list'] = [
+        '#type' => 'fieldset',
+        '#title' => t('Members'),
+        '#attributes' => ['class' => ['member-list', 'member-list-inline']],
+        'items' => [
+          '#type' => 'container',
+        ],
+        'pager' => [
+          '#type' => 'pager',
+          '#theme' => 'pagerer',
+          '#element' => $pager,
+          '#weight' => 100,
+          '#config' => [
+            'preset' => $this->config('pagerer.settings')
+              ->get('core_override_preset'),
           ],
-          'pager' => [
-            '#type' => 'pager',
-            '#theme' => 'pagerer',
-            '#element' => $pager,
-            '#weight' => 100,
-            '#config' => [
-              'preset' => $this->config('pagerer.settings')
-                ->get('core_override_preset'),
-            ],
+        ],
+      ];
+
+      // Split the members up into chunks:
+      $chunks = array_chunk($members, $number_per_page);
+      foreach ($chunks[$current_pager->getCurrentPage()] as $delta => $entity) {
+        if (!$entity instanceof ParDataEntityInterface) {
+          continue;
+        }
+
+        // Display the member.
+        $form['members']['list']['items'][$delta] = [
+          '#type' => 'fieldset',
+          '#attributes' => ['class' => ['grid-row', 'form-group', 'coordinated-member']],
+          '#collapsible' => FALSE,
+          '#collapsed' => FALSE,
+          'entity' => [
+            '#type' => 'html_tag',
+            '#tag' => 'div',
+            '#value' => $entity->label(),
+            '#attributes' => ['class' => ['column-full']],
           ],
         ];
-
-        // Split the members up into chunks:
-        $chunks = array_chunk($members, $number_per_page);
-        foreach ($chunks[$current_pager->getCurrentPage()] as $delta => $entity) {
-          if (!$entity instanceof ParDataEntityInterface) {
-            continue;
-          }
-
-          // Display the member.
-          $form['members']['list']['items'][$delta] = [
-            '#type' => 'fieldset',
-            '#attributes' => ['class' => ['grid-row', 'form-group', 'coordinated-member']],
-            '#collapsible' => FALSE,
-            '#collapsed' => FALSE,
-            'entity' => [
-              '#type' => 'html_tag',
-              '#tag' => 'div',
-              '#value' => $entity->label(),
-              '#attributes' => ['class' => ['column-full']],
-            ],
-//            'operations' => [
-//              'update' => [
-//                '#type' => 'html_tag',
-//                '#tag' => 'p',
-//                '#value' => !empty($update_authority_contact_link) ? $update_authority_contact_link : '',
-//                '#attributes' => ['class' => ['column-one-third']],
-//              ],
-//              'remove' => [
-//                '#type' => 'html_tag',
-//                '#tag' => 'p',
-//                '#value' => !empty($remove_authority_contact_link) ? $remove_authority_contact_link : '',
-//                '#attributes' => ['class' => ['column-two-thirds']],
-//              ],
-//            ],
-          ];
-        }
       }
     }
-
-    // Add link to add a new member.
-    try {
-      $add_member_link = $this->getLinkByRoute('par_member_add_flows.add_organisation_name', [], [], TRUE);
-    } catch (ParFlowException $e) {
-
-    }
-    if (isset($add_member_link) && $add_member_link instanceof Link) {
-      $link_label = $members && !empty($members) && count($members) >= 1 ? "add another member" : "add a member";
-      $form['members']['add'] = [
-        '#type' => 'html_tag',
-        '#tag' => 'p',
-        '#value' => $add_member_link->setText($link_label)->toString(),
-        '#attributes' => ['class' => ['add-member']],
+    // Show the member list inline.
+    elseif ($this->getFlowDataHandler()->getFormPermValue("member_format") === self::MEMBER_FORMAT_REQUEST) {
+      $form['members']['list'] = [
+        '#type' => 'fieldset',
+        '#title' => t('Members'),
+        '#attributes' => ['class' => 'form-group'],
+        'link' => [
+          '#type' => 'html_tag',
+          '#tag' => 'p',
+          '#value' => "Please request a list of members from the coordinator, they will be able to provide a list on request.",
+          '#attributes' => ['class' => ['member-list', 'member-list-link']],
+        ],
       ];
     }
 
-    // Add link to upload a new csv member list.
-    try {
-      $upload_member_link = $this->getLinkByRoute('par_member_upload_flows.member_upload', [], [], TRUE);
-    } catch (ParFlowException $e) {
+    // Operation links should not be added for the link format, where the update
+    // links will be available on the referenced page.
+    if ($this->getFlowDataHandler()->getFormPermValue("member_format") !== self::MEMBER_FORMAT_LINK) {
+      // Add link to add a new member.
+      try {
+        $add_member_link = $this->getLinkByRoute('par_member_add_flows.add_organisation_name', [], [], TRUE);
+      } catch (ParFlowException $e) {
 
-    }
-    if (isset($upload_member_link) && $upload_member_link instanceof Link) {
-      $form['members']['upload'] = [
-        '#type' => 'html_tag',
-        '#tag' => 'p',
-        '#value' => $upload_member_link->setText('upload a member list (csv)')->toString(),
-        '#attributes' => ['class' => ['upload-member']],
-      ];
+      }
+      if (isset($add_member_link) && $add_member_link instanceof Link) {
+        $link_label = $members && !empty($members) && count($members) >= 1 ? "add another member" : "add a member";
+        $form['members']['add'] = [
+          '#type' => 'html_tag',
+          '#tag' => 'p',
+          '#value' => $add_member_link->setText($link_label)->toString(),
+          '#attributes' => ['class' => ['add-member']],
+        ];
+      }
+
+      // Add link to upload a new csv member list.
+      try {
+        $upload_member_link = $this->getLinkByRoute('par_member_upload_flows.member_upload', [], [], TRUE);
+      } catch (ParFlowException $e) {
+
+      }
+      if (isset($upload_member_link) && $upload_member_link instanceof Link) {
+        $form['members']['upload'] = [
+          '#type' => 'html_tag',
+          '#tag' => 'p',
+          '#value' => $upload_member_link->setText('upload a member list (csv)')
+            ->toString(),
+          '#attributes' => ['class' => ['upload-member']],
+        ];
+      }
     }
 
     return $form;
