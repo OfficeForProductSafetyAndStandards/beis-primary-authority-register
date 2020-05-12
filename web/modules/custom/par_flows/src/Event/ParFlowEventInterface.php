ec76ef5c61755792cc9b1625c1bad2e89b57a7cf
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2020-05-12 08:01:08 +0100

    PAR-1582: submission redirection listener.

---
+++ web/modules/custom/par_flows/src/Event/ParFlowEventInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\par_flows\Event;
+
+use Drupal\Core\Url;
+
+/**
+ * The event is dispatched whenever we need to determine the next flow step.
+ */
+interface ParFlowEventInterface {
+
+  /**
+   * @return \Drupal\par_flows\Entity\ParFlowInterface
+   */
+  public function getFlow();
+
+  /**
+   * @return string
+   */
+  public function getCurrentRoute();
+
+  /**
+   * @return array
+   */
+  public function getCurrentStep();
+
+  /**
+   * Get the url to redirect to.
+   *
+   * @return \Drupal\Core\Url
+   */
+  public function getUrl();
+
+  /**
+   * Set the next url to redirect to.
+   *
+   * @param \Drupal\Core\Url $url
+   *   A url object to redirect to.
+   */
+  public function setUrl(Url $url);
+
+}
