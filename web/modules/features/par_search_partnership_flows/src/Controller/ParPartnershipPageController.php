7eff9c69b7c9e1ec674d54d94df848873966b767
Jon <jon.edery@googlemail.com>
2020-06-19 19:43:02 +0100

    PAR-1618 updates.

--- web/modules/features/par_search_partnership_flows/src/Controller/ParPartnershipPageController.php
+++ web/modules/features/par_search_partnership_flows/src/Controller/ParPartnershipPageController.php
@@ -76,7 +76,7 @@ class ParPartnershipPageController extends ParBaseController {
     ];
 
     // Display details about the partnership for information.
-    $about_partnership_display = $par_data_partnership->about_partnership->value;
+    $about_partnership_display = $par_data_partnership->about_partnership->view(['label' => 'hidden']);
     $build['partnership_info']['about_partnership'] = [
       '#type' => 'fieldset',
       '#title' => 'About the partnership',
@@ -85,7 +85,7 @@ class ParPartnershipPageController extends ParBaseController {
       'details' => [
         '#type' => 'html_tag',
         '#tag' => 'div',
-        '#value' => $about_partnership_display,
+        '#value' => $this->getRenderer()->render($about_partnership_display),
       ],
     ];
 
