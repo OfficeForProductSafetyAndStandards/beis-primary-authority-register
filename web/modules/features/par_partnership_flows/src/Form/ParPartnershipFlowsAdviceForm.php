518f0795af2c145ac5b862f618555a8abd9478cd
Andrew Hughes-Onslow <kalpaitch@users.noreply.github.com>
2020-07-27 10:14:07 +0100

    Update ParPartnershipFlowsAdviceForm.php

--- web/modules/features/par_partnership_flows/src/Form/ParPartnershipFlowsAdviceForm.php
+++ web/modules/features/par_partnership_flows/src/Form/ParPartnershipFlowsAdviceForm.php
@@ -191,7 +191,7 @@ class ParPartnershipFlowsAdviceForm extends ParBaseForm {
     parent::submitForm($form, $form_state);
 
     $regulatory_functions = $this->getFlowDataHandler()->getTempDataValue('regulatory_functions');
-    $assigned_reg_functions = count(array_filter($regulatory_functions));
+    $assigned_reg_functions = array_filter($regulatory_functions);
 
     // Get the advice entity from the URL.
     $par_data_advice = $this->getFlowDataHandler()->getParameter('par_data_advice');
@@ -236,7 +236,7 @@ class ParPartnershipFlowsAdviceForm extends ParBaseForm {
     }
 
     // Set regulatory functions.
-    $par_data_advice->set('field_regulatory_function', $regulatory_functions);
+    $par_data_advice->set('field_regulatory_function', $assigned_reg_functions);
 
     // Set the status to active for the advice entity.
     $par_data_advice->setParStatus('active', TRUE);
@@ -247,7 +247,7 @@ class ParPartnershipFlowsAdviceForm extends ParBaseForm {
     }
 
     // Save and attach new advice entities.
-    if ($par_data_advice->isNew() && $assigned_reg_functions > 0 && $par_data_advice->save()) {
+    if ($par_data_advice->isNew() && !empty($assigned_reg_functions) && $par_data_advice->save()) {
       $par_data_partnership = $this->getFlowDataHandler()->getParameter('par_data_partnership');
       $par_data_partnership->get('field_advice')->appendItem($par_data_advice->id());
 
@@ -264,7 +264,7 @@ class ParPartnershipFlowsAdviceForm extends ParBaseForm {
       }
     }
     // Save existing advice entities.
-    else if ($assigned_reg_functions > 0 && $par_data_advice->save()) {
+    else if (!empty($assigned_reg_functions) && $par_data_advice->save()) {
       $this->getFlowDataHandler()->deleteStore();
     }
     // Log an error.
