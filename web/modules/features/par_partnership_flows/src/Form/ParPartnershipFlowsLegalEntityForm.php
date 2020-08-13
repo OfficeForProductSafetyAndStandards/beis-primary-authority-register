ee7531071436f34c785cc74db16c264f72513570
Jon <jon.edery@googlemail.com>
2020-08-13 19:14:40 +0100

    PAR-1672 correct error messages for validation forms.

--- web/modules/features/par_partnership_flows/src/Form/ParPartnershipFlowsLegalEntityForm.php
+++ web/modules/features/par_partnership_flows/src/Form/ParPartnershipFlowsLegalEntityForm.php
@@ -23,13 +23,13 @@ class ParPartnershipFlowsLegalEntityForm extends ParBaseForm {
    */
   protected $entityMapping = [
     ['registered_name', 'par_data_legal_entity', 'registered_name', NULL, NULL, 0, [
-      'This value should not be null.' => 'You must enter the name of this legal entity.'
+      'You must fill in the missing information.' => 'You must enter the name of this legal entity.'
     ]],
     ['legal_entity_type', 'par_data_legal_entity', 'legal_entity_type', NULL, NULL, 0, [
-      'This value should not be null.' => 'You must choose which type of legal entity this is.'
+      'You must fill in the missing information.' => 'You must choose which type of legal entity this is.'
     ]],
     ['registered_number', 'par_data_legal_entity', 'registered_number', NULL, NULL, 0, [
-      'This value should not be null.' => 'You must enter the registered number for this legal entity.'
+      'You must fill in the missing information.' => 'You must enter the registered number for this legal entity.'
     ]],
   ];
 
