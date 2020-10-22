cd355012446ad023e8d2d295cf6ff29def70160a
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2020-10-22 09:34:40 +0100

    PAR-1684: Updated error logging on staging.

--- web/sites/default/settings.local.staging.php
+++ web/sites/default/settings.local.staging.php
@@ -5,3 +5,10 @@ $config['config_split.config_split.test_config']['status'] = TRUE;
 
 // Setting to enable PAR Green header/footer override.
 $settings['par_branded_header_footer'] = TRUE;
+
+/**
+ * Show all error messages, with backtrace information.
+ *
+ * @TODO Temporary to resolve staging issues.
+ */
+$config['system.logging']['error_level'] = 'verbose';
