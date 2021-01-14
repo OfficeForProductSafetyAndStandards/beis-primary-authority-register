a833bdcc346e9ee6e2650c6f5052ae4e98da64d9
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2021-01-14 10:37:20 +0000

    Error: Updated error handling settings.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1020,7 +1020,7 @@ $config['govuk_notify.settings']['default_template_id'] = getenv('PAR_GOVUK_NOTI
 $settings['ideal_postcodes_api_key'] = getenv('IDEAL_POSTCODES_API_KEY');
 
 /**
- * Set the Raven Sentry keys.
+ * Set the Raven Sentry keys and default error settings.
  *
  * These are confidential and should be set with ENV variables.
  *
@@ -1028,6 +1028,8 @@ $settings['ideal_postcodes_api_key'] = getenv('IDEAL_POSTCODES_API_KEY');
  */
 $config['raven.settings']['client_key'] = getenv('SENTRY_DSN');
 $config['raven.settings']['public_dsn'] = getenv('SENTRY_DSN_PUBLIC');
+
+$config['system.logging']['error_level'] = ERROR_REPORTING_HIDE;
 
 // Ensure all environments use production config unless overwritten.
 $config['config_split.config_split.dev_config']['status'] = FALSE;
