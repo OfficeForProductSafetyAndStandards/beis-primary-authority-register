0229a8c8c6ec785e9525fc83ba8855462fc927e4
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2021-01-19 22:12:41 +0000

    Test: Updated login.

--- web/modules/custom/par_login/tests/src/Functional/ParLoginTest.php
+++ web/modules/custom/par_login/tests/src/Functional/ParLoginTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\par_login\Functional;
 
+use Drupal\Component\Render\FormattableMarkup;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Url;
 use Drupal\Tests\BrowserTestBase;
 use Drupal\user\Entity\User;
@@ -19,6 +21,30 @@ class UserLoginTest extends BrowserTestBase {
   protected $defaultTheme = 'stark';
 
   /**
+   * Extending the default login test to take care of the GOV UK sign in form.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   */
+  protected function parLogin(AccountInterface $account) {
+    if ($this->loggedInUser) {
+      $this->drupalLogout();
+    }
+
+    $this->drupalGet(Url::fromRoute('user.login'));
+    $this->submitForm([
+      'name' => $account->getAccountName(),
+      'pass' => $account->passRaw,
+    ], t('Sign in'));
+
+    // @see ::drupalUserIsLoggedIn()
+    $account->sessionId = $this->getSession()->getCookie(\Drupal::service('session_configuration')->getOptions(\Drupal::request())['name']);
+    $this->assertTrue($this->drupalUserIsLoggedIn($account), new FormattableMarkup('User %name successfully logged in.', ['%name' => $account->getAccountName()]));
+
+    $this->loggedInUser = $account;
+    $this->container->get('current_user')->setAccount($account);
+  }
+
+  /**
    * Test the login is case insensitive.
    *
    * Moving this single to into PAR tests so that the checks for case insensitive
@@ -26,11 +52,18 @@ class UserLoginTest extends BrowserTestBase {
    * @see https://www.drupal.org/project/drupal/issues/2490294
    */
   public function testCaseInsensitiveLogin() {
+    $translations = [
+      'en' => [
+        'Log in' => 'Sign in',
+        'Log out' => 'Sign out',
+      ]
+    ];
+
     $username = 'SENSITİVEKINDĄCASE';
     $user1 = $this->drupalCreateUser([], $username);
     // Change username to lowercase without saving to test case insensitive login.
     $user1->name = mb_strtolower($username);
-    $this->drupalLogin($user1);
+    $this->parLogin($user1);
 
     $this->drupalLogout();
   }
