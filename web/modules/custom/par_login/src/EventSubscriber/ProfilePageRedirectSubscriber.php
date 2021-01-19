b738375e25dcab6b179de9a400fc9fd26d46eb9d
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2021-01-19 21:14:38 +0000

    PAR-1698: Added logging to redirect request.

--- web/modules/custom/par_login/src/EventSubscriber/ProfilePageRedirectSubscriber.php
+++ web/modules/custom/par_login/src/EventSubscriber/ProfilePageRedirectSubscriber.php
@@ -4,6 +4,7 @@ namespace Drupal\par_login\EventSubscriber;
 
 use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\Url;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
@@ -19,13 +20,23 @@ class ProfilePageRedirectSubscriber implements EventSubscriberInterface {
   private $account;
 
   /**
+   * The logger service.
+   *
+   * @param \Psr\Log\LoggerInterface $logger
+   */
+  private $logger;
+
+  /**
    * @param \Drupal\Core\Session\AccountProxyInterface $account
    *   The current user.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   The logger service.
    *
    * @throws \InvalidArgumentException
    */
-  public function __construct(AccountProxyInterface $account) {
+  public function __construct(AccountProxyInterface $account, LoggerInterface $logger) {
     $this->account = $account;
+    $this->logger = $logger;
   }
 
   /**
@@ -55,6 +66,13 @@ class ProfilePageRedirectSubscriber implements EventSubscriberInterface {
     // to view their profile pages.
     if (!$this->account->hasPermission('administer users') &&
       ltrim($request->getPathInfo(), '/') === ltrim($profile_page->getInternalPath(), '/')) {
+
+      // Log the denied request as well as the referer for information.
+      $referer = $request->headers->get('referer');
+      $this->logger->warning(
+        'User @user is not allowed to access their profile page. Redirected from %redirected',
+        ['%user' => $this->account->id(), '%redirected' => $referer]
+      );
 
       $redirect_url = Url::fromRoute('par_dashboards.dashboard');
       $response = new RedirectResponse($redirect_url->toString(), 301);
