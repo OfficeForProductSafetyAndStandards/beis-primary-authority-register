c044f0a378ba1eb4eee463cbd88262c0e6884f73
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2021-01-20 10:20:42 +0000

    Tests

--- web/modules/custom/par_login/tests/src/Functional/ParLoginTest.php
+++ web/modules/custom/par_login/tests/src/Functional/ParLoginTest.php
@@ -1,51 +1,86 @@
 <?php
 
-namespace Drupal\Tests\par_login\Functional;
+namespace Drupal\Tests\par_data\Kernel;
 
 use Drupal\Component\Render\FormattableMarkup;
+use Drupal\Core\Entity\EntityStorageException;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\Url;
-use Drupal\Tests\BrowserTestBase;
-use Drupal\user\Entity\User;
+use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
 
 /**
- * Ensure that login works as expected.
+ * Tests PAR Login.
  *
- * This test is part of the case insensitive login patch. Adding the test
- * directly to PAR ensures that this functionality is tested regardless of
- * whether the patch has been applied.
- * @see https://www.drupal.org/project/drupal/issues/2490294
- *
- * @group user
+ * @group PAR Login
  */
-class UserLoginTest extends BrowserTestBase {
+class ParLoginTestBase extends EntityKernelTestBase {
+
+  static $modules = [
+    'user',
+    'system',
+    'field',
+    'entity_test'
+  ];
+
+  /**
+   * @var AccountInterface
+   */
+  protected $account;
+
+  protected $permissions = [
+    'access content',
+  ];
+
+  protected $entityTypes = [];
 
   /**
    * {@inheritdoc}
    */
-  protected $defaultTheme = 'stark';
+  protected function setUp() {
+    // Must change the bytea_output to the format "escape" before running tests.
+    // @see https://www.drupal.org/node/2810049
+    //db_query("ALTER DATABASE 'par' SET bytea_output = 'escape';")->execute();
+    parent::setUp();
+
+    $this->entityTypeManager = \Drupal::entityTypeManager();
+  }
 
   /**
-   * {@inheritdoc}
-   */
-  protected static $modules = ['email_registration'];
-
-  /**
-   * Test the login is case insensitive.
+   * Test the email address is case insensitive.
    *
    * Moving this single to into PAR tests so that the checks for case insensitive
    * login are conducted regardless of whether we have this patch installed...
    * @see https://www.drupal.org/project/drupal/issues/2490294
    */
-  public function testCaseInsensitiveLogin() {
-    $username = 'SENSITİVEKINDĄCASE';
-    $email = "$username@EXAMPLE.com";
-    $user1 = $this->drupalCreateUser([], $username, ['mail' => $email]);
-    // Change username to lowercase without saving to test case insensitive login.
-    $user1->name = mb_strtolower($username);
-    $this->drupalLogin($user1);
+  public function testCaseInsensitiveEmail() {
+    $uppercase_username = 'SENSITİVEKINDĄCASE';
+    $lowercase_username = mb_strtolower($uppercase_username);
+    $uppercase_email = "$uppercase_username@EXAMPLE.com";
+    $lowercase_email = "$lowercase_username@example.com";
+    $emailcase_email = "$lowercase_username@ExAmPlE.com";
 
-    $this->drupalLogout();
+    $this->drupalCreateUser([], $uppercase_username, ['mail' => $uppercase_email]);
+    try {
+      $lowercase_account = $this->drupalCreateUser([], $lowercase_username, ['mail' => $uppercase_email]);
+      // This assertion should never be reached, an exception _should_ be thrown.
+      $this->assertFalse($lowercase_account, new FormattableMarkup('The user account could not be created because this email %email already exists.', ['%email' => $lowercase_email]));
+    }
+    catch (EntityStorageException $e) {
+      $this->assertTrue(true, new FormattableMarkup('The user account could not be created because this email %email already exists.', ['%email' => $lowercase_email]));
+    }
+
+    $uppercase_users = $this->entityTypeManager->getStorage('user')
+      ->loadByProperties(['mail' => $uppercase_email]);
+    $lowercase_users = $this->entityTypeManager->getStorage('user')
+      ->loadByProperties(['mail' => $lowercase_email]);
+    $emailcase_users = $this->entityTypeManager->getStorage('user')
+      ->loadByProperties(['mail' => $emailcase_email]);
+
+    $this->assertCount(1, $uppercase_users, new FormattableMarkup('The user account was identified using an uppercase email %email', ['%email' => $uppercase_email]));
+    $this->assertCount(1, $lowercase_users, new FormattableMarkup('The user account was identified using a lowercase email %email', ['%email' => $lowercase_email]));
+    $this->assertCount(1, $emailcase_users, new FormattableMarkup('The user account was identified using a mixed case email %email', ['%email' => $emailcase_email]));
+
+    $this->assertIdentical($uppercase_users, $lowercase_users, new FormattableMarkup('The user accounts loaded by email %email match.', ['%email' => $lowercase_email]));
+    $this->assertIdentical($uppercase_users, $emailcase_users, new FormattableMarkup('The user accounts loaded by email %email match.', ['%email' => $emailcase_email]));
   }
 
 }
