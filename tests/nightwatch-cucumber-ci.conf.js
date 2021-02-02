4752bbb799655a3487e95e976a5d7869c5755e1d
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2021-02-02 18:29:50 +0000

    PAR-1680: Updated --tag.

--- tests/nightwatch-cucumber-ci.conf.js
+++ tests/nightwatch-cucumber-ci.conf.js
@@ -8,7 +8,7 @@ require('nightwatch-cucumber')({
         '--require', 'support',
         '--format', 'node_modules/cucumber-pretty',
         '--format', 'json:reports/cucumber.json',
-        '--tags', 'ci', '--tags', 'v2',
+        '--tag', 'ci', '--tag', 'v2',
         'features-v1'
     ]
 })
