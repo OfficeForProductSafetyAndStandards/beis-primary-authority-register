9a49eafd14ca05ad803a1cf9f213f5af8a4b2f79
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2021-05-25 19:17:39 +0100

    File Entity: Added remote backup script.

--- scripts/db-remote-backup.sh
+++ scripts/db-remote-backup.sh
@@ -8,7 +8,6 @@ set -o errexit -euo pipefail -o noclobber -o nounset
 # Prerequisites - You'll need the following installed
 #    AWS CLI - http://docs.aws.amazon.com/cli/latest/userguide/installing.html
 #    Cloud Foundry CLI - https://docs.cloudfoundry.org/cf-cli/install-go-cli.html
-#    Vault CLI - https://www.vaultproject.io/docs/install/index.html
 ####################################################################################
 ! getopt --test > /dev/null
 if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
@@ -18,6 +17,13 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
 
     exit 1
 fi
+
+command -v cf >/dev/null 2>&1 || {
+    echo "################################################################################################"
+    echo >&2 "Please install Cloud Foundry CLI - https://docs.cloudfoundry.org/cf-cli/install-go-cli.html"
+    echo "################################################################################################"
+    exit 1
+}
 
 
 ####################################################################################
@@ -144,65 +150,55 @@ FILE_NAME="db-dump-$NAME-$SANITISED"
 DATE=$(date +%Y-%m-%d)
 
 mkdir -p $DIRECTORY
-rm -f $DIRECTORY/${FILE_NAME}_raw.sql
 rm -f $DIRECTORY/$FILE_NAME.sql
 
 printf "Exporting database dump...\n"
 
-# Select the tables to exclude from the dump.
-dump_tables=(
-  --exclude-table "'*.migration_*'"
-  --exclude-table "'*.test*'"
-  --exclude-table "'*.watchdog'"
-  --exclude-table "'*.webprofiler'"
-  --exclude-table-data "'*.cache'"
-  --exclude-table-data "'*.cache_data'"
-  --exclude-table-data "'public.cache_data'"
-  --exclude-table-data "'cache_data'"
-  --exclude-table-data "'*.cache_*'"
-  --exclude-table-data "'*.history'"
-  --exclude-table-data "'*.search_*'"
-  --exclude-table-data "'*.sessions'"
-  --exclude-table-data "'*.flood'"
+# Select the tables to exclude from the dump entirely.
+skip_tables=(
+  migration_*
+  test*
+  watchdog
+  webprofiler
 )
+# Select the tables to only dump the schema for, ignoring all content.
+temp_tables=(
+  cache
+  cache_*
+  history
+  search_*
+  sessions
+  flood
+)
+
+# Set the dump arguments
+dump_args=(
+  -f "$DIRECTORY/${FILE_NAME}.sql"
+  --verbose
+  --clean
+  --no-owner
+  --no-acl
+)
+for t in ${skip_tables[@]}; do
+  dump_args+=(--exclude-table 'public.'$t)
+done
+for t in ${temp_tables[@]}; do
+  dump_args+=(--exclude-table-data 'public.'$t)
+done
 
 # Using `pg_dump` instead of `drush sql:dump` @see https://regulatorydelivery.atlassian.net/browse/PAR-1699
 # postgres client binary no longer available to drush in deployed app.
-#pg_dump --verbose --clean --no-owner --no-acl \
-#  -f /var/dev/projects/par/backups/db-dump-production-unsanitised_raw.sql \
-#  --exclude-table '*.migration_*' \
-#  --exclude-table '*.test*' \
-#  --exclude-table '*.watchdog' \
-#  --exclude-table '*.webprofiler' \
-#  --exclude-table-data '*.cache' \
-#  --exclude-table-data '*.cache_data' \
-#  --exclude-table-data 'public.cache_data' \
-#  --exclude-table-data 'cache_data' \
-#  --exclude-table-data '*.cache_*' \
-#  --exclude-table-data '*.history' \
-#  --exclude-table-data '*.search_*' \
-#  --exclude-table-data '*.sessions' \
-#  --exclude-table-data '*.flood'
 echo -e "Piping command: cf conduit $SERVICE_NAME -c '{\"read_only\": true}'"
-echo -e "Running: pg_dump --file $DIRECTORY/${FILE_NAME}.sql --clean --no-owner --no-acl ${dump_tables[@]}"
+echo -e "Running: pg_dump ${dump_args[@]}"
 cf conduit $SERVICE_NAME -c '{"read_only": true}' -- \
-  pg_dump --verbose --clean --no-owner --no-acl \
-    -f "$DIRECTORY/${FILE_NAME}_raw.sql" \
-    "${dump_tables[@]}"
+  pg_dump "${dump_args[@]}"
 
 ## Remove erroneous pg_dump roles applied despite --no-owner flags.
 ## https://stackoverflow.com/questions/60605491/using-pg-dump-with-no-owner-information-still-outputting-role-information
-sed -e '/-- do not execute if not member of manager role/,+4d' $DIRECTORY/${FILE_NAME}_raw.sql > $DIRECTORY/$FILE_NAME.sql
+#sed -e '/-- do not execute if not member of manager role/,+4d' $DIRECTORY/${FILE_NAME}_raw.sql > $DIRECTORY/$FILE_NAME.sql
 
 printf "Packaging database dump...\n"
 tar -zcvf $DIRECTORY/$FILE_NAME-latest.tar.gz -C $DIRECTORY "$FILE_NAME.sql"
 tar -zcvf $DIRECTORY/$FILE_NAME-$DATE.tar.gz -C $DIRECTORY "$FILE_NAME.sql"
-
-if [[ $AWS_PUSH == y ]]; then
-    printf "Uploading database archives...\n"
-    cd ./web
-    ../vendor/drush/drush/drush fsp s3backups $DIRECTORY/$FILE_NAME-latest.tar.gz $FILE_NAME-latest.tar.gz
-    ../vendor/drush/drush/drush fsp s3backups $DIRECTORY/$FILE_NAME-$DATE.tar.gz $FILE_NAME-$DATE.tar.gz
-fi
 
 printf "Database archive completed...\n"
