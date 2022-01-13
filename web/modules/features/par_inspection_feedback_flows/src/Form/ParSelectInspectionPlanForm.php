900bf9bc75e2172fa3f4dab4cdb5d4f0fa33dfa1
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2022-01-13 10:52:42 +0000

    PAR-1747: Updated inspection plan enquiries.

--- web/modules/features/par_inspection_feedback_flows/src/Form/ParSelectInspectionPlanForm.php
+++ web/modules/features/par_inspection_feedback_flows/src/Form/ParSelectInspectionPlanForm.php
@@ -26,7 +26,7 @@ class ParSelectInspectionPlanForm extends ParBaseForm {
   public function buildForm(array $form, FormStateInterface $form_state) {
     // Get the inspection plan data set by the select_inspection form plugin.
     $inspection_plans = $this->getFlowDataHandler()->getFormPermValue('inspection_plan_options');
-    if (count($inspection_plans) <= 0) {
+    if ($inspection_plans && count($inspection_plans) <= 0) {
       $this->getFlowNegotiator()->getFlow()->disableAction('next');
     }
 
