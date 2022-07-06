fb2b98f39602f9c6e0968d0c77a7644ace7525fb
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2022-07-06 13:01:08 +0100

    PAR-1603: Added scheduler rule to update legal entities.

---
+++ web/modules/custom/par_actions/src/Plugin/Action/ParUpdateLegalEntityName.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\par_actions\Plugin\Action;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\par_data\Entity\ParDataEntityInterface;
+
+/**
+ * Updates a legal entity name.
+ *
+ * @Action(
+ *   id = "par_update_legal_entity_name",
+ *   label = @Translation("Update legal entity name"),
+ *   type = "system"
+ * )
+ */
+class ParUpdateLegalEntityName extends ActionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    if ($entity instanceof ParDataEntityInterface) {
+      $organisation_register_manager = \Drupal::service('registered_organisations.organisation_manager');
+      $companies_house_register = $organisation_register_manager->getDefinition('companies_house');
+
+      // Only certain legal entity types are registered with Companies House.
+      $companies_house_types = ['partnership', 'limited_company', 'public_limited_company', 'limited_partnership', 'limited_liability_partnership'];
+
+      if ($entity->getType() && !empty($entity->getRegisteredNumber()) &&
+        in_array($entity->get('legal_entity_type')->getString(), $companies_house_types)) {
+        $company_profile = $organisation_register_manager->lookupOrganisation($entity->getRegisteredNumber(), $companies_house_register);
+
+        $companies_house_name = $company_profile ? $company_profile['company_name'] : false;
+        if ($companies_house_name && $companies_house_name !== $entity->getName()) {
+          // Update the name.
+          $entity->get('registered_name')->setValue($companies_house_name);
+          $entity->save();
+        }
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    // @TODO Implement entity/action checks
+    $result = AccessResult::allowed();
+    return $return_as_object ? $result : $result->isAllowed();
+  }
+
+}
---
+++ web/modules/custom/par_actions/src/Plugin/ParSchedulerRule/ParLegalEntityUpdate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\par_actions\Plugin\ParSchedulerRule;
+
+use Drupal\par_actions\ParSchedulerRuleBase;
+
+/**
+ * Approve an enforcement notice.
+ *
+ * @ParSchedulerRule(
+ *   id = "legal_entity_update",
+ *   title = @Translation("Weekly syncing of legal entities with Companies House."),
+ *   entity = "par_data_legal_entity",
+ *   frequency = "1 week",
+ *   queue = TRUE,
+ *   status = TRUE,
+ *   action = "par_update_legal_entity_name"
+ * )
+ */
+class ParLegalEntityUpdate extends ParSchedulerRuleBase {
+
+  public function query() {
+    $query = parent::query();
+
+    $time = \Drupal::time()->getRequestTime();
+
+    // Get all entities that haven't been updated in a week.
+    $query->condition('updated', $time-(60*60*24*6), '<');
+
+    return $query;
+  }
+}
