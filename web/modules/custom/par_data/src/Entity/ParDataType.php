1c98ea5cbf7a437a56041d5e5a6c284c0852f3d0
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2022-09-12 23:46:05 +0100

    PAR-1805: Updated.

--- web/modules/custom/par_data/src/Entity/ParDataType.php
+++ web/modules/custom/par_data/src/Entity/ParDataType.php
@@ -211,7 +211,7 @@ abstract class ParDataType extends TranceType implements ParDataTypeInterface {
    * {@inheritdoc}
    */
   public function getAllowedFieldlabel($field_name, $value = FALSE) {
-    $allowed_values = $this->getAllowedValues();
+    $allowed_values = $this->getAllowedValues($field_name);
     return isset($allowed_values[$value]) ? $allowed_values[$value] : FALSE;
   }
 
@@ -219,7 +219,7 @@ abstract class ParDataType extends TranceType implements ParDataTypeInterface {
    * {@inheritdoc}
    */
   public function getAllowedValueBylabel($field_name, $label, $fuzzy = FALSE) {
-    $allowed_values = $this->getAllowedValues();
+    $allowed_values = $this->getAllowedValues($field_name);
 
     $key = $fuzzy ?
       array_search(strtolower($label), array_map('strtolower', $allowed_values)) :
