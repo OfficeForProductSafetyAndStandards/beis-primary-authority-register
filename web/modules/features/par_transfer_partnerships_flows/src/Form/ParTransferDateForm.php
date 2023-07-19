149adebdc745726a09956832e82507fc55c6ac60
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2023-07-19 10:38:09 +0100

    PAR-2193: Added all journey pages.

---
+++ web/modules/features/par_transfer_partnerships_flows/src/Form/ParTransferDateForm.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\par_transfer_partnerships_flows\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\par_data\Entity\ParDataPartnership;
+use Drupal\par_flows\Form\ParBaseForm;
+
+/**
+ * The form for setting a transfer date.
+ */
+class ParTransferDateForm extends ParBaseForm {
+
+  protected $pageTitle = 'When would you like this transfer to take place?';
+
+  /**
+   * Load the data for this form.
+   */
+  public function loadData() {
+    $par_data_partnership = $this->getFlowDataHandler()->getParameter('par_data_partnership');
+    $par_data_inspection_plan = $this->getFlowDataHandler()->getParameter('par_data_inspection_plan');
+
+    parent::loadData();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, ParDataPartnership $par_data_partnership = NULL, $par_data_inspection_plan = NULL) {
+
+
+    // Change the main button title to 'remove'.
+    $this->getFlowNegotiator()->getFlow()->setPrimaryActionTitle('Remove');
+
+    // Make sure to add the person cacheability data to this form.
+    $this->addCacheableDependency($par_data_partnership);
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+
+    if (!$form_state->getValue('remove_reason')) {
+      $id = $this->getElementId('remove_reason', $form);
+      $form_state->setErrorByName($this->getElementName(['confirm']), $this->wrapErrorMessage('Please enter the reason you are removing this inspection plan.', $id));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $par_data_partnership = $this->getFlowDataHandler()->getParameter('par_data_partnership');
+    $par_data_inspection_plan = $this->getFlowDataHandler()->getParameter('par_data_inspection_plan');
+    $delta = $this->getFlowDataHandler()->getTempDataValue('delta');
+  }
+
+}
