1963a03fcf8ce1c0cdeb675cd20051a825ab77f9
Andrew Crook (Somersoft) <andrew.crook@somersoft.co.uk>
2024-03-21 01:09:09 +0000

    The sample database I was given had the maillog and dblog modules enabled. The config split then tried to enable these two modules and hence why the script disables the two modules.

--- scripts/docksal/install.sh
+++ scripts/docksal/install.sh
@@ -4,12 +4,14 @@ fin exec npm install &&
 fin exec npm run frontend &&
 # fin drush --yes site:install &&
 fin drush @par.paas --yes sql:drop &&
-fin drush @par.paas sql:cli < backups/db-dump-production-sanitised.sql &&
+fin exec "drush @par.paas sql:cli < backups/db-dump-production-sanitised.sql" &&
+fin drush pm:uninstall --yes dblog &&
+fin drush pm:uninstall --yes maillog &&
 fin drush --yes updatedb &&
 fin drush --yes config:import &&
 fin drush pm:uninstall --yes config_readonly &&
-fin drush config:set --yes search_api.server.opensearch backend_config.connector_config.url https://os:9200 &&
+fin drush config:set --yes search_api.server.opensearch backend_config.connector_config.url http://beis-par-search:9200 &&
 fin drush config:set --yes system.performance css.preprocess false &&
 fin drush config:set --yes system.performance js.preprocess false &&
-fin drush cache:rebuid &&
+fin drush cache:rebuild &&
 fin drush user:login
