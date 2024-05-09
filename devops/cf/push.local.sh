7cb5a4ed52563e72d59ee2bf409ca8959d1881fd
Robert Wild <robert.wild@affinity-digital.com>
2024-05-09 14:39:54 +0100

    Tweaks to build location

--- devops/cf/push.local.sh
+++ devops/cf/push.local.sh
@@ -555,7 +555,7 @@ if [[ $ENV != "production" ]] && [[ $DB_RESET == 'y' ]]; then
     printf "Importing the database $DB_NAME.sql...\n"
     cf run-task $TARGET_ENV -m 2G -k 2G --name DB_IMPORT -c "./scripts/drop.sh && \
         cd $REMOTE_BUILD_DIR/web && \
-        tar --no-same-owner -zxvf $REMOTE_BUILD_DIR/$DB_DIR/$DB_NAME.tar.gz -C $REMOTE_BUILD_DIR/$DB_DIR && \
+        tar --no-same-owner -zxvf $BUILD_DIR/$DB_DIR/$DB_NAME.tar.gz -C $REMOTE_BUILD_DIR/$DB_DIR && \
         ../vendor/bin/drush @par.paas sql:cli < $REMOTE_BUILD_DIR/$DB_DIR/$DB_NAME.sql && \
         rm -f $REMOTE_BUILD_DIR/$DB_DIR/$DB_NAME.sql"
 
