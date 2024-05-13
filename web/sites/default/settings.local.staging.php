64c171f355a18276c6aeb2d32c2c5827f0821028
Robert Wild <robert.wild@affinity-digital.com>
2024-05-13 16:54:04 +0100

    More resource config tweaks

--- web/sites/default/settings.local.staging.php
+++ web/sites/default/settings.local.staging.php
@@ -3,7 +3,7 @@
 $config['config_split.config_split.dev_config']['status'] = TRUE;
 
 ini_set('memory_limit', '2G');
-ini_set('max_execution_time', 480);
+ini_set('max_execution_time', 600);
 
 // Setting to enable PAR Green header/footer override.
 $settings['par_branded_header_footer'] = TRUE;
