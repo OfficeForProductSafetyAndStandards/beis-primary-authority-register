f2431e9f62269b4a4d0b2fce839bd3d48fc9aa10
Robert Wild <robert.wild@affinity-digital.com>
2024-05-13 13:47:32 +0100

    Amends to execution time

--- web/sites/default/settings.local.staging.php
+++ web/sites/default/settings.local.staging.php
@@ -3,7 +3,7 @@
 $config['config_split.config_split.dev_config']['status'] = TRUE;
 
 ini_set('memory_limit', '1024M');
-ini_set('max_execution_time', 120);
+ini_set('max_execution_time', 240);
 
 // Setting to enable PAR Green header/footer override.
 $settings['par_branded_header_footer'] = TRUE;
