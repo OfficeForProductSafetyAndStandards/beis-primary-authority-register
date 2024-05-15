112cd51dab18971e46c3bf4985bc04d37e8d464f
Robert Wild <robert.wild@affinity-digital.com>
2024-05-15 11:04:43 +0100

    Unblocking user 1 on non prod environments

--- devops/cf/push.local.sh
+++ devops/cf/push.local.sh
@@ -560,6 +560,7 @@ if [[ $ENV != "production" ]] && [[ $DB_RESET == 'y' ]]; then
         cd $REMOTE_BUILD_DIR/web && \
         tar --no-same-owner -zxvf $REMOTE_BUILD_DIR/$DB_DIR/$DB_NAME.tar.gz -C $REMOTE_BUILD_DIR/$DB_DIR && \
         ../vendor/bin/drush @par.paas sql:cli < $REMOTE_BUILD_DIR/$DB_DIR/$DB_NAME.sql && \
+        ../vendor/bin/drush user:unblock dadmin && \
         rm -f $REMOTE_BUILD_DIR/$DB_DIR/$DB_NAME.sql"
 
     # Wait for database to be imported.
