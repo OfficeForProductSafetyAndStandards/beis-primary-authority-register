5fc332f0d8591da570a7aa98fb85196b04015009
robwild-affinitydigital <162973285+robwild-affinitydigital@users.noreply.github.com>
2024-06-05 09:54:30 +0100

    PAR-2473 - DB Request reduction (#1145)
    * PAR-2473 - DB Request reduction
    
     - Removes additional db queries for getflows, as this multiples dbs requests unnecessarily
     - Enables BugPipe to speed up response times.
    
    * PAR-2473 - Moving getData logic to profile feature reduces db queries from 35k to 4k
    
    * Updates for timeouts
    
    * Updates for CircleCI and test regressions
    
    * Updating stage deploy task

--- web/modules/features/par_profile_view_flows/src/ParFlowAccessTrait.php
+++ web/modules/features/par_profile_view_flows/src/ParFlowAccessTrait.php
@@ -4,8 +4,6 @@ namespace Drupal\par_profile_view_flows;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\par_data\Entity\ParDataCoordinatedBusiness;
-use Drupal\par_data\Entity\ParDataPartnership;
 use Drupal\par_data\Entity\ParDataPerson;
 use Drupal\par_flows\ParFlowException;
 use Drupal\user\Entity\User;
@@ -24,7 +22,7 @@ trait ParFlowAccessTrait {
    */
   public function accessCallback(Route $route, RouteMatchInterface $route_match, AccountInterface $account, ParDataPerson $par_data_person = NULL, User $user = NULL): AccessResult {
     try {
-      // Get a new flow negotiator that points the the route being checked for access.
+      // Get a new flow negotiator that points the route being checked for access.
       $access_route_negotiator = $this->getFlowNegotiator()->cloneFlowNegotiator($route_match);
     } catch (ParFlowException $e) {
 
