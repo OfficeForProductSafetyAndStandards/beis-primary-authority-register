1d958e09a1555f91009397af483e97306de05f31
Robert Wild <robert.wild@affinity-digital.com>
2024-06-05 09:28:43 +0100

    Re-base against master

--- web/sites/default/settings.local.ci-sanitise.php
+++ web/sites/default/settings.local.ci-sanitise.php
@@ -12,3 +12,9 @@ $databases['default']['default'] = array (
 );
 
 $settings['trusted_host_patterns'] = [''];
+
+// Ensure ci always runs with the same memory that other environments do.
+ini_set('memory_limit', '512M');
+ini_set('max_execution_time', 60);
+
+$settings['hash_salt'] = 'y9fBHyfS_V90Ubd42vTlAnp0VvZ7Ljgbb68UTNLsBBgXAkGViooWGE59oevcc6UU2X0ObGIIlA';
