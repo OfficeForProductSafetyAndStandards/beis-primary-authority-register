4daf0f5af82009e0e689c6d4f596a50807b5abfe
Robert Wild <robert.wild@affinity-digital.com>
2024-06-10 09:37:53 +0100

    Setting correct db name

--- devops/cf/push.local.sh
+++ devops/cf/push.local.sh
@@ -69,7 +69,7 @@ CF_INSTANCES=${CF_INSTANCES:=1}
 BUILD_VER=${BUILD_VER:-}
 BUILD_DIR=${BUILD_DIR:=$PWD}
 REMOTE_BUILD_DIR=${REMOTE_BUILD_DIR:="/home/vcap/app"}
-DB_NAME="db-seed"
+DB_NAME="db-dump-production-sanitised-latest"
 DB_DIR="backups"
 DB_RESET=${DB_RESET:=n}
 DEPLOY_PRODUCTION=${DEPLOY_PRODUCTION:=n}
