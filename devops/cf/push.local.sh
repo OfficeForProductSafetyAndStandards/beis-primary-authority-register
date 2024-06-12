450d32dadfd8d382cc4cf6b28dc8696852b8b318
Robert Wild <robert.wild@affinity-digital.com>
2024-06-12 16:09:32 +0100

    Bypass db import temporarily

--- devops/cf/push.local.sh
+++ devops/cf/push.local.sh
@@ -558,9 +558,6 @@ if [[ $ENV != "production" ]] && [[ $DB_RESET == 'y' ]]; then
     cf run-task $TARGET_ENV -m 2G -k 2G --name DB_IMPORT -c "
         ls -la /home/vcap/app && ls -la /home/vcap/app/web&& ls -la /home/vcap/app/backups && \
         cd $REMOTE_BUILD_DIR/web && \
-        tar --no-same-owner -zxvf $REMOTE_BUILD_DIR/$DB_DIR/$DB_NAME.tar.gz -C $REMOTE_BUILD_DIR/$DB_DIR && \
-        ../vendor/bin/drush @par.paas sql:cli < $REMOTE_BUILD_DIR/$DB_DIR/$DB_NAME.sql && \
-        ../vendor/bin/drush user:unblock dadmin && \
         rm -f $REMOTE_BUILD_DIR/$DB_DIR/$DB_NAME.sql"
 
     # Wait for database to be imported.
