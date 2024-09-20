04e6adc8f3148c2538c86672f659180909d395e0
robwild-affinitydigital <162973285+robwild-affinitydigital@users.noreply.github.com>
2024-09-20 11:24:23 +0100

    Update db-backup.sh

--- scripts/db-backup.sh
+++ scripts/db-backup.sh
@@ -8,7 +8,6 @@ set -o errexit -euo pipefail -o noclobber -o nounset
 # Prerequisites - You'll need the following installed
 #    AWS CLI - http://docs.aws.amazon.com/cli/latest/userguide/installing.html
 #    Cloud Foundry CLI - https://docs.cloudfoundry.org/cf-cli/install-go-cli.html
-#    Vault CLI - https://www.vaultproject.io/docs/install/index.html
 ####################################################################################
 ! getopt --test > /dev/null
 if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
@@ -19,18 +18,25 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
     exit 1
 fi
 
+command -v cf >/dev/null 2>&1 || {
+    echo "################################################################################################"
+    echo >&2 "Please install Cloud Foundry CLI - https://docs.cloudfoundry.org/cf-cli/install-go-cli.html"
+    echo "################################################################################################"
+    exit 1
+}
+
 
 ####################################################################################
 # Set required parameters
 #    ENV (required) - the password for the user account
-#    GOVUK_CF_USER (required) - the user deploying the script
-#    GOVUK_CF_PWD (required) - the password for the user account
+#    PROD_GOVUK_CF_USER (required) - the user deploying the script
+#    PROD_GOVUK_CF_PWD (required) - the password for the user account
 #    BUILD_DIR - the directory containing the build assets
 #    VAULT_ADDR - the vault service endpoint
 #    VAULT_UNSEAL_KEY (required) - the key used to unseal the vault
 ####################################################################################
-OPTIONS=pszd:a:
-LONGOPTS=push,sanitised,seed,directory:,alias:
+OPTIONS=bsd:a:u:p:
+LONGOPTS=backup,sanitised,directory:,alias:,user:,password:
 
 # -use ! and PIPESTATUS to get exit code with errexit set
 # -temporarily store output to be able to check for errors
@@ -49,22 +55,20 @@ eval set -- "$PARSED"
 AWS_PUSH=${AWS_PUSH:=n}
 DIRECTORY=${DIRECTORY:="/tmp"}
 DRUPAL_ALIAS=${DRUPAL_ALIAS:="@par.paas"}
+DEV_GOVUK_CF_USER=${DEV_GOVUK_CF_USER:-}
+DEV_GOVUK_CF_PWD=${DEV_GOVUK_CF_PWD:-}
 
 # Set the defaults.
-CLEANSED=unsanitised
+SANITISED=unsanitised
 
 while true; do
     case "$1" in
-        -p|--push)
+        -b|--backup)
             AWS_PUSH=y
             shift
             ;;
         -s|--sanitised)
-            CLEANSED=sanitised
-            shift
-            ;;
-        -z|--seed)
-            CLEANSED=seed
+            SANITISED=sanitised
             shift
             ;;
         -d|--directory)
@@ -73,6 +77,14 @@ while true; do
             ;;
         -a|--alias)
             DRUPAL_ALIAS="$2"
+            shift 2
+            ;;
+        -u|--user)
+            DEV_GOVUK_CF_USER="$2"
+            shift 2
+            ;;
+        -p|--password)
+            DEV_GOVUK_CF_PWD="$2"
             shift 2
             ;;
         --)
@@ -93,35 +105,100 @@ if [[ $# -ne 1 ]]; then
 fi
 NAME=$1
 
+
+####################################################################################
+# Allow manual input of missing parameters
+#    DEV_GOVUK_CF_USER (required) - the user deploying the script
+#    DEV_GOVUK_CF_PWD (required) - the password for the user account
+####################################################################################
+if [[ -z "${DEV_GOVUK_CF_USER}" ]]; then
+    echo -n "Enter your Cloud Foundry username: "
+    read DEV_GOVUK_CF_USER
+fi
+if [[ -z "${DEV_GOVUK_CF_PWD}" ]]; then
+    echo -n "Enter your Cloud Foundry password (will be hidden): "
+    read -s DEV_GOVUK_CF_PWD
+fi
+
+
+####################################################################################
+# Login to GovUK PaaS
+####################################################################################
+printf "Authenticating with GovUK PaaS...\n"
+
+if [[ $NAME == 'production' ]] || [[ $NAME == production-* ]]; then
+    cf login -a api.cloud.service.gov.uk -u $DEV_GOVUK_CF_USER -p $DEV_GOVUK_CF_PWD \
+      -o "office-for-product-safety-and-standards" -s "primary-authority-register-production"
+elif [[ $NAME == 'staging' ]] || [[ $NAME == staging-* ]]; then
+    cf login -a api.cloud.service.gov.uk -u $DEV_GOVUK_CF_USER -p $DEV_GOVUK_CF_PWD \
+      -o "office-for-product-safety-and-standards" -s "primary-authority-register-staging"
+else
+    cf login -a api.cloud.service.gov.uk -u $DEV_GOVUK_CF_USER -p $DEV_GOVUK_CF_PWD \
+      -o office-for-product-safety-and-standards -s primary-authority-register-development
+fi
+
+
 ####################################################################################
 # Backup Database to s3
 ####################################################################################
-WEBROOT="${BASH_SOURCE%/*}/../web"
+WEBROOT="${BASH_SOURCE%/*}/.."
 cd $WEBROOT
 printf "Current working directory: $PWD\n"
 
-FILE_NAME="db-dump-$NAME-$CLEANSED"
+SERVICE_NAME="par-pg-$NAME"
+FILE_NAME="db-dump-$NAME-$SANITISED"
 DATE=$(date +%Y-%m-%d)
 
 mkdir -p $DIRECTORY
 rm -f $DIRECTORY/$FILE_NAME.sql
 
 printf "Exporting database dump...\n"
-printf "Running: drush $DRUPAL_ALIAS sql-dump --result-file='$DIRECTORY/$FILE_NAME.sql'\n"
-../vendor/drush/drush/drush $DRUPAL_ALIAS sql-dump --result-file="$DIRECTORY/${FILE_NAME}_raw.sql"
+
+# Select the tables to exclude from the dump entirely.
+skip_tables=(
+  migration_*
+  test*
+  watchdog
+  webprofiler
+)
+# Select the tables to only dump the schema for, ignoring all content.
+structure_tables=(
+  cache
+  cache_*
+  history
+  search_*
+  sessions
+  flood
+)
+
+# Set the dump arguments
+dump_args=(
+  -f "$DIRECTORY/${FILE_NAME}.sql"
+  --verbose
+  --clean
+  --no-owner
+  --no-acl
+)
+for t in ${skip_tables[@]}; do
+  dump_args+=(--exclude-table 'public.'$t)
+done
+for t in ${structure_tables[@]}; do
+  dump_args+=(--exclude-table-data 'public.'$t)
+done
+
+# Using `pg_dump` instead of `drush sql:dump` @see https://regulatorydelivery.atlassian.net/browse/PAR-1699
+# postgres client binary no longer available to drush in deployed app.
+echo -e "Piping command: cf conduit $SERVICE_NAME -c '{\"read_only\": true}'"
+echo -e "Running: pg_dump ${dump_args[@]}"
+cf conduit $SERVICE_NAME -c '{"read_only": true}' -- \
+  pg_dump "${dump_args[@]}"
 
 ## Remove erroneous pg_dump roles applied despite --no-owner flags.
 ## https://stackoverflow.com/questions/60605491/using-pg-dump-with-no-owner-information-still-outputting-role-information
-sed -e '/-- do not execute if not member of manager role/,+4d' $DIRECTORY/${FILE_NAME}_raw.sql > $DIRECTORY/$FILE_NAME.sql
+#sed -e '/-- do not execute if not member of manager role/,+4d' $DIRECTORY/${FILE_NAME}_raw.sql > $DIRECTORY/$FILE_NAME.sql
 
 printf "Packaging database dump...\n"
 tar -zcvf $DIRECTORY/$FILE_NAME-latest.tar.gz -C $DIRECTORY "$FILE_NAME.sql"
 tar -zcvf $DIRECTORY/$FILE_NAME-$DATE.tar.gz -C $DIRECTORY "$FILE_NAME.sql"
-
-if [[ $AWS_PUSH == y ]]; then
-    printf "Uploading database archives...\n"
-    ../vendor/drush/drush/drush fsp s3backups $DIRECTORY/$FILE_NAME-latest.tar.gz $FILE_NAME-latest.tar.gz
-    ../vendor/drush/drush/drush fsp s3backups $DIRECTORY/$FILE_NAME-$DATE.tar.gz $FILE_NAME-$DATE.tar.gz
-fi
 
 printf "Database archive completed...\n"
