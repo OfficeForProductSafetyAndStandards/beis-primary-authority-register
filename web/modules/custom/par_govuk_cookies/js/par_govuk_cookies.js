fd6278cc7cf4d385e6d1c42029d92a0c0af5937f
Robert Wild <robert.wild@affinity-digital.com>
2024-11-15 08:41:35 +0000

    PAR-2500 GTM Updates
    - Swaps out contrib module and custom module and replaces with a combo module that does it all

---
+++ web/modules/custom/par_govuk_cookies/js/par_govuk_cookies.js
@@ -0,0 +1,71 @@
+/**
+ * @file
+ * GOVUK Cookies banner behaviors.
+ */
+(function (Drupal, cookies) {
+
+  'use strict';
+
+  Drupal.behaviors.hideCookieBanner = {
+    attach: function (context, settings) {
+      // This should only be necessary if the page is cached
+      // with the cookie banner in place.
+      const banner_settings = settings.par_govuk_cookies.banner;
+      const cookie_policy = cookies.get(banner_settings.cookie_name);
+
+      // Remove the cookie banner if cookies are set.
+      if (cookie_policy) {
+        document.querySelectorAll('.par-govuk-cookie-banner__message').forEach(function (message) {
+          message.remove()
+        })
+        document.querySelectorAll('.par-govuk-cookie-banner').forEach(function (banner) {
+          banner.remove()
+        })
+      }
+    }
+  };
+
+  Drupal.behaviors.interactCookieBanner = {
+    attach: function (context, settings) {
+      document.querySelectorAll('.par-govuk-cookie-banner__message .govuk-button').forEach(function (el) {
+        el.addEventListener('click', function (event) {
+          let button = event.target.value;
+
+          // Accepted.
+          if (button === 'accept') {
+            setCookie('cookie_policy', '{"usage":true,"campaigns":true,"other":true}', 365);
+            setCookie('cookie_preferences_set', 'true', 365);
+          }
+
+          // Rejected.
+          if (button === 'reject') {
+            setCookie('cookie_policy', '', 365);
+            setCookie('cookie_preferences_set', 'false', 365);
+          }
+
+          // Hide the current cookie message.
+          let current_message = event.target.closest('.par-govuk-cookie-banner__message');
+          current_message.hidden = true;
+
+          // Show and focus the next appropriate cookie message if set.
+          let next_message_id = event.target.dataset.cookieReplacementMessage;
+          if (next_message_id !== undefined) {
+            let next_message = document.getElementById(next_message_id);
+            next_message.hidden = false;
+            next_message.focus({ preventScroll:true });
+          }
+          // Else hide the entire banner.
+          else {
+            document.querySelectorAll('.par-govuk-cookie-banner__message').forEach(function (message) {
+              message.remove()
+            })
+            document.querySelectorAll('.par-govuk-cookie-banner').forEach(function (banner) {
+              banner.remove()
+            })
+          }
+        });
+      });
+    }
+  };
+
+})(Drupal, window.Cookies);
