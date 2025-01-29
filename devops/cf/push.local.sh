e7791acff855da18421f6bd42c4d021991f4286a
Robert Wild <robert.wild@affinity-digital.com>
2025-01-29 10:38:22 +0000

    stop staging getting a new db

--- devops/cf/push.local.sh
+++ devops/cf/push.local.sh
@@ -476,7 +476,7 @@ fi
 # TODO Error happens here if there's an error during the creation of services.
 # some services may not be in the correct state to tear down and so may not be removed.
 # Catch any service errors and don't tear down until all services are ready.
-if [[ $ENV != "production" ]]; then
+if [[ $ENV != "production" ]] && [[ $ENV != "staging" ]]; then
     ## Check for the postgres database service
     if ! cf service $PG_BACKING_SERVICE 2>&1; then
         echo "################################################################################################"
@@ -530,7 +530,7 @@ if [[ $ENV == "production" ]] && cf service $LOGGING_BACKING_SERVICE 2>&1; then
 fi
 
 ## Deployment to no production environments need a database
-if [[ $ENV != "production" ]] && [[ $DB_RESET == 'y' ]] && [[ ! -f $DB_IMPORT ]]; then
+if [[ $ENV != "production" ]] && [[ $ENV != "staging" ]] && [[ $DB_RESET == 'y' ]] && [[ ! -f $DB_IMPORT ]]; then
     printf "Non-production environments need a copy of the database to seed from at '$DB_IMPORT'.\n"
     exit 5
 fi
@@ -544,7 +544,7 @@ printf "Starting the application...\n"
 cf start $TARGET_ENV
 
 ## Import the seed database and then delete it.
-if [[ $ENV != "production" ]] && [[ $DB_RESET ]]; then
+if [[ $ENV != "production" ]] && [[ $ENV != "staging" ]] && [[ $DB_RESET ]]; then
     if [[ ! -f "$BUILD_DIR/$DB_DIR/$DB_NAME.tar.gz" ]]; then
         printf "Seed database required, but could not find one at '$BUILD_DIR/$DB_DIR/sanitised-db.sql'.\n"
         exit 6
