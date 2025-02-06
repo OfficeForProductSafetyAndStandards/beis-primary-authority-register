7f93415c4ce08684cb25d5450a316ff456fe1e51
Robert Wild <robert.wild@affinity-digital.com>
2025-02-06 11:17:38 +0000

    PAR-2516: Clearing setting env

--- scripts/create-stage-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -o errexit -euo pipefail -o noclobber -o nounset
-
-cf env beis-par-staging | \
-  awk '/environment_variables/{print; getline; while (match($0, /^[[:space:]]*".*":/)) {gsub(/[",]/, "", $0); gsub(/^[[:space:]]*{/, "", $0); gsub(/}$/, "", $0); split($0, a, ":"); printf "%s=%s\n", a[1], a[2]; getline;}}' | \
-  sed 's/^[[:space:]]*//;s/[[:space:]]*$//' > ${BASH_SOURCE%/*}/../.env
