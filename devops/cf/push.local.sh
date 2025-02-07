2f5efe82cc9a896b85ab55ded8a786a31301d024
Robert Wild <robert.wild@affinity-digital.com>
2025-02-07 08:50:54 +0000

    PAR-2516: Removing duplication of db sanitisation

--- devops/cf/push.local.sh
+++ devops/cf/push.local.sh
@@ -478,7 +478,6 @@ if [[ $ENV != "production" ]] && [[ $DB_RESET ]]; then
         cd $REMOTE_BUILD_DIR/web && \
         tar --no-same-owner -zxvf $REMOTE_BUILD_DIR/$DB_DIR/$DB_NAME.tar.gz -C $REMOTE_BUILD_DIR/$DB_DIR && \
         ../vendor/bin/drush @par.paas sql:cli < $REMOTE_BUILD_DIR/$DB_DIR/$DB_NAME.sql && \
-        ../vendor/bin/drush spp && \
         rm -f $REMOTE_BUILD_DIR/$DB_DIR/$DB_NAME.sql"
 
     # Wait for database to be imported.
