ccc18924728b6d6424e65e1ebbb8d5c2e0e078a8
Robert Wild <robert.wild@affinity-digital.com>
2025-02-07 11:33:47 +0000

    PAR-2516: Updating build tasks

--- scripts/sanitise-par-people.sh
+++ scripts/sanitise-par-people.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# This script will push local assets to an environment.
 echo $BASH_VERSION
-
 set -o errexit -euo pipefail -o noclobber -o nounset
 
 WEBROOT="${BASH_SOURCE%/*}/../web"
