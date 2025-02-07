e55337fb8bfa4d03542bc880d94fba9ca09c7bcf
Robert Wild <robert.wild@affinity-digital.com>
2025-02-07 12:47:21 +0000

    PAR-251: Simplify update task

--- scripts/drupal-update.sh
+++ scripts/drupal-update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-## Commands that must be run to update a drupal instance.
-## Use as `./drupal-update.sh`
+
 echo $BASH_VERSION
 
 set -o errexit -euo pipefail -o noclobber -o nounset
@@ -10,50 +9,12 @@ cd $WEBROOT
 
 echo "Current working directory is ${PWD}"
 
-# Put the site in maintenance mode.
-printf "Enabling maintenance mode...\n"
-drush cr;
-drush state:set system.maintenance_mode 1 --input-format=integer;
-# Clear cache
-printf "Clearing cache...\n"
-drush cr;
-
-# Run db updates.
-printf "Running database updates...\n"
-drush updb -y;
-
-## CONFIG IMPORT
-printf "Importing config...\n"
-counter=1;
-# Check that there's no remaining config diff.
-until drush --quiet --no cim &> /dev/null
-do
-  if [ $counter -gt 5 ]; then
-      echo "Config successfully imported after #$counter tries"
-      break
-  fi
-  echo "Trying import: #$counter"
-  # This import is allowed to fail.
-  drush cim -y || true
-  ((counter++))
-  sleep 1;
-done
-# Run config import a second time to avoid installed
-# module config overriding saved config.
-printf "Re-importing config...\n"
+drush cr
+drush state:set system.maintenance_mode 1
+drush cr
+drush updb -y
 drush cim -y
-
-# To doubly make sure drush registers features commands.
-printf "Clearing drush caches...\n"
 drush cr drush;
-# Revert all features
-printf "Reverting features...\n"
 drush features:import:all -y;
-
-# Take the site out of maintenance mode.
-printf "Disabling maintenance mode...\n"
 drush state:set system.maintenance_mode 0;
-
-# Clear cache.
-printf "Clearing final cache...\n"
 drush cr;
